d4c8459fc4164f2b534ebb067d60aa1bfb548bc3
martind <martind@mapfactor.com>
2016-12-06 07:11:13 +0100

    plot: filter input sources, draw WHEEL data

--- tools/plot.py
+++ tools/plot.py
@@ -12,13 +12,38 @@ import matplotlib.pyplot as plt
 def get_arr(filename):
     arr = []
     for line in open(filename):
-        if 'ENC' in line:
-            prefix_cmd, x, y = line.split()
-            arr.append((int(x), int(y)))
+        if 'xENC' in line:
+            prefix_cmd, t, x, y = line.split()
+            arr.append((t, (int(x), int(y))))
+        if 'WHEEL' in line:
+            prefix_cmd, t, angle, desired = line.split()
+            if desired != 'None':
+                arr.append((t, (int(angle), float(desired))))
+            else:
+                arr.append((t, (int(angle), 0)))
+        if 'xSPEED' in line:
+            prefix_cmd, t, raw, avr = line.split()
+            arr.append((t, (raw, avr)))
+        if 'xGAS' in line:
+            prefix_cmd, gas = line.split()
+            arr.append((len(arr), int(gas)))
+        if 'xSYNC' in line:
+            gas = line.split()[-1]
+            if gas != '0':
+                arr.append((len(arr), float(gas)))
     return arr
 
 def draw(arr):
-    plt.plot(arr, 'o-', linewidth=2)
+#    plt.plot(arr, 'o-', linewidth=2)
+    x = [x for (x, _) in arr]
+    y = [y for (_, y) in arr]
+    plt.plot(x, y, 'o-', linewidth=2)
+
+#    z = []
+#    for i in xrange(len(y)):
+#        z.append(sum(y[i-50:i+50])/100.0)
+#    plt.plot(x, z, 'o-', linewidth=2)
+
     plt.show()
 
 
