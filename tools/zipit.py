07079c4e21019730021687d05b4f32a08e55f47c
Martin Dlouhy <md@robotika.cz>
2017-06-12 21:18:42 +0200

    Add tool for zipping output logs

---
+++ tools/zipit.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+  simple tool ZIP output logfile into single ZIP archive
+    ./zipit.py <metalog filename>
+"""
+
+import sys
+import os
+from zipfile import ZipFile
+
+
+def zipit(metalog_filename, output_filename):
+    dir_name = os.path.dirname(metalog_filename)
+    with ZipFile(output_filename, 'w') as myzip:
+        camera_file = None
+        for line in open(metalog_filename):
+            if line.startswith('['):
+                continue  # initial arguments
+            assert ': ' in line, line
+            name = line.split()[1].strip()
+            assert name.startswith('logs/')
+            filename = name[5:]
+            print filename
+            myzip.write(os.path.join(dir_name, filename), filename)
+            if filename.startswith('gps_'):
+                filename = filename.replace('.log', '.nmea')
+                filename = filename.replace('gps_', 'gps')
+                print filename
+                myzip.write(os.path.join(dir_name, filename), filename)
+            elif filename.startswith('camera_'):
+                camera_file = os.path.join(dir_name, filename)
+        
+        if camera_file is not None:
+            for line in open(camera_file):
+                if not line.startswith('('):
+                    continue
+                name = eval(line)[0]
+                assert name.startswith('logs/')
+                filename = name[5:]
+                print filename
+                myzip.write(os.path.join(dir_name, filename), filename)
+
+
+if __name__ == "__main__": 
+    if len(sys.argv) < 2:
+        print(__doc__)
+        sys.exit(2)
+
+    zipit(sys.argv[1], 'out.zip')
+
+# vim: expandtab sw=4 ts=4 
+
