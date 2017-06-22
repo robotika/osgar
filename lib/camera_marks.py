7366010aa9090c262afbb5fa1f875026164b77d4
tajgr <levjakub@gmail.com>
2017-06-22 15:56:47 +0200

    Improved cones detector. Key function is the findCones. It returns a list of XY coordinates in a image and list of cones cnt (first red strip, green color).

--- lib/camera_marks.py
+++ lib/camera_marks.py
@@ -34,26 +34,63 @@ def marksDetektor( im ):
     gray = r/mat*255
     gray = gray.astype(np.uint8)
     #cv2.imwrite( "test.png", gray )
-    ret, binaryImg = cv2.threshold( gray, 100, 255,cv2.THRESH_BINARY)
+    ret, binaryImg = cv2.threshold( gray, 110, 255,cv2.THRESH_BINARY)
     #cv2.imwrite( "test2.png", binaryImg )
     binaryImg = openingClosing( binaryImg )
     binaryImg = openingClosing( binaryImg, ker = 4, method = "closing" )
     #cv2.imwrite( "test3.png", binaryImg )
     contours, hierarchy= cv2.findContours( binaryImg, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
     lMarks = []
+    bRects = []
+    areas = []
     for cnt in contours:
-        if cv2.contourArea(cnt) > 16:
+        area = cv2.contourArea(cnt)
+        x,y,w,h = cv2.boundingRect(cnt)
+        whRatio = w/float(h)
+        arRatio = area/float(w*h)
+        if area > 20 and arRatio > 0.5 and whRatio > 0.33 and whRatio < 3:
             lMarks.append(cnt)
-    
-    return lMarks
+            bRects.append([x,y,w,h])
+            areas.append(area)
+        
+    return lMarks, bRects, areas
 
 
-def draw_cones(filename):
+def draw_cnt(filename):
     print filename
     im  = cv2.imread(filename, 1)
-    lMarks = marksDetektor(im)
+    lMarks, bRects, areas = marksDetektor(im)
+    conesXY, lMarksF = findCones(im)
     cv2.drawContours(im, lMarks, -1, (255,0,0), 2)
+    cv2.drawContours(im, lMarksF, -1, (0,255,0), 2)
     cv2.imshow('cones', im)
+    
+def findCones(im):
+    lMarks, bRects, areas = marksDetektor(im)
+    conesXY = []
+    lMarksF = []
+    for ii in range(len(lMarks)):
+        areaM = areas[ii]
+        xM, yM, wM, hM = bRects[ii]
+        xcM, ycM  = xM + wM/2.0, yM + hM/2.0
+        cntAbove = 1
+        for jj in range(len(lMarks)):
+            areaO = areas[jj]
+            xO, yO, wO, hO = bRects[jj]
+            xcO, ycO  = xO + wO/2.0, yO + hO/2.0
+            if abs(xcM-xcO) > wM/2.0:
+                continue
+            elif (ycM - ycO > 4*hM) or (ycM - ycO < hM):
+                continue
+            elif (areaO/areaM > 1.0) or (areaO/areaM < 0.1):
+                continue
+            cntAbove += 1
+        #print cntAbove
+        if cntAbove == 3:
+            conesXY.append([xcM, ycM])
+            lMarksF.append(lMarks[ii])
+            
+    return conesXY, lMarksF
 
 
 if __name__ == "__main__":
@@ -62,13 +99,13 @@ if __name__ == "__main__":
         sys.exit()
 
     if sys.argv[1].endswith('.jpg'):
-        draw_cones(sys.argv[1])
+        draw_cnt(sys.argv[1])
         cv2.waitKey()
     else:
-        for filename in os.listdir(sys.argv[1]):
+        for filename in sorted(os.listdir(sys.argv[1])):
             if filename.endswith('jpg'):
-                draw_cones(os.path.join(sys.argv[1], filename))
-                if cv2.waitKey(100) == 27:
+                draw_cnt(os.path.join(sys.argv[1], filename))
+                if cv2.waitKey(100) % 256 == 27:
                     break
 
 # vim: expandtab sw=4 ts=4
