4246f2fb790d5bbb5ad5ba04a6dee4220627dcca
Zbynek Winkler <zbynek.winkler@gmail.com>
2017-08-23 13:43:15 +0200

    Refactor common code for zip and dir based logs.

--- tools/logparser2.py
+++ tools/logparser2.py
@@ -5,92 +5,117 @@
        ./logparser2.py <metalog>
 """
 import ast
-import math
 import os
-import struct
 import sys
 from zipfile import ZipFile
 
-
-def timestamps_gen(id, stream):
-    if id == 'can': # binary log, not implemented
-        return
-    if id == 'timestamps':
-        for line in stream:
-            line = ast.literal_eval(line)
-            assert len(line) == 3
-            yield float(line[0]), 'can', line[1:]
-    for i, line in enumerate(stream):
-        if i % 2 == 1:
-            data = ast.literal_eval(line)
-            yield timestamp, id, data
-        else:
-            vals = line.split()
-            if len(vals) != 2: # only at the end of file
-                return
-            timestamp = float(vals[1])
-
-
-def sensor_gen(meta_filename, selected=None):
+def sensor_gen(path, *selected):
     """
       Yield timestamp ordered data from all selected sensors.
-      Use None for all available sensors
+      By default return data from all sensors.
     """
-    meta_dir = os.path.dirname(meta_filename)
-    filenames = {}
-    with open(meta_filename, 'rb') as meta:
-        for line in filter(lambda a: ':' in a, meta): # skip unknown lines
-            sensor, filepath = line.strip().split(':')
-            filename = os.path.basename(filepath)
-            filenames[sensor] = os.path.join(meta_dir, filename)
-
-    if selected is not None:
-        # delete filenames not in selected
-        for key in filenames.viewkeys() ^ selected:
-            del filenames[key]
-
-    # zip multiple generators
-    data = []
-    for sensor, filename in filenames.iteritems():
-        with open(filename, 'rb') as stream:
-            data.extend(timestamps_gen(sensor, stream))
-
-    data.sort()
-    for a in data:
-        yield a
-
-
-def sensor_gen_zip(filepath, selected=None):
-    with ZipFile(filepath) as zip:
-        filenames = zip.namelist()
-        meta_filename = [name for name in filenames if name.startswith('meta_')][0]
-
-        filenames = {}
-        with zip.open(meta_filename) as meta:
-            for line in filter(lambda a: ':' in a, meta): # skip unknown lines
-                sensor, filepath = line.strip().split(':')
-                filenames[sensor] = os.path.basename(filepath)
-
-        if selected is not None:
-            # delete filenames not in selected
-            for key in filenames.viewkeys() ^ selected:
-                del filenames[key]
-
-        # zip multiple generators
+    selected = set(selected)
+    with Log(path) as log:
         data = []
-        for sensor, filename in filenames.iteritems():
-            with zip.open(filename) as stream:
-                data.extend(timestamps_gen(sensor, stream))
+        for sensor in log.streams:
+            if sensor != 'can' and (len(selected) == 0 or sensor in selected):
+                data.extend(_stream(log, sensor))
 
         data.sort()
         for a in data:
             yield a
 
+
+def _stream(log, id):
+    if id == 'can': # binary log, not implemented
+        return
+    if id == 'timestamps':
+        with log.open(log.streams[id]) as stream:
+            for line in stream:
+                line = ast.literal_eval(line)
+                assert len(line) == 3
+                yield float(line[0]), 'can', line[1:]
+        return
+
+    with log.open(log.streams[id]) as stream:
+        for i, line in enumerate(stream):
+            if i % 2 == 1:
+                data = ast.literal_eval(line)
+                yield timestamp, id, data
+            else:
+                vals = line.split()
+                if len(vals) != 2: # only at the end of file
+                    return
+                timestamp = float(vals[1])
+
+
+def Log(path):
+    "Factory function returning either ZipLog or Dirlog based on argument."
+    if path.endswith('.zip'):
+        return ZipLog(path)
+    else:
+        filename = os.path.basename(path)
+        assert filename.startswith('meta_')
+        return DirLog(path)
+
+
+class DirLog:
+    def __init__(self, metapath):
+        self.dirpath = os.path.dirname(metapath)
+        self.metaname = os.path.basename(metapath)
+
+    def __enter__(self):
+        self.streams = dict(_meta(self))
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def open(self, filename):
+        assert '/' not in filename
+        return open(self.dirpath + '/' + filename)
+
+    def read(self, filename):
+        with self.open(filename) as f:
+            return f.read()
+
+
+class ZipLog:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def __enter__(self):
+        self.zip = ZipFile(self.filepath)
+        meta_filename = [name for name in self.zip.namelist() if name.startswith('meta_')]
+        assert len(meta_filename) == 1
+        self.metaname = meta_filename[0]
+        self.streams = dict(_meta(self))
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.zip.close()
+
+    def open(self, filename):
+        assert '/' not in filename
+        return self.zip.open(filename)
+
+    def read(self, filename):
+        assert '/' not in filename
+        return self.zip.read(filename)
+
+
+def _meta(log):
+    with log.open(log.metaname) as stream:
+        for line in filter(lambda a: ':' in a, stream):  # skip unknown lines:
+            sensor, filepath = line.strip().split(':')
+            yield sensor, os.path.basename(filepath)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(__doc__)
         sys.exit(2)
-    for timestamp, sensor, data in sensor_gen_zip(sys.argv[1]):
+    for timestamp, sensor, data in sensor_gen(sys.argv[1]):
         print(timestamp, sensor, data[:10])
 
 # vim: expandtab sw=4 ts=4 
