ea6d89145dcc056c57fdee0da3c2dede0f797cf7
Martin Dlouhy <md@robotika.cz>
2017-10-19 19:25:21 +0200

    Refactoring - use LaserDetector class instead of global variables

--- navpat.py
+++ navpat.py
@@ -42,63 +42,64 @@ def min_dist(data, infinity=None):
         return np.min(data[mask]) * 0.001
     return infinity
 
-prev_cones = []
-prev_near = False
-last_laser_update_time = None
 
-def detect_near_extension(robot, id, data):
-    global prev_near, last_laser_update_time
-    if last_laser_update_time is None:
-        # well, we do not want to stop the machine immediately - there could be
-        # first update of some other sensor
-        # TODO review no data from the beginning
-        last_laser_update_time = robot.time
+class LaserDetector:
+    def __init__(self):
+        self.prev_cones = []
+        self.prev_near = False
+        self.last_laser_update_time = None
 
-    if id=='laser':
-        last_laser_update_time = robot.time
-        if data is not None and data != []:
-            if prev_near and min_dist(data) < 0.5:
-                raise NearObstacle()
-            prev_near = min_dist(data) < 1.0
-#            prev_near = False # suicide!
+    def near_range_extension(self, robot, id, data):
+        if self.last_laser_update_time is None:
+            # well, we do not want to stop the machine immediately - there could be
+            # first update of some other sensor
+            # TODO review no data from the beginning
+            self.last_laser_update_time = robot.time
 
-            finder = ConeLandmarkFinder()
-            global prev_cones
-            cones = finder.find_cones(data)
-#            print '(%.2f, %.2f, %.3f)' % robot.localization.pose(), finder.match_pairs(prev_cones, cones)
-            robot.localization.update_landmarks(id, cones)
-            prev_cones = cones
-            # TODO:
-            #  - collection of all potential cones
-            #  - cross distance verification
-            #  - "feature tracking"
-            #  - localization
-            #  - camera verification
-    
-    elif last_laser_update_time + 1.0 < robot.time:
-        raise NoLaserData()
+        if id=='laser':
+            self.last_laser_update_time = robot.time
+            if data is not None and data != []:
+                if self.prev_near and min_dist(data) < 0.5:
+                    raise NearObstacle()
+                self.prev_near = min_dist(data) < 1.0
+#                self.prev_near = False # suicide!
+
+                finder = ConeLandmarkFinder()
+                cones = finder.find_cones(data)
+#                print '(%.2f, %.2f, %.3f)' % robot.localization.pose(), finder.match_pairs(prev_cones, cones)
+                robot.localization.update_landmarks(id, cones)
+                self.prev_cones = cones
+                # TODO:
+                #  - collection of all potential cones
+                #  - cross distance verification
+                #  - "feature tracking"
+                #  - localization
+                #  - camera verification
+
+        elif self.last_laser_update_time + 1.0 < robot.time:
+            raise NoLaserData()
 
 
-def navpat_viewer_extension(robot, id, data):
-    if id == 'laser':
-        x, y, heading = robot.localization.pose()
-        laser_pose = x + math.cos(heading)*LASER_OFFSET[0], y + math.sin(heading)*LASER_OFFSET[0], heading
+    def viewer_extension(self, robot, id, data):
+        if id == 'laser':
+            x, y, heading = robot.localization.pose()
+            laser_pose = x + math.cos(heading)*LASER_OFFSET[0], y + math.sin(heading)*LASER_OFFSET[0], heading
 
-        for raw_angle, raw_dist, raw_width in prev_cones:
-            dist = raw_dist/1000.0
-            angle = math.radians(raw_angle/2 - 135)
-            xx, yy, _ = getCombinedPose(laser_pose, (math.cos(angle)*dist, math.sin(angle)*dist, 0))
-            color = (0xFF, 0x80, 0)
-            colors = [(0xFF, 0xFF, 0xFF), (0xFF, 0, 0), (0, 0xFF, 0), (0, 0, 0xFF)]
-            for cone_xy, cone_color in zip(robot.localization.global_map, colors):
-                if math.hypot(xx-cone_xy[0], yy-cone_xy[1]) < 2.0:
-                    color = cone_color
+            for raw_angle, raw_dist, raw_width in self.prev_cones:
+                dist = raw_dist/1000.0
+                angle = math.radians(raw_angle/2 - 135)
+                xx, yy, _ = getCombinedPose(laser_pose, (math.cos(angle)*dist, math.sin(angle)*dist, 0))
+                color = (0xFF, 0x80, 0)
+                colors = [(0xFF, 0xFF, 0xFF), (0xFF, 0, 0), (0, 0xFF, 0), (0, 0, 0xFF)]
+                for cone_xy, cone_color in zip(robot.localization.global_map, colors):
+                    if math.hypot(xx-cone_xy[0], yy-cone_xy[1]) < 2.0:
+                        color = cone_color
 
-            width = raw_width * math.radians(0.5) * raw_dist/1000.0  # in meters
-            print "width", width
-            if width < 0.05 or width > 0.5:
-                color = (128, 128, 128)  # gray
-            viewer_scans_append( ( (xx, yy, 0), -1.5, color) ) # color param
+                width = raw_width * math.radians(0.5) * raw_dist/1000.0  # in meters
+                print "width", width
+                if width < 0.05 or width > 0.5:
+                    color = (128, 128, 128)  # gray
+                viewer_scans_append( ( (xx, yy, 0), -1.5, color) ) # color param
 
 
 def follow_line(robot, line, speed=None, timeout=None):
@@ -176,13 +177,15 @@ def navigate_pattern(robot, metalog, conf, viewer=None):
     robot.canproxy.stop()
     robot.canproxy.set_turn_raw(0)
 
+    laser_detector = LaserDetector()    
+
     if viewer is not None:
-        robot.extensions.append(('navpat_viewer', navpat_viewer_extension))
+        robot.extensions.append(('navpat_viewer', laser_detector.viewer_extension))
 
     speed = 0.5
 
     try:
-        robot.extensions.append(('detect_near', detect_near_extension))
+        robot.extensions.append(('detect_near', laser_detector.near_range_extension))
 
         for i in xrange(10):
 #            run_oval(robot, speed)
@@ -192,7 +195,7 @@ def navigate_pattern(robot, metalog, conf, viewer=None):
         print "Near Exception Raised!"
         robot.extensions = []  # hack
     except NoLaserData:
-        print "!!!ERROR!!! Missing laser updates for last {:.1f}s".format(robot.time - last_laser_update_time)
+        print "!!!ERROR!!! Missing laser updates for last {:.1f}s".format(robot.time - laser_detector.last_laser_update_time)
         robot.extensions = []  # hack
 
     robot.canproxy.stop()
