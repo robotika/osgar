8cceba5ff0071ef4d082b43fd743806e4219fb15
Martin Dlouhy <md@robotika.cz>
2018-01-30 15:50:30 +0100

    Integrate driver.spider
    - add --turn parameter for easier testing
    - add parsing and printing 0x204

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -1,6 +1,7 @@
 from .gps import GPS
 from .imu import IMU
+from .spider import Spider
 
 # dictionary of all available drivers
-all_drivers = dict(gps=GPS, imu=IMU)
+all_drivers = dict(gps=GPS, imu=IMU, spider=Spider)
 
