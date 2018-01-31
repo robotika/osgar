a47db4e97483d623b446af8c732d7bcc5c2d553f
Martin Dlouhy <md@robotika.cz>
2018-01-31 13:04:45 +0100

    Fix bug in dGPS parsing

--- drivers/test_gps.py
+++ drivers/test_gps.py
@@ -16,13 +16,27 @@ class GPSTest(unittest.TestCase):
         self.assertEqual(GPS.parse_line(line), (51750280, 180463129))  # arc milliseconds (x, y) = (lon, lat)
 
     def test_split(self):
-        buf = b'blabla$something*12'
-        self.assertEqual(GPS.split_buffer(buf), (b'', b'$something*12'))
+        buf = b'blabla$GPGGAsomething*12\r\n'
+        self.assertEqual(GPS.split_buffer(buf), (b'\r\n', b'$GPGGAsomething*12'))
 
         self.assertEqual(GPS.split_buffer(b'$toofew*1'), (b'$toofew*1', b''))
 
     def test_parsing_old_GPGGA(self):
         line = b'$GPGGA,051852.000,5005.0244,N,01430.3360,E,1,06,3.8,253.1,M,45.4,M,,0000*58'
         self.assertEqual(GPS.parse_line(line), (52220160, 180301464))  # arc milliseconds (x, y) = (lon, lat)
+
+    def test_split_with_binary_data(self):
+        buf = b'\xb5b\x010\x04\x01\xf8n\x8a\x0e\x15\x04\x00\x00\x05\x02\r\x07&&@\x00S\xff\xff\xff\x02\x04\x10\x07$\xa5' \
+            b'\x00\x00\x00\x00\x00\x00\x13\x05\x04\x04\x15\x00h\x00\x00\x00\x00\x00\x03\x06\r\x07\x1b\r#\x00\xee\xfa' \
+            b'\xff\xff\x00\x0c\r\x07/*`\x00\x1d\x00\x00\x00\x04\x0e\r\x07"\x17\x0b\x01\x12\x01\x00\x00\n\x18\x0c\x07"' \
+            b'\x07\x9e\x00\x8b\x00\x00\x00\x01\x19\r\x070S8\x00\xc8\xff\xff\xff\x0f\x1a\x04\x01\x00\x04\x1f\x01\x00\x00' \
+            b'\x00\x00\r\x1d\r\x071C\xe3\x00\xcf\xff\xff\xff\x06\x1f\r\x07&).\x01\xa2\x03\x00\x00\x14 \x00\x07$\xa5\x00' \
+            b'\x00\x00\x00\x00\x00\x0cA\x04\x07\x1c\x05\x1b\x00\x00\x00\x00\x00\x12G\x04\x01\x00\x02*\x01\x00\x00\x00\x00' \
+            b'\x0bH\x04\x04\x13\x0bW\x01\x00\x00\x00\x00\x08I\r\x07\x1b\x1c=\x01]\x02\x00\x00\x11O\r\x07)\x1c\x88\x00\xdf' \
+            b'\xfe\xff\xff\x10P\r\x07(VD\x01}\xff\xff\xff\tQ\r\x07\',)\x00\x1a\x02\x00\x00\x07R\r\x07*N\xb5\x00\xd7\xfe' \
+            b'\xff\xff\x0eS\r\x07!\x19\xd0\x00\xca\x00\x00\x00\x83\x17\xb5b\x01\x03\x10\x00\xf8n\x8a\x0e\x03\xdd\x00\x08' \
+            b'iy\x00\x00\xdd\xd9\x03\x00\x95($GNGGA,194535.40,5007.71276,N,01422.49206,E,1,12,0.80,284.6,M,44.3,M,,*42\r\n'
+        buf, nmea = GPS.split_buffer(buf)
+        self.assertEqual(nmea, b'$GNGGA,194535.40,5007.71276,N,01422.49206,E,1,12,0.80,284.6,M,44.3,M,,*42')
 
 # vim: expandtab sw=4 ts=4
