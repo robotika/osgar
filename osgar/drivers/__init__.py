329c15ada2911e2eb893567499975e2112f926c7
Martin Dlouhy <md@robotika.cz>
2018-02-20 06:56:16 +0100

    CANSerial - serial line adapter for CAN bus
    - provide simple parsing of CAN messages
    - add fake DBC

--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -2,7 +2,9 @@ from .gps import GPS
 from .imu import IMU
 from .spider import Spider
 from .logserial import LogSerial
+from .canserial import CANSerial
 
 # dictionary of all available drivers
-all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial)
+all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial,
+                   can=CANSerial)
 
---
+++ osgar/drivers/test_canserial.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest.mock import MagicMock
+
+from osgar.drivers.canserial import CANSerial, CAN_packet
+from osgar.lib.logger import LogWriter
+
+
+class CANSerialTest(unittest.TestCase):
+
+    def test_split_buffer(self):
+        self.assertEqual(CANSerial.split_buffer(b''), (b'', b''))
+        self.assertEqual(CANSerial.split_buffer(b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x10'), (b'', b'\xfe\x10'))
+        data = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfeW\xfe0@h\x9e\x01i\x01\xf7\x01\x18\x00'
+        self.assertEqual(CANSerial.split_buffer(data), (b'\xfe0@h\x9e\x01i\x01\xf7\x01\x18\x00', b'\xfeW'))
+
+        data = b'0@h\x9e\x01i\x01\xf7\x01\x18\x00'
+        self.assertEqual(CANSerial.split_buffer(data), (b'h\x9e\x01i\x01\xf7\x01\x18\x00', b'0@'))
+
+    def test_can_packet(self):
+        self.assertEqual(CAN_packet(0x400, [0, 0]), b'\x80\x02\x00\x00')
+
+    def test_can_bridge_initialization(self):
+        bus = MagicMock()
+        can = CANSerial(config={}, bus=bus)
+        self.assertFalse(can.can_bridge_initialized)
+
+        for __ in can.process_gen(b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\x10'):
+            pass
+        self.assertTrue(can.can_bridge_initialized)
+
+        bus.publish.assert_called_with('raw', b'\xfe1')
+
+# vim: expandtab sw=4 ts=4
