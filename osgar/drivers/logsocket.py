9958d04f6f225f0a7013b2f0abba1cb6d53d6b6c
Martin Dlouhy <md@robotika.cz>
2018-03-26 17:32:42 +0200

    Implement LogTCP for SICK laser integration

---
+++ osgar/drivers/logsocket.py
@@ -0,0 +1,68 @@
+"""
+  Wrapper & timestamper of input/out over socket
+"""
+
+import socket
+from threading import Thread
+
+from osgar.lib.logger import LogWriter
+from osgar.drivers.bus import BusShutdownException
+
+
+class LogTCP:
+    def __init__(self, config, bus):
+        self.input_thread = Thread(target=self.run_input, daemon=True)
+        self.output_thread = Thread(target=self.run_output, daemon=True)
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = config['host']
+        port = config['port']
+        self.socket.connect((host, port))
+        if 'timeout' in config:
+            self.socket.settimeout(config['timeout'])
+
+        self.bus = bus
+        self.buf = b''
+
+    def start(self):
+        self.input_thread.start()
+        self.output_thread.start()
+
+    def join(self, timeout=None):
+        self.input_thread.join(timeout=timeout)
+        self.output_thread.join(timeout=timeout)
+
+    def run_input(self):
+        while self.bus.is_alive():
+            try:
+                data = self.socket.recv(1024)
+                if len(data) > 0:
+                    self.bus.publish('raw', data)
+            except socket.timeout:
+                pass
+
+    def run_output(self):
+        try:
+            while True:
+                __, __, data = self.bus.listen()
+                self.socket.send(data)
+        except BusShutdownException:
+            pass
+
+    def request_stop(self):
+        self.bus.shutdown()
+
+
+if __name__ == "__main__":
+    import time
+    from osgar.drivers.bus import BusHandler
+
+    config = {'host':'localhost', 'port': 8001, 'timeout': 1.0}
+    log = LogWriter(prefix='test-tcp-')
+    device = LogTCP(config, bus=BusHandler(log, out={'raw':[]}))
+    device.start()
+    time.sleep(2)
+    device.request_stop()
+    device.join()
+
+# vim: expandtab sw=4 ts=4
