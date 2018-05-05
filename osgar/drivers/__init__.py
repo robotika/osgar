58d2b73e292e4fdf715830baa191f6c26594a2d9
Martin Dlouhy <md@robotika.cz>
2018-05-05 13:36:38 +0200

    Create simple SpiderSimulator for ro2018.py
    - read initial GPS position from config
    - integrate relative pose
    - integrate raw wheels angles status
    - duration as xternal parameter

--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -3,8 +3,9 @@ from .imu import IMU
 from .spider import Spider
 from .logserial import LogSerial
 from .canserial import CANSerial
+from .simulator import SpiderSimulator
 
 # dictionary of all available drivers
 all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial,
-                   can=CANSerial)
+                   can=CANSerial, simulator=SpiderSimulator)
 
