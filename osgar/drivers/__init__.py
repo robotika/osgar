f8ac86386311c322d713e7ae0dfe5274ab2e40ff
Martin Dlouhy <md@robotika.cz>
2018-09-04 16:51:21 +0200

    Add config for test I2C and publish "i2c" driver

--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -6,9 +6,11 @@ from .canserial import CANSerial
 from .simulator import SpiderSimulator
 from .logsocket import LogTCP, LogUDP
 from .sicklidar import SICKLidar
+from .logi2c import LogI2C
 
 # dictionary of all available drivers
 all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial,
                    can=CANSerial, simulator=SpiderSimulator,
-                   tcp=LogTCP, udp=LogUDP, lidar=SICKLidar)
+                   tcp=LogTCP, udp=LogUDP, lidar=SICKLidar,
+                   i2c=LogI2C)
 
