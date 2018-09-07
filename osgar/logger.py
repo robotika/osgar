abfa4b383bf4abf3c4873098b730beceaefc793e
Martin Dlouhy <md@robotika.cz>
2018-09-07 18:31:20 +0200

    logger.py - support look up stream by name

--- osgar/logger.py
+++ osgar/logger.py
@@ -32,6 +32,7 @@
 import datetime
 import struct
 from threading import RLock
+from ast import literal_eval
 
 
 INFO_STREAM_ID = 0
@@ -139,6 +140,18 @@ class LogAsserter(LogReader):
             pass
 
 
+def lookup_stream_id(filename, stream_name):
+    names = []
+    with LogReader(args.logfile) as log:
+        for __, __, line in log.read_gen(0):
+            if b'Errno' in line:
+                continue
+            d = literal_eval(line.decode('ascii'))
+            if 'names' in d:
+                names = d['names']
+    return names.index(stream_name) + 1
+
+
 if __name__ == "__main__":
     import argparse
     import sys
@@ -147,13 +160,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract data from log')
     parser.add_argument('logfile', help='filename of stored file')
     parser.add_argument('--stream', help='stream ID', type=int, default=None)
+    parser.add_argument('--name', help='stream name', default=None)
     parser.add_argument('--times', help='display timestamps', action='store_true')
     parser.add_argument('--raw', help='skip data deserialization',
                         action='store_true')
     args = parser.parse_args()
 
+    only_stream = args.stream
+    if args.name is not None:
+        only_stream = lookup_stream_id(args.logfile, args.name)
+
     with LogReader(args.logfile) as log:
-        for timestamp, stream_id, data in log.read_gen(args.stream):
+        for timestamp, stream_id, data in log.read_gen(only_stream):
             if not args.raw and stream_id != 0:
                 data = deserialize(data)
             if args.times:
