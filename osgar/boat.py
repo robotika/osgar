0db483e624da818fce26a05475caf64bbb2b03b8
Martin Dlouhy <md@robotika.cz>
2018-09-07 21:46:15 +0200

    boat.py - add parameter --duration
    boat.py - integrate angular_speed from gyro

--- osgar/boat.py
+++ osgar/boat.py
@@ -15,7 +15,7 @@ from osgar.robot import Robot
 from osgar.bus import BusHandler
 
 
-TIMEOUT = timedelta(seconds=10)
+TIMEOUT = timedelta(seconds=10)  # configurable via --duration parameter
 
 
 ################ COPY & PASTE ###################
@@ -128,7 +128,8 @@ def navigate(boat, waypoints):
 #                    angular_speed = 0.0 # hack for now
 #                    angular_speed = -angular_speed
 #                    angular_speed = -angular_speed*0.5
-                    angular_speed = -angular_speed*2
+#                    angular_speed = -angular_speed*2
+                    angular_speed *= 2  # former version did not correct gyro direction
 
                     diff = math.degrees(normalizeAnglePIPI(gpsAngle-compassAngle-angular_speed))
                     print("ANGLES DIFF =", diff, dist)  #, (gpsAngle, compassAngle)
@@ -168,6 +169,7 @@ class BoatMarina2:
 
         self.gps = GPSData()
         self._heading = None
+        self._angular_speed = None
         self.channel_move = 1000
         self.channel_turn = 1000
 
@@ -175,7 +177,7 @@ class BoatMarina2:
         return self._heading  # TODO refactor
 
     def angular_speed(self):
-        return None
+        return self._angular_speed
 
     def set_desired_speed(self, speed, angular_speed):
         # legacy function
@@ -201,6 +203,8 @@ class BoatMarina2:
                 else:
                     self._heading = math.radians(data/100)
                 self.bus.publish('move', [self.channel_move, self.channel_turn])
+            elif channel == 'angular_speed':
+                self._angular_speed = math.radians(data/14.375)  # copy & paste from I2CLibraries
 
     def start(self):
         pass
@@ -251,12 +255,17 @@ if __name__ == "__main__":
     parser_run = subparsers.add_parser('run', help='run on real HW')
     parser_run.add_argument('config', nargs='+', help='configuration file')
     parser_run.add_argument('--note', help='add description')
+    parser_run.add_argument('--duration', help='run navigation for given number of seconds', type=int)
 
     parser_replay = subparsers.add_parser('replay', help='replay from logfile')
     parser_replay.add_argument('logfile', help='recorded log file')
     parser_replay.add_argument('--force', '-F', dest='force', action='store_true', help='force replay even for failing output asserts')
     parser_replay.add_argument('--config', nargs='+', help='force alternative configuration file')
+    parser_replay.add_argument('--duration', help='run navigation for given number of seconds', type=int)  # TODO parse from cmd line
     args = parser.parse_args()
+
+    if args.duration is not None:
+        TIMEOUT = timedelta(seconds=args.duration)
 
     if args.command == 'replay':
         from replay import replay
