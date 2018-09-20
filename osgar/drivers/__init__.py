71ab4ff45155c224faa0a40d92afed7080893ad4
Martin Dlouhy <md@robotika.cz>
2018-09-20 21:06:56 +0200

    Add HTTP driver for IP camera

--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -4,11 +4,12 @@ from .spider import Spider
 from .logserial import LogSerial
 from .canserial import CANSerial
 from .simulator import SpiderSimulator
-from .logsocket import LogTCP, LogUDP
+from .logsocket import LogTCP, LogUDP, LogHTTP
 from .sicklidar import SICKLidar
 
 # dictionary of all available drivers
 all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial,
                    can=CANSerial, simulator=SpiderSimulator,
-                   tcp=LogTCP, udp=LogUDP, lidar=SICKLidar)
+                   tcp=LogTCP, udp=LogUDP, http=LogHTTP,
+                   lidar=SICKLidar)
 
