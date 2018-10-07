794c9335bbf08d02341a0daf84a2cb757fef3a49
Martin Dlouhy <md@robotika.cz>
2018-10-07 15:17:18 +0200

    scan_feature.py - draw scan and connected groups

--- osgar/lib/scan_feature.py
+++ osgar/lib/scan_feature.py
@@ -1,19 +1,62 @@
 """
-   Laser Feature extractor
+   Laser Feature Extractor
 """
+import math
+
+def scan_split(scan, max_diff, min_len=10):
+    arr = []
+    prev = scan[0]
+    start_i = 0
+    for i, dist in enumerate(scan[1:], start=1):
+        diff = abs(dist - prev)
+        if diff > max_diff:
+            if i - start_i >= min_len:
+                arr.append((start_i, i-1))
+            start_i = i
+        prev = dist
+    arr.append((start_i, i))
+    return arr
 
 
 def extract_features(scan):
-    pass
+    prev = None
+    arr = []
+    for i in range(len(scan)):
+        dist = scan[i]
+        if prev is not None:
+            diff = abs(dist - prev)
+            arr.append(diff)
+        prev = dist
+    return arr
+
+
+def draw_xy(scan, pairs):
+    step_angle = math.radians(1/3)
+    arr_x, arr_y = [], []
+    for i, dist in enumerate(scan):
+        angle = (i - len(scan)//2) * step_angle
+        x, y = math.cos(angle) * dist, math.sin(angle) * dist
+        arr_x.append(x)
+        arr_y.append(y)
+    plt.plot(arr_x, arr_y, '.', linewidth=2)
+
+    for f, t in pairs:
+        plt.plot([arr_x[f], arr_x[t]], [arr_y[f], arr_y[t]], 'o-', linewidth=2)
+
+    plt.axis('equal')
+    plt.show()
 
 
 if __name__ == "__main__":
     import argparse
+    import matplotlib.pyplot as plt
     from osgar.logger import LogReader, lookup_stream_id
     from osgar.lib.serialize import deserialize
 
     parser = argparse.ArgumentParser(description='Extract features in laser scan')
     parser.add_argument('filename', help='input log file')
+    parser.add_argument('--verbose', '-v', help="verbose mode", action='store_true')
+    parser.add_argument('--draw', '-d', help="draw result", action='store_true')
     args = parser.parse_args()
 
     filename = args.filename
@@ -24,10 +67,17 @@ if __name__ == "__main__":
             if ind < index:
                 continue
             timestamp, stream_id, data = row
-            data = deserialize(data)
-            print(extract_features(data))
+            scan = deserialize(data)
+            pairs = scan_split(scan[135:-135], max_diff=20)
+            if args.verbose:
+                for f, t in pairs:
+                    print(f, t)
+                    print(scan[135+f:135+t])
+            pairs = [(f+135, t+135) for f, t in pairs]
+            if args.draw:
+                draw_xy(scan, pairs)
             break
-    
+
 
 # vim: expandtab sw=4 ts=4
 
--- osgar/lib/test_scan_feature.py
+++ osgar/lib/test_scan_feature.py
@@ -1,12 +1,19 @@
 import unittest
 
-from osgar.lib.scan_feature import extract_features
+from osgar.lib.scan_feature import extract_features, scan_split
 
 
 class ScanFeatureTest(unittest.TestCase):
 
-    def test_extract_feature(self):
+    def Xtest_extract_feature(self):
         scan = [0]*811
         self.assertIsNone(extract_features(scan))
+
+    def test_scan_split(self):
+        scan = [1000]*180
+        self.assertEqual(scan_split(scan, max_diff=15), [(0, 179)])
+        box = [700]*30
+        self.assertEqual(scan_split(scan+box+scan, max_diff=15), 
+                [(0, 179), (180, 209), (210, 389)])
 
 # vim: expandtab sw=4 ts=4
