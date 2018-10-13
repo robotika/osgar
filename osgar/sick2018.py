f9b87be16f3fd97f02c9597547245930c054a4cf
Martin Dlouhy <md@robotika.cz>
2018-10-13 15:12:15 +0200

    SICK - game2, corrections 4.5 for side difference

--- osgar/sick2018.py
+++ osgar/sick2018.py
@@ -167,7 +167,7 @@ class SICKRobot2018:
         print(self.time, "drop ball END")
 
     def ver0(self):
-        DIST_MAG = 1.32  # distance from magnets to the center of transporter when closest
+        DIST_MAG = 1.32 - 0.045  # distance from magnets to the center of transporter when closest
         print(self.time, '=== ver0 ===')
         self.wait_for_start()
         game_start = self.time
