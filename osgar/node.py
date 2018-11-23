619c650eba0338cb7255b4bce362e7501c9c4da8
Martin Dlouhy <md@robotika.cz>
2018-11-23 06:07:32 +0100

    Provide class Node
    Use bus internally and wrap also publish, listen, sleep and is_alive
    functions. The update() by default sets variables by received channel
    name.

---
+++ osgar/node.py
@@ -0,0 +1,46 @@
+"""
+  Node - processing unit
+"""
+from threading import Thread
+
+from osgar.bus import BusShutdownException
+
+
+class Node(Thread):
+    """
+       Parent classs for all processing "nodes".
+    """
+    def __init__(self, config, bus):
+        super().__init__()
+        self.setDaemon(True)
+        self.bus = bus
+        self.time = None
+
+    def publish(self, channel, data):
+        return self.bus.publish(channel, data)
+
+    def listen(self):
+        return self.bus.listen()
+
+    def sleep(self, secs):
+        self.bus.sleep(secs)
+
+    def is_alive(self):
+        return self.bus.is_alive()
+
+    def update(self):
+        timestamp, channel, data = self.bus.listen()
+        self.time = timestamp
+        setattr(self, channel, data)
+
+    def run(self):
+        try:
+            while True:
+                self.update()
+        except BusShutdownException:
+            pass
+
+    def request_stop(self):
+        self.bus.shutdown()
+
+# vim: expandtab sw=4 ts=4
