68c50f3b617b5d9ecd0ec0bfdfb642a89d8536ab
Martin Dlouhy <md@robotika.cz>
2018-11-25 19:54:25 +0100

    Extend TCP driver for "server" and "dynamic" option
    Needed for ROSProxy.

--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -4,14 +4,16 @@ from .spider import Spider
 from .logserial import LogSerial
 from .canserial import CANSerial
 from .simulator import SpiderSimulator
-from .logsocket import LogTCP, LogUDP, LogHTTP
+from .logsocket import (LogTCPStaticIP, LogTCPDynamicIP, LogTCPServer,
+                        LogUDP, LogHTTP)
 from .sicklidar import SICKLidar
 from .eduro import Eduro
 
 # dictionary of all available drivers
 all_drivers = dict(gps=GPS, imu=IMU, spider=Spider, serial=LogSerial,
                    can=CANSerial, simulator=SpiderSimulator,
-                   tcp=LogTCP, udp=LogUDP, http=LogHTTP,
+                   tcp=LogTCPStaticIP, tcpdynamic=LogTCPDynamicIP,
+                   tcpserver=LogTCPServer, udp=LogUDP, http=LogHTTP,
                    lidar=SICKLidar,
                    eduro=Eduro)
 
