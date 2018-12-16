9a75e7f259526044418a17a6e8c98904cd7916da
Martin Dlouhy <md@robotika.cz>
2018-12-16 07:03:46 +0100

    subt.py - copy & paste go_straight and turn functions

--- examples/subt/subt.py
+++ examples/subt/subt.py
@@ -1,5 +1,5 @@
 """
-  SubT Challenge Version 0
+  SubT Challenge Version 1
 """
 import sys
 import math
@@ -9,28 +9,76 @@ from osgar.lib.config import load as config_load
 from osgar.record import Recorder
 
 
-class SubTVer0:
+def min_dist(laser_data):
+    if len(laser_data) > 0:
+        # remove ultra near reflections and unlimited values == 0
+        laser_data = [x if x > 10 else 10000 for x in laser_data]
+        return min(laser_data)/1000.0
+    return 0
+
+
+def distance(pose1, pose2):
+    return math.hypot(pose1[0] - pose2[0], pose1[1] - pose2[1])
+
+
+class SubTChallenge:
     def __init__(self, config, bus):
         self.bus = bus
         self.start_pose = None
         self.traveled_dist = 0.0
         self.time = None
+        self.max_speed = 1.0  # TODO load from config
+        self.max_angular_speed = math.radians(45)
+
+        self.last_position = (0, 0, 0)  # proper should be None, but we really start from zero
 
     def send_speed_cmd(self, speed, angular_speed):
         return self.bus.publish('desired_speed', [round(speed*1000), round(math.degrees(angular_speed)*100)])
 
+    def go_straight(self, how_far):
+        print(self.time, "go_straight %.1f" % how_far, self.last_position)
+        start_pose = self.last_position
+        if how_far >= 0:
+            self.send_speed_cmd(self.max_speed, 0.0)
+        else:
+            self.send_speed_cmd(-self.max_speed, 0.0)
+        while distance(start_pose, self.last_position) < abs(how_far):
+            self.update()
+        self.send_speed_cmd(0.0, 0.0)
+
+    def turn(self, angle, with_stop=True):
+        print(self.time, "turn %.1f" % math.degrees(angle))
+        start_pose = self.last_position
+        if angle >= 0:
+            self.send_speed_cmd(0.0, self.max_angular_speed)
+        else:
+            self.send_speed_cmd(0.0, -self.max_angular_speed)
+        while abs(start_pose[2] - self.last_position[2]) < abs(angle):
+            self.update()
+        if with_stop:
+            self.send_speed_cmd(0.0, 0.0)
+            start_time = self.time
+            while self.time - start_time < timedelta(seconds=2):
+                self.update()
+                if not self.is_moving:
+                    break
+            print(self.time, 'stop at', self.time - start_time)
+
     def update(self):
         packet = self.bus.listen()
         if packet is not None:
-            print('Go1m', packet)
+#            print('SubT', packet)
             timestamp, channel, data = packet
             self.time = timestamp
             if channel == 'pose2d':
                 x, y, heading = data
                 pose = (x/1000.0, y/1000.0, math.radians(heading/100.0))
+                self.last_position = pose
                 if self.start_pose is None:
                     self.start_pose = pose
                 self.traveled_dist = math.hypot(pose[0] - self.start_pose[0], pose[1] - self.start_pose[1])
+            elif channel == 'scan':
+                print('min_dist', min_dist(data))
                         
     def wait(self, dt):  # TODO refactor to some common class
         if self.time is None:
@@ -40,11 +88,8 @@ class SubTVer0:
             self.update()
 
     def play(self):
-        print("Go One Meter!")
-        self.send_speed_cmd(1.0, 0.0)
-        while self.traveled_dist < 10.0:
-            self.update()
-        self.send_speed_cmd(0.0, 0.0)
+        print("SubT Challenge Ver1!")
+        self.go_straight(5.0)  # go to the tunnel entrance
         self.wait(timedelta(seconds=1))
 
     def start(self):
@@ -61,7 +106,7 @@ if __name__ == "__main__":
     from osgar.logger import LogWriter, LogReader
     import argparse
 
-    parser = argparse.ArgumentParser(description='Go One Meter')
+    parser = argparse.ArgumentParser(description='SubT Challenge')
     subparsers = parser.add_subparsers(help='sub-command help', dest='command')
     subparsers.required = True
     parser_run = subparsers.add_parser('run', help='run on real HW')
@@ -77,14 +122,14 @@ if __name__ == "__main__":
     if args.command == 'replay':
         from osgar.replay import replay
         args.module = 'app'
-        game = replay(args, application=SubTVer0)
+        game = replay(args, application=SubTChallenge)
         game.play()
 
     elif args.command == 'run':
         log = LogWriter(prefix='go1m-', note=str(sys.argv))
         config = config_load(*args.config)
         log.write(0, bytes(str(config), 'ascii'))  # write configuration
-        robot = Recorder(config=config['robot'], logger=log, application=SubTVer0)
+        robot = Recorder(config=config['robot'], logger=log, application=SubTChallenge)
         game = robot.modules['app']  # TODO nicer reference
         robot.start()
         game.play()
