88a2194fd7d835117fc56c291fd30add812bd1e0
Martin Dlouhy <md@robotika.cz>
2018-12-19 13:18:45 +0100

    subt.py - increase simulation time to 3hours

--- examples/subt/subt.py
+++ examples/subt/subt.py
@@ -65,7 +65,7 @@ class SubTChallenge:
                     break
             print(self.time, 'stop at', self.time - start_time)
 
-    def follow_wall(self, radius, timeout=timedelta(minutes=50)):
+    def follow_wall(self, radius, timeout=timedelta(hours=3)):
         start_time = self.time
         desired_speed = 1.0
         while self.time - start_time < timeout:
