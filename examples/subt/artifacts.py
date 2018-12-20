7c8aaa0a4e3025a50227313948c690b4be2480a3
Martin Dlouhy <md@robotika.cz>
2018-12-20 17:49:23 +0100

    artifacts.py - add simple red pixel detector (slow, brute naive force)

--- examples/subt/artifacts.py
+++ examples/subt/artifacts.py
@@ -7,16 +7,28 @@ import numpy as np
 
 from osgar.node import Node
 
+def count_red(img):
+    count = 0
+    for x in range(320):
+        for y in range(240):
+            b, g, r = img[y][x]
+            if r > 100 and r > 2 * g and r > 2 * b:
+                count += 1
+    return count
+
 
 class ArtifactDetector(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
 
-    def update(self):  # hack, this method should be called run instead!       
+    def update(self):  # hack, this method should be called run instead!
         channel = super().update()  # define self.time
         assert channel == "image", channel
         img = cv2.imdecode(np.fromstring(self.image, dtype=np.uint8), 1)
-        print(self.time, img.shape)
+        count = count_red(img)
+        print(self.time, img.shape, count)
+        if count > 100:
+            self.publish('artf', count)
 
 # vim: expandtab sw=4 ts=4
 
