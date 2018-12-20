dd8c2f724f0ad22efc239ae8028ce4ba68e9cf66
Martin Dlouhy <md@robotika.cz>
2018-12-20 06:55:36 +0100

    Add dummy ArtifactDetector

---
+++ examples/subt/artifacts.py
@@ -0,0 +1,22 @@
+"""
+  Detect SubT Artifact in Camera Image
+"""
+
+import cv2
+import numpy as np
+
+from osgar.node import Node
+
+
+class ArtifactDetector(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+
+    def update(self):  # hack, this method should be called run instead!       
+        channel = super().update()  # define self.time
+        assert channel == "image", channel
+        img = cv2.imdecode(np.fromstring(buf, dtype=np.uint8), 1)
+        print(self.time, img.shape)
+
+# vim: expandtab sw=4 ts=4
+
