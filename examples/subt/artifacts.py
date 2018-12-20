5bf8d7e1e8b2dd51faf493c77e2ac723ce69e811
Martin Dlouhy <md@robotika.cz>
2018-12-20 19:42:42 +0100

    artifacts.py - optimization for red pixel detection

--- examples/subt/artifacts.py
+++ examples/subt/artifacts.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from osgar.node import Node
 
-def count_red(img):
+def old_count_red(img):
     count = 0
     for x in range(320):
         for y in range(240):
@@ -15,6 +15,15 @@ def count_red(img):
             if r > 100 and r > 2 * g and r > 2 * b:
                 count += 1
     return count
+
+
+def count_red(img):
+    b = img[:,:,0]
+    g = img[:,:,1]
+    r = img[:,:,2]
+    mask = np.logical_and(r > 100, np.logical_and(r/2 > g, r/2 > b))
+    img[mask] = 0, 255, 0
+    return mask.sum()
 
 
 class ArtifactDetector(Node):
