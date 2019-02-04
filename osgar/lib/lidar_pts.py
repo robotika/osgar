f049c0d6f576d85f2c05c3a00b81cf4c03c8c6e4
Martin Dlouhy <md@robotika.cz>
2019-02-04 07:18:47 +0100

    dual_convex_hull experiment

--- osgar/lib/lidar_pts.py
+++ osgar/lib/lidar_pts.py
@@ -35,4 +35,30 @@ def convex_hull(scan, debug_poly=None):
         debug_poly[-1].append(hull[0][0])  # close polygon
 
 
+def dual_convex_hull(scan, debug_poly=None):
+    """
+    Compute convex hull for left and right side of the scan
+    (usefull if already in tunnel)
+    """
+    heading = 0.0  # TODO
+    pts_left, pts_right = [], []
+    for i, i_dist in enumerate(scan):
+        if i_dist == 0 or i_dist >= 10000:
+            continue
+        angle = math.radians(270 * (i / len(scan)) - 135) + heading
+        dist = i_dist/1000.0
+        x, y = dist * math.cos(angle), dist * math.sin(angle)
+        if angle >= 0:  # beware of heading modification, TODO autodetect
+            pts_left.append((x, y))
+        else:
+            pts_right.append((x, y))
+
+    hull_left = cv2.convexHull(np.array(pts_left, dtype=np.float32))
+    hull_right = cv2.convexHull(np.array(pts_right, dtype=np.float32))
+    if debug_poly is not None:
+        debug_poly.append([p[0] for p in hull_left])
+        debug_poly[-1].append(hull_left[0][0])  # close polygon
+        debug_poly.append([p[0] for p in hull_right])
+        debug_poly[-1].append(hull_right[0][0])  # close polygon
+
 # vim: expandtab sw=4 ts=4
