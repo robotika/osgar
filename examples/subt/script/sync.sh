2c359be5b079db9161ef8bcb6457127773049952
Martin Dlouhy <md@robotika.cz>
2019-02-18 21:06:56 +0100

    Add examples/subt/script/sync.sh

---
+++ examples/subt/script/sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp osgar/examples/subt/src/subt_example_node.cc src/subt/subt_example/src/subt_example_node.cc
+cp osgar/examples/subt/launch/x2lr_team.launch src/subt/subt_example/launch/x2lr_team.launch
+cp osgar/examples/subt/config/robot_config.yaml src/subt/subt_example/config/robot_config.yaml
+
+catkin_make install
+
