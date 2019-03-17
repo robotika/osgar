7b62b893aa51d549bca57597b98ab13ea35d6c76
Zbynek Winkler <zw@robotika.cz>
2019-03-17 21:25:41 +0100

    Switch cortexpilot from 0xC to 0xD packet.

--- osgar/drivers/test_cortexpilot.py
+++ osgar/drivers/test_cortexpilot.py
@@ -4,35 +4,24 @@ from unittest.mock import MagicMock, call
 from osgar.drivers.cortexpilot import Cortexpilot
 from osgar.bus import BusHandler
 
-
-SAMPLE_DATA = (b'\x00\x02,\x01\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x16' +
-    b'\xdd\x13>\x0f@>\xbcT\x17j=\x91\xeb|\xbf\xaaW#C\xef\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x19')
+SAMPLE_DATA = bytes.fromhex(
+        '000259010d0000000040952c416666be410000000000000000000000800000000000000' +
+        '00000006fe702008013464840dd6e1db143f60900805f43ed44413d63e3ac3c3386edbb' +
+        'aeb17f3f5d663943008037440000c040002079461ab754bd9c137d3cff149dbbf37345b' +
+        'ed29ec8bbb763eabebf8304000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '00000000000000000000000000000000000000000000000000000000000000000000000' +
+        '000000000000000000000000000000000000000000000000000000000000000000000047')
 
 
 class CortextpilotTest(unittest.TestCase):
@@ -67,7 +56,7 @@ class CortextpilotTest(unittest.TestCase):
 
         self.assertEqual(q.put.call_args_list, [
             call((135, 'raw', b'\x00\x00\x03\x01\x01\xfb')),  # request version
-            call((135, 'raw', b'\x00\x00\x0f\x01\x0c\x00\x00\x00\x00\x00\x00\x00\x80@\x00\x00\x00$')) # cmd
+            call((135, 'raw', bytes.fromhex('00000f010d00000000000000804000000023'))) # cmd
             ])
 
     def test_create_packet(self):
@@ -75,14 +64,14 @@ class CortextpilotTest(unittest.TestCase):
         packet = robot.create_packet()
         self.assertEqual(len(packet), 3 + 15)
         self.assertEqual(sum(packet) % 256, 0)
-        self.assertEqual(packet[-1], 0x24)
+        self.assertEqual(packet[-1], 0x23)
 
         robot.desired_speed = 0.5
         robot.lidar_valid = True  # otherwise the speed will be reset to zero
         packet = robot.create_packet()
         self.assertEqual(len(packet), 3 + 15)
         self.assertEqual(sum(packet) % 256, 0)
-        self.assertEqual(packet[-1], 0x82)
+        self.assertEqual(packet[-1], 0x81)
 
         # test packet checksum
         robot.desired_speed = -0.12314114151
@@ -94,12 +83,10 @@ class CortextpilotTest(unittest.TestCase):
         self.assertIsNone(packet)
 
         robot._buf = SAMPLE_DATA
-        self.assertEqual(len(robot._buf), 559)
-
         packet = robot.get_packet()
         self.assertIsNotNone(packet)
-        self.assertEqual(len(packet), 512 + 44 + 3)
-        self.assertEqual(len(robot._buf), 559 - 512 - 44 - 3)
+        self.assertEqual(len(packet), len(SAMPLE_DATA))
+        self.assertEqual(len(robot._buf), 0)
 
         packet = robot.get_packet()
         self.assertIsNone(packet)
@@ -108,7 +95,7 @@ class CortextpilotTest(unittest.TestCase):
         robot = Cortexpilot(config={}, bus=MagicMock())
         robot.parse_packet(SAMPLE_DATA)
         self.assertEqual(robot.flags, 0x0)
-        self.assertAlmostEqual(robot.voltage, 0.0)
+        self.assertAlmostEqual(robot.voltage, 10.78643798)
 
         # trigger pose update
         robot.parse_packet(SAMPLE_DATA)
