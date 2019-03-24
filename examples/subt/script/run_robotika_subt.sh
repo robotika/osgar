3527623713abd7e4149ac9ecfded72bced267a85
Zbynek Winkler <zw@robotika.cz>
2019-03-24 20:46:40 +0100

    Tweaks in scripts (run, sync).
    Replace sleeps in run-script with detection.
    Run simulation headless by default.
    Add extra output when evaluating score for each article.
    Add launch file to optionally launch gui (gzclient).
    Call /subt/finish at the end.

--- examples/subt/script/run_robotika_subt.sh
+++ examples/subt/script/run_robotika_subt.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
 # Cleanup
-#rm -rf ~/.gazebo/log/*
 rm -f osgar/examples/subt/call_base.txt
 rm -f osgar/examples/subt/call_base_x2l.txt
 rm -f osgar/examples/subt/call_base_x2r.txt
 source ~/subt_ws/install/setup.sh
 
-# Start gazebo.
+echo Starting gazebo....
 export DISPLAY=:0  # Even when connected through ssh, Gazebo needs a local window.
 roslaunch subt_gazebo competition.launch scenario:=tunnel_qual extra_gazebo_args:="-r --record_period 0.0167 --record_filter *.pose/*.pose" &
 GAZEBO_PID=$!
-sleep 20
+# wait for gazebo to fully load
+while true; do
+	rostopic type /subt/score >/dev/null 2>/dev/null && break
+	sleep 1
+done
+
+sleep 1
 
 # Set up robots.
 X2_SENSOR_CONFIG_4=1 DISPLAY=:0 roslaunch subt_example x2lr_team.launch &
 ROBOTS_SIM_PID=$!
-sleep 25
+while true; do
+	x2r=$(rostopic list | grep X2R | wc -l)
+	x2l=$(rostopic list | grep X2L | wc -l)
+	if [ $x2l -ne 0 -a $x2l -eq $x2r ]; then
+	        break
+        else
+		echo "still waiting for robots to be loaded..."
+		sleep 1
+	fi
+done
+
+sleep 1
 
 # Control the robots.
+export OSGAR_LOGS=$(pwd)/osgar/examples/subt/logs
+mkdir $OSGAR_LOGS
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/osgar
 cd osgar/examples/subt
 python3 subt.py run subt-x2-left.json &
@@ -52,14 +70,10 @@ rosservice call '/X2R/center_right_headlight/enable' true
 wait ${ROBOT_LEFT_PID}
 wait ${ROBOT_RIGHT_PID}
 
-# Get some time for final reporting.
-sleep 10
+rosservice call '/subt/finish' true
 
 # Take robot simulation down.
 kill ${ROBOTS_SIM_PID}
 kill ${GAZEBO_PID}
 wait
-
-# Convert the log.
-#time gz log -f ~/.gazebo/log/*/gzserver/state.log --filter *.pose/*.pose -z 60 -o ~/.gazebo/log/subt_tunnel_qual_sim_state.log
 
