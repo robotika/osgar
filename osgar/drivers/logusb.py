ae588b947e9ea2c43e066f3bcde6b954d8d34167
Zbynek Winkler <zw@robotika.cz>
2019-03-26 20:37:40 +0100

    Use a single thread in logusb.

--- osgar/drivers/logusb.py
+++ osgar/drivers/logusb.py
@@ -14,8 +14,7 @@ from osgar.bus import BusShutdownException
 
 class LogUSB:
     def __init__(self, config, bus):
-        self.input_thread = Thread(target=self.run_input, daemon=True)
-        self.output_thread = Thread(target=self.run_output, daemon=True)
+        self.thread = Thread(target=self.run, daemon=True)
 
         # TODO make configurable for other devices then SICK lidar TIM310
         self.dev = usb.core.find(idVendor=0x19A2, idProduct=0x5001)
@@ -30,24 +29,22 @@ class LogUSB:
         self.bus = bus
 
     def start(self):
-        self.input_thread.start()
-        self.output_thread.start()
+        self.thread.start()
 
     def join(self, timeout=None):
-        self.input_thread.join(timeout=timeout)
-        self.output_thread.join(timeout=timeout)
+        self.thread.join(timeout=timeout)
 
-    def run_input(self):
-        while self.bus.is_alive():
-            data = self.dev.read(1|usb.ENDPOINT_IN, 65535, timeout = 100)
-            if len(data) > 0:
-                self.bus.publish('raw', bytes(data))
-
-    def run_output(self):
+    def run(self):
         try:
             while True:
                 __, __, data = self.bus.listen()
-                self.dev.write(2|usb.ENDPOINT_OUT, data, 0)
+                try:
+                    self.dev.write(2|usb.ENDPOINT_OUT, data, 0)
+                    data = self.dev.read(1|usb.ENDPOINT_IN, 65535, timeout = 100)
+                    self.bus.publish('raw', bytes(data))
+                except usb.core.USBError as error:
+                    print(error)
+                    self.bus.publish('raw', bytes())
         except BusShutdownException:
             pass
 
