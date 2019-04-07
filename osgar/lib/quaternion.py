b12007f63c71ffb7f0347a775ba727978adfd3e7
Zbynek Winkler <zw@robotika.cz>
2019-04-07 11:59:53 -0600

    cortexpilot.py: identity quat to east instead of north.

---
+++ osgar/lib/quaternion.py
@@ -0,0 +1,13 @@
+
+def multiply(quaternion1, quaternion0):
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return [-x1*x0 - y1*y0 - z1*z0 + w1*w0,
+             x1*w0 + y1*z0 - z1*y0 + w1*x0,
+            -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+             x1*y0 - y1*x0 + z1*w0 + w1*z0]
+
+def conjugate(quaternion):
+    w, x, y, z = quaternion
+    return [w, -x, -y, -z]
+
