d7f03dbc5a70f9201c9f4bc01b27fcce3a4c2e6a
Martin Dlouhy <md@robotika.cz>
2019-04-07 21:19:00 -0600

    Add test_quaternion.py

---
+++ osgar/lib/test_quaternion.py
@@ -0,0 +1,21 @@
+import unittest
+import math
+
+from . import quaternion
+
+
+class QuaternionTest(unittest.TestCase):
+
+    def test_multiply(self):
+        qa = [1, 0, 0, 0]
+        qb = [0, 1, 0, 0]
+        qc = quaternion.multiply(qa, qb)
+        self.assertEqual(qc, qb)  # identity
+
+        qd = quaternion.multiply(qb, qb)
+        self.assertEqual(qd, [-1, 0, 0, 0])  # twice rotation by 90deg
+
+        qdd = quaternion.multiply(qd, qd)
+        self.assertEqual(qdd, qa)  # identity
+
+# vim: expandtab sw=4 ts=4
