5ad1383e8c634c7d6463639569eae6e6e185e5a1
Martin Dlouhy <md@robotika.cz>
2019-07-11 22:15:07 +0200

    compass_calib.py - hack pyplot drawing

--- osgar/compass_calib.py
+++ osgar/compass_calib.py
@@ -5,6 +5,8 @@
 import argparse
 import struct
 
+import matplotlib.pyplot as plt
+
 from osgar.logger import LogReader, lookup_stream_id
 from osgar.lib.serialize import deserialize
 
@@ -19,6 +21,7 @@ def compass_calib(filename):
     val = 0x8000
     minx, maxx, miny, maxy = val, -val, val, -val
 
+    record = []
     with LogReader(filename) as log:
         for timestamp, __, data in log.read_gen(stream_id):
             data = deserialize(data)
@@ -26,10 +29,17 @@ def compass_calib(filename):
             if addr == COMPASS_ADDR:
                 assert len(arr) == 6, arr
                 x, z, y = struct.unpack('>hhh', bytes(arr))  # axis Y and Z swapped in orig
+                record.append((x, y, z))
                 minx = min(minx, x)
                 maxx = max(maxx, x)
                 miny = min(miny, y)
                 maxy = max(maxy, y)
+
+    arr = record
+    x = [x for (x, _, _) in arr]
+    y = [y for (_, y, _) in arr]
+    plt.plot(x, y, 'o-', linewidth=2)
+    
     return (minx+maxx)//2, (miny+maxy)//2, [minx, maxx, miny, maxy]
 
 if __name__ == "__main__":
@@ -38,6 +48,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print(compass_calib(args.logfile))
+    plt.show()
 
 
 # vim: expandtab sw=4 ts=4
