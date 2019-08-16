2f5fa1af882c228300d8649c94f70fc6a29a657f
Martin Dlouhy <md@robotika.cz>
2019-08-16 09:09:49 -0400

    report_artf.py - update comments and add extra sleep
    This is version without authorization token.

--- subt/report_artf.py
+++ subt/report_artf.py
@@ -1,18 +1,20 @@
 """
    Report artifact for STIX in Colorado April 2019
+         reused for Pittsburgh August 2019
 """
 import requests
+import time
 
 
 #URL_BASE = "http://localhost:8000"  # demo
-URL_BASE = "http://10.100.2.200:8000"  # Miami Tunnel
-#URL_BASE = "http://10.100.1.200:8000"  # Army Tunnel
+URL_BASE = "http://10.100.1.200:8000"  # Safety Research (was Army) Tunnel
+#URL_BASE = "http://10.100.2.200:8000"  # Experimental (was Miami) Tunnel
 
 ARTF_TYPES = ['Survivor', 'Backpack', 'Cell Phone', 'Drill', 'Fire Extinguisher']
 
 json_headers = {
 #    "Authorization" : "Bearer subttesttoken123",  # demo
-    "Authorization" : "Bearer NfEwAHEYsKqQkxSf",
+#    "Authorization" : "Bearer NfEwAHEYsKqQkxSf",  # STIX
     "Content-Type" : "application/json",
 }
 
@@ -23,8 +25,9 @@ def get_status():
 
     # Correct GET /api/status/ request
     response = requests.get(url, headers=json_headers)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.status_code
     print(response.content)
+    print("-------------------")
 
 
 def report_artf(artf_type, x, y, z):
@@ -39,8 +42,9 @@ def report_artf(artf_type, x, y, z):
 
     # Correct POST /api/artifact_reports/ request
     response = requests.post(url, json=artifact_report_data, headers=json_headers)
-    assert response.status_code == 201
     print(response.content)
+    assert response.status_code == 201, response.status_code
+    print("-------------------")
 
 
 if __name__ == '__main__':
@@ -54,7 +58,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     get_status()
+    time.sleep(2)
     report_artf(args.artf_type, args.x, args.y, args.z)
+    time.sleep(2)
     get_status()
 
 # vim: expandtab sw=4 ts=4
