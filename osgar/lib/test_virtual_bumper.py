c8ea85483c51ad5d56adcb5401675f1b04711dc2
Martin Dlouhy <md@robotika.cz>
2019-08-19 13:40:57 -0400

    virtual_bumper.py - fix bug with irregular set speed update

--- osgar/lib/test_virtual_bumper.py
+++ osgar/lib/test_virtual_bumper.py
@@ -7,13 +7,13 @@ from osgar.lib.virtual_bumper import VirtualBumper
 
 class VirtualBumperTest(unittest.TestCase):    
     def test_usage(self):
-        vb = VirtualBumper(timedelta(seconds=3))
+        vb = VirtualBumper(timedelta(seconds=2.5))
         t0 = timedelta(seconds=12)
         t_step = timedelta(seconds=1)
         pose = [1.0, 2.0, math.pi]
-        vb.update_pose(t0, pose)
         vb.update_desired_speed(0.5, 0.0)
         self.assertFalse(vb.collision())
+        vb.update_pose(t0, pose)
         vb.update_pose(t0 + t_step, pose)
         vb.update_pose(t0 + 2*t_step, pose)
         vb.update_pose(t0 + 3*t_step, pose)
@@ -59,5 +59,18 @@ class VirtualBumperTest(unittest.TestCase):
         for i, pose in enumerate(poses):
             vb.update_pose(t0 + i*t_step, pose)
         self.assertTrue(vb.collision())
+
+    def test_false_trigger_via_lora_pause(self):
+        # bug shown during LoRa command "Pause"
+        vb = VirtualBumper(timedelta(seconds=2))
+        vb.update_desired_speed(0.5, 0.0)
+        vb.update_pose(timedelta(seconds=2.479321), (0.0, 0.0, 0.0))
+        vb.update_pose(timedelta(seconds=27.612698), (2.503, -0.013, -0.00977))
+        vb.update_desired_speed(0.0, 0.0)
+        vb.update_pose(timedelta(seconds=27.767734), (2.507, -0.013, -0.0095993))
+        vb.update_pose(timedelta(seconds=30.458526), (2.525, -0.014, -0.0076794487))
+        vb.update_pose(timedelta(seconds=37.808329), (2.525, -0.014, -0.0089011791851))
+        self.assertFalse(vb.collision())
+
 
 # vim: expandtab sw=4 ts=4
--- osgar/lib/virtual_bumper.py
+++ osgar/lib/virtual_bumper.py
@@ -15,19 +15,25 @@ def equal_poses(pose1, pose2):
 class VirtualBumper:
     def __init__(self, stuck_time):
         self.stuck_time = stuck_time
-        self.should_be_moving = False
+        self.should_be_moving = False  # status based on desired speed commands
+        self.verbose = False
+        self.reset_counters()
+
+    def reset_counters(self):
         self.last_pose = None
-        self.last_pose_time = None
-        self.not_moving = None
+        self.last_pose_time = None  # time of last pose without motion
+        self.not_moving = None  # how long it is not moving?
 
     def update_desired_speed(self, desired_speed, desired_angular_speed):
         self.should_be_moving = abs(desired_speed) > 0.001 or abs(desired_angular_speed) > 0.001
+        if self.verbose:
+            print('VirtualBumper', desired_speed, desired_angular_speed, self.should_be_moving)
         if not self.should_be_moving:
-            self.last_pose = None
-            self.last_pose_time = None
-            self.not_moving = None
+            self.reset_counters()
 
     def update_pose(self, timestamp, pose):
+        if self.verbose:
+            print('VirtualBumper', timestamp, pose)
         if self.last_pose is not None:
             if equal_poses(pose, self.last_pose):
                 self.not_moving = timestamp - self.last_pose_time
@@ -37,6 +43,11 @@ class VirtualBumper:
         else:
             self.last_pose = pose
             self.last_pose_time = timestamp
+        if self.verbose:
+            print(self.last_pose, self.last_pose_time, self.not_moving, self.stuck_time)
+        # if it should not be moving reset all counters
+        if not self.should_be_moving:
+            self.reset_counters()
 
     def collision(self):
         if self.not_moving is None:
