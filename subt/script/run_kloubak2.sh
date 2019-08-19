21f91e47e357180518398b6d86ca77681a3921ab
Jakub <jlev@tf.czu.cz>
2019-08-20 01:03:02 +0200

    run_kloubak2.sh - modified path to configs

--- subt/script/run_kloubak2.sh
+++ subt/script/run_kloubak2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python -m osgar.record ../../config/test-vesc.json &
+python -m osgar.record config/test-vesc.json &
 VESC_PID=$!
 
-python -m subt run ../../config/kloubak2-subt-estop-lora.json --side right --speed 0.3 --timeout 20
+python -m subt run config/kloubak2-subt-estop-lora.json --side right --speed 0.7 --timeout 20
 
 kill ${VESC_PID}
 wait
