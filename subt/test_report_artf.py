7ad3109121c05e072076ce9d8d4205f67ed92141
Martin Dlouhy <md@robotika.cz>
2019-08-19 20:50:47 -0400

    report_artf.py - call multiple artifact reports

---
+++ subt/test_report_artf.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import math
+
+from subt.report_artf import get_status, report_artf, triple, score
+
+
+class ReportArtfTest(unittest.TestCase):
+
+    def test_triple(self):
+        arr = triple(0, 0, 0)
+        self.assertEqual(len(arr), 3)
+        self.assertEqual(arr[0], (4, 0, 0))
+        self.assertAlmostEqual(arr[1][0], -2)
+        self.assertAlmostEqual(arr[1][1], math.sqrt(12))
+        self.assertAlmostEqual(arr[2][0], -2)
+        self.assertAlmostEqual(arr[2][1], -math.sqrt(12))
+
+        arr = triple(10, 0, 0)
+        self.assertEqual(len(arr), 3)
+        self.assertEqual(arr[0], (14, 0, 0))
+
+
+    def test_score(self):
+        with patch('subt.report_artf.get_status') as p, \
+             patch('subt.report_artf.report_artf') as p2, \
+             patch('time.sleep'):
+            p.side_effect = [
+                b'{"score":0,"remaining_reports":38,"current_team":"robotika","run_clock":983.1}',
+                b'{"score":1,"remaining_reports":37,"current_team":"robotika","run_clock":987.4}',
+            ]
+            self.assertTrue(score('Survivor', 0, 0, 0))
+
+            p.side_effect = [
+                b'{"score":0,"remaining_reports":39,"current_team":"robotika","run_clock":966.5}',
+                b'{"score":0,"remaining_reports":38,"current_team":"robotika","run_clock":970.8}',
+            ]
+            self.assertFalse(score('Backpack', 1, 2, 3))
+
+# vim: expandtab sw=4 ts=4
+
