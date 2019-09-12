841ec43c617974cfe020390662d1eb28f29e193c
Martin Dlouhy <md@robotika.cz>
2019-09-12 21:20:14 +0200

    sync.sh - FIXUP, and removing unused copy

--- subt/script/sync.sh
+++ subt/script/sync.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-cp osgar/examples/subt/src/subt_example_node.cc src/subt/subt_example/src/subt_example_node.cc
-#cp osgar/examples/subt/src/CMakeLists.txt src/subt/subt_example/CMakeLists.txt
-#cp osgar/examples/subt/launch/x2lr_team.launch src/subt/subt_example/launch/x2lr_team.launch
-#cp osgar/examples/subt/config/robot_config.yaml src/subt/subt_example/config/robot_config.yaml
-
-cp osgar/examples/subt/launch/x2lr_team.launch src/subt/subt_example/launch/example_robot.launch
+cp osgar/subt/src/subt_example_node.cc src/subt/subt_example/src/subt_example_node.cc
+cp osgar/subt/launch/example_robot.launch src/subt/subt_example/launch/example_robot.launch
 
 catkin_make install
 
