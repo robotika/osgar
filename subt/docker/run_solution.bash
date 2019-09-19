2b47b3c3dd2ebf7881472ffcec6e1c468ea44cef
Martin Dlouhy <md@robotika.cz>
2019-09-19 17:59:47 +0200

    run_solution.bash - guess to run our example

--- subt/docker/run_solution.bash
+++ subt/docker/run_solution.bash
@@ -7,4 +7,12 @@
 sleep 30
 
 # Run your solution.
-roslaunch subt_seed x1.launch
+#roslaunch subt_seed x1.launch
+
+roslaunch subt_example example_robot.launch &
+
+#export PYTHONPATH=${PYTHONPATH}:$(pwd)/osgar
+cd osgar
+
+python3 -m subt run subt/subt-x2.json --side left --walldist 2.0 --timeout 180 --note "try to visit artifact and return home"
+
