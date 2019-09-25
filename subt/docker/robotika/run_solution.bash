1148f0e7a0628869ffef497d855cd71d13241e47
Martin Dlouhy <md@robotika.cz>
2019-09-25 23:09:06 +0200

    run_solution.bash - wait_for_sensors.py

--- subt/docker/robotika/run_solution.bash
+++ subt/docker/robotika/run_solution.bash
@@ -8,9 +8,10 @@ sleep 30
 
 # Run your solution.
 roslaunch subt_seed x1.launch &
-sleep 10
 
 cd osgar
+
+python subt/wait_for_sensors.py
 
 python3 -m subt run subt/subt-x2.json --side left --walldist 0.75 --timeout 180 --note "try to visit artifact and return home" 2>&1 | python subt/std2ros.py
 
