36bcafc60fbe882d67961c0ecb4c7b28886a967d
Martin Dlouhy <md@robotika.cz>
2019-09-25 23:04:36 +0200

    wait_for_sensors.py - fix termination

--- subt/wait_for_sensors.py
+++ subt/wait_for_sensors.py
@@ -14,40 +14,45 @@ sensors = set()
 def callback_imu(data):
     global sensors
     if 'imu' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + "Imu")
+        rospy.loginfo(rospy.get_caller_id() + " Imu")
         sensors.add('imu')
 
 
 def callback_scan(data):
     global sensors
     if 'scan' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + "LidarScan")
+        rospy.loginfo(rospy.get_caller_id() + " LidarScan")
         sensors.add('scan')
 
 
 def callback_image(data):
     global sensors
     if 'image' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + "Image")
+        rospy.loginfo(rospy.get_caller_id() + " Image")
         sensors.add('image')
 
 
 def callback_odom(data):
     global sensors
     if 'odom' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + "Odom")
+        rospy.loginfo(rospy.get_caller_id() + " Odom")
         sensors.add('odom')
 
 
 def wait_for_sensors():
+    global sensors
     rospy.init_node('listener', anonymous=True)
     rospy.loginfo('-------------- BEGIN --------------')
     rospy.Subscriber('/X2/imu/data', Imu, callback_imu)
     rospy.Subscriber('/X2/front_scan', LaserScan, callback_scan)
     rospy.Subscriber('/X2/front/image_raw/compressed', CompressedImage, callback_image)
     rospy.Subscriber('/X2/odom', Odometry, callback_odom)
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        if len(sensors) >= 4:
+            break
+        r.sleep()
     rospy.loginfo('--------------- END ---------------')
-    rospy.spin()
 
 
 if __name__ == '__main__':
