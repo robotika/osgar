d070baa19ea15285ea7912d1ba343adf548c7ba2
Martin Dlouhy <md@robotika.cz>
2019-09-26 11:35:48 +0200

    wait_for_sensors.py - refactor + wait for battery_state

--- subt/wait_for_sensors.py
+++ subt/wait_for_sensors.py
@@ -2,57 +2,25 @@
   Wait for all necessary ROS sensors
 """
 import rospy
-from sensor_msgs.msg import Imu
-from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Imu, LaserScan, CompressedImage, BatteryState
 from nav_msgs.msg import Odometry
 
 
-sensors = set()
-
-
-def callback_imu(data):
-    global sensors
-    if 'imu' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + " Imu")
-        sensors.add('imu')
-
-
-def callback_scan(data):
-    global sensors
-    if 'scan' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + " LidarScan")
-        sensors.add('scan')
-
-
-def callback_image(data):
-    global sensors
-    if 'image' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + " Image")
-        sensors.add('image')
-
-
-def callback_odom(data):
-    global sensors
-    if 'odom' not in sensors:
-        rospy.loginfo(rospy.get_caller_id() + " Odom")
-        sensors.add('odom')
+def wait_for(topic, topic_type):
+    rospy.loginfo('Wait for ' + topic)
+    rospy.wait_for_message(topic, topic_type)
+    rospy.loginfo('Done with ' + topic)
 
 
 def wait_for_sensors():
-    global sensors
-    rospy.init_node('listener', anonymous=True)
-    rospy.loginfo('-------------- BEGIN --------------')
-    rospy.Subscriber('/X2/imu/data', Imu, callback_imu)
-    rospy.Subscriber('/X2/front_scan', LaserScan, callback_scan)
-    rospy.Subscriber('/X2/front/image_raw/compressed', CompressedImage, callback_image)
-    rospy.Subscriber('/X2/odom', Odometry, callback_odom)
-    r = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        if len(sensors) >= 4:
-            break
-        r.sleep()
-    rospy.loginfo('--------------- END ---------------')
+    rospy.init_node('mdwait', anonymous=True)
+    rospy.loginfo('-------------- mdwait BEGIN --------------')
+    wait_for('/X2/imu/data', Imu)
+    wait_for('/X2/front_scan', LaserScan)
+    wait_for('/X2/front/image_raw/compressed', CompressedImage)
+    wait_for('/X2/odom', Odometry)
+    wait_for('/X2/battery_state', BatteryState)  # note, that this is maybe the critical component!
+    rospy.loginfo('--------------- mdwait END ---------------')
 
 
 if __name__ == '__main__':
