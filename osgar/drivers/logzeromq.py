37053cb4ed2ed680c49f13d0e85698eb82a45609
Martin Dlouhy <md@robotika.cz>
2019-09-27 15:20:02 +0200

    Add ZeroQM Pipefile driver
    - support PULL/PUSH modes
    - add zeromq-fwd.json
    - experiment with daemon=False
    - add timeout for send and receive
    - add pyzmq package into requirements.txt

---
+++ osgar/drivers/logzeromq.py
@@ -0,0 +1,82 @@
+"""
+  Logger for ZeroMQ communication
+"""
+
+from threading import Thread
+
+import zmq
+
+from osgar.bus import BusShutdownException
+
+
+class LogZeroMQ:
+    def __init__(self, config, bus):
+        mode = config['mode']
+        endpoint = config['endpoint']
+
+        self.context = zmq.Context()
+        print("Connecting to hello world server ...")
+        if mode == 'PULL':
+            self.socket = self.context.socket(zmq.PULL)
+            if 'timeout' in config:
+                # https://stackoverflow.com/questions/7538988/zeromq-how-to-prevent-infinite-wait
+                self.socket.RCVTIMEO = int(config['timeout'] * 1000) # in milliseconds
+            self.thread = Thread(target=self.run_input, daemon=False)
+        elif mode == 'PUSH':
+            self.socket = self.context.socket(zmq.PUSH)
+            # https://stackoverflow.com/questions/24619490/how-do-i-clear-the-buffer-upon-start-exit-in-zmq-socket-to-prevent-server-from
+            # ZMQ_LINGER: Set linger period for socket shutdown
+            # The default value of -1 specifies an infinite linger period.
+            # Pending messages shall not be discarded after a call to
+            # zmq_close(); attempting to terminate the socket's context with
+            # zmq_term() shall block until all pending messages have been sent
+            # to a peer.
+            self.socket.setsockopt(zmq.LINGER, 100)  # milliseconds
+            self.thread = Thread(target=self.run_output, daemon=False)
+        else:
+            assert False, mode  # unknown/unsupported mode
+
+        self.socket.connect(endpoint)
+
+        self.bus = bus
+
+    def start(self):
+        self.thread.start()
+
+    def join(self, timeout=None):
+        self.thread.join(timeout=timeout)
+
+    def _close(self):
+        self.socket.close()
+        self.context.term()
+
+    def run_input(self):
+        while self.bus.is_alive():
+            try:
+                message = self.socket.recv()
+                self.bus.publish('raw', message)
+            except zmq.error.Again:
+                pass
+        self._close()
+
+    def slot_raw(self, timestamp, data):
+        while self.bus.is_alive():
+            try:
+                self.socket.send(data, zmq.NOBLOCK)
+                break
+            except zmq.error.Again:
+                self.bus.sleep(0.1)
+
+    def run_output(self):
+        try:
+            while True:
+                dt, __, data = self.bus.listen()
+                self.slot_raw(dt, data)
+        except BusShutdownException:
+            pass
+        self._close()
+
+    def request_stop(self):
+        self.bus.shutdown()
+
+# vim: expandtab sw=4 ts=4
---
+++ osgar/drivers/test_logzeromq.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import MagicMock
+import time
+
+import zmq
+
+from osgar.drivers.logzeromq import LogZeroMQ
+from osgar.bus import BusShutdownException
+
+
+class LogZeroMQTest(unittest.TestCase):
+    def test_recv_timeout(self):
+        config = {
+            'mode': 'PULL',
+            'endpoint': 'tcp://localhost:5555',
+            'timeout': 0.1
+        }
+        bus = MagicMock()
+        bus.is_alive = MagicMock(return_value=True)
+        node = LogZeroMQ(config, bus)
+        node.start()
+        time.sleep(0.01)  # give it a chance to start
+        node.request_stop()
+        bus.is_alive = MagicMock(return_value=False)  # supplement mock request_stop()
+        node.join()
+
+    def test_send_timeout(self):
+        config = {
+            'mode': 'PUSH',
+            'endpoint': 'tcp://localhost:5556',
+            'timeout': 0.1
+        }
+        bus = MagicMock()
+        bus.listen = MagicMock(return_value=(1, 2, b'data'))
+        bus.is_alive = MagicMock(return_value=True)
+        node = LogZeroMQ(config, bus)
+        node.start()
+        time.sleep(0.01)  # give it a chance to start
+        node.request_stop()
+        bus.listen = MagicMock(side_effect=BusShutdownException())
+        bus.is_alive = MagicMock(return_value=False)  # supplement mock request_stop()
+        node.join()
+
+# vim: expandtab sw=4 ts=4
