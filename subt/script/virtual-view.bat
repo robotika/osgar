4de4494c61c42812c727a925cf0b8ee33d3bdc11
Martin Dlouhy <md@robotika.cz>
2019-09-29 20:24:17 +0200

    virtual-view.bat - new names for ZeroMQ ROS logfiles

--- subt/script/virtual-view.bat
+++ subt/script/virtual-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg_laser.scan --camera rosmsg_image.image --keyframes detector.artf %1 
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera rosmsg.image --keyframes detector.artf %1 
