a9f6ba785e9c7bdb11516044c4f632f02eec69a9
Martin Dlouhy <md@robotika.cz>
2019-09-29 10:36:39 +0200

    logzeromq.py - refactor, support PULL/PUSH modes

--- osgar/drivers/logzeromq.py
+++ osgar/drivers/logzeromq.py
@@ -11,12 +11,21 @@ from osgar.bus import BusShutdownException
 
 class LogZeroMQ:
     def __init__(self, config, bus):
-        self.thread = Thread(target=self.run, daemon=True)
+        mode = config['mode']
+        endpoint = config['endpoint']
 
         context = zmq.Context()
         print("Connecting to hello world server ...")
-        self.socket = context.socket(zmq.PULL)  # config?
-        self.socket.connect("tcp://localhost:5555")  # TODO config
+        if mode == 'PULL':
+            self.socket = context.socket(zmq.PULL)
+            self.thread = Thread(target=self.run_input, daemon=True)
+        elif mode == 'PUSH':
+            self.socket = context.socket(zmq.PUSH)
+            self.thread = Thread(target=self.run_output, daemon=True)
+        else:
+            assert False, mode  # unknown/unsupported mode
+
+        self.socket.connect(endpoint)  # TODO config
 
         self.bus = bus
 
@@ -26,10 +35,21 @@ class LogZeroMQ:
     def join(self, timeout=None):
         self.thread.join(timeout=timeout)
 
-    def run(self):
+    def run_input(self):
         while self.bus.is_alive():
             message = self.socket.recv()
             self.bus.publish('raw', message)
+
+    def slot_raw(self, timestamp, data):
+        self.socket.send(data)
+
+    def run_output(self):
+        try:
+            while True:
+                dt, __, data = self.bus.listen()
+                self.slot_raw(dt, data)
+        except BusShutdownException:
+            pass
 
     def request_stop(self):
         self.bus.shutdown()
