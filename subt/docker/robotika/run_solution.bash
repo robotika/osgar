5018c3130bba2d5e4fcc5bd905f1ceeacfa192ef
Martin Dlouhy <md@robotika.cz>
2019-09-30 20:18:31 +0200

    run_solution.bash - choose side automatically

--- subt/docker/robotika/run_solution.bash
+++ subt/docker/robotika/run_solution.bash
@@ -7,7 +7,7 @@
 sleep 30
 
 cd osgar
-python3 -m subt run subt/zmq-subt-x2.json --side left --walldist 0.75 --timeout 180 --note "try to visit artifact and return home" &
+python3 -m subt run subt/zmq-subt-x2.json --side auto --walldist 0.75 --timeout 180 --note "try to visit artifact and return home" &
 cd ..
 
 # Run your solution.
