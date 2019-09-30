504a0356c166254211bf569cd16c06acb95cc2ab
Martin Dlouhy <md@robotika.cz>
2019-09-30 22:10:56 +0200

    ros_proxy_node.cc - FIXUP position of termination character for ROS_INFO

--- subt/src/ros_proxy_node.cc
+++ subt/src/ros_proxy_node.cc
@@ -119,7 +119,7 @@ bool getSpeedCmd(geometry_msgs::Twist& msg)
   {
     if(strncmp(buffer, "stdout ", 7) == 0)
     {
-      buffer[size - 1] = 0;
+      buffer[size] = 0;
       ROS_INFO("Python3: %s", buffer);
     }
     else
