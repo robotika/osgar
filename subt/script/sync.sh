7588c8da942b96c33d9415fc625fc7c0e06a5503
Martin Dlouhy <md@robotika.cz>
2019-10-02 07:27:37 +0200

    sync.sh - update with ros_proxy

--- subt/script/sync.sh
+++ subt/script/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp osgar/subt/src/subt_example_node.cc src/subt/subt_example/src/subt_example_node.cc
-cp osgar/subt/launch/example_robot.launch src/subt/subt_example/launch/example_robot.launch
+cp osgar/subt/src/ros_proxy_node.cc src/subt/subt_example/src/subt_example_node.cc
+cp osgar/subt/src/CMakeLists.txt src/subt/subt_example/CMakeLists.txt
 
 catkin_make install
 
