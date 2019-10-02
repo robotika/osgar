6877dbd5742d62fb0b44400a7cfcc85f9ce148c9
Martin Dlouhy <md@robotika.cz>
2019-10-02 07:31:41 +0200

    sync - FIXUP

--- subt/script/sync.sh
+++ subt/script/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp osgar/subt/src/ros_proxy_node.cc src/subt/subt_example/src/subt_example_node.cc
-cp osgar/subt/src/CMakeLists.txt src/subt/subt_example/CMakeLists.txt
+cp osgar/subt/src/ros_proxy_node.cc src/subt_seed/src/subt_example_node.cc
+cp osgar/subt/src/CMakeLists.txt src/subt_seed/CMakeLists.txt
 
 catkin_make install
 
