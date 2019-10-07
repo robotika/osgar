423fd45ffd122e9f787ece2aa56201cbd9d66eb9
Martin Dlouhy <md@robotika.cz>
2019-10-07 13:36:43 +0200

    aws2log.py - add option "recover", which ignores missing beginning

--- subt/aws2log.py
+++ subt/aws2log.py
@@ -5,17 +5,19 @@ import sys
 import os.path
 
 
-def aws2log(filename, outname):
+def aws2log(filename, outname, recover=False):
     with open(filename) as f:
-        for line in f:
-            if 'Python3' in line and 'Dump' in line:
-                print(line)
-                break
+        if not recover:
+            # search for the beginning
+            for line in f:
+                if 'Python3' in line and 'Dump' in line:
+                    print(line)
+                    break
 
-        for line in f:
-            if 'Python3' in line and 'Size' in line:
-                print(line)
-                break
+            for line in f:
+                if 'Python3' in line and 'Size' in line:
+                    print(line)
+                    break
 
         with open(outname, 'wb') as out:
             prev = None
@@ -27,6 +29,8 @@ def aws2log(filename, outname):
                     offset = int(s.split()[0])
                     data = eval(s[s.index(' '):])
                     out.write(data)
+                    if prev is None and recover:
+                        i = offset//100
                     assert i * 100 == offset, (i, s, prev)
                     prev = line
                     i += 1
@@ -38,9 +42,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('filename', help='AWS ROS recorded log file')
+    parser.add_argument('--recover', help='Skip initial part and recover from missing global header', action='store_true')
     args = parser.parse_args()
 
     outname = os.path.join(os.path.dirname(args.filename), 'osgar.log')
-    aws2log(args.filename, outname=outname)
+    aws2log(args.filename, outname=outname, recover=args.recover)
 
 # vim: expandtab sw=4 ts=4
