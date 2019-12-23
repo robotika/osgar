265ad7a2fcbb500ce808618a22404ef4a996a694
Martin Dlouhy <md@robotika.cz>
2019-12-23 11:00:53 +0100

    tankmaria.py - simple SpeedControl
    - send PWM based on received encoders
    - increase communication speed with motor controllers
    - increase PWM limit to 220
    - increase speed to 0.75m/s

--- osgar/drivers/tankmaria.py
+++ osgar/drivers/tankmaria.py
@@ -25,25 +25,52 @@ from osgar.bus import BusShutdownException
 
 ENC_SCALE = 0.00052
 WHEEL_DISTANCE = 0.4  # 50cm long, 40cm wide
+UPDATE_RATE = 20.0  # Hz
+MAX_PWM = 220  # limit motor driver PWM output (255 is HW limit)
 
 GREEN_LED = 1
 RED_LED = 2
 
 
-class MariaTimer(Node):
-    def __init__(self, config, bus):
-        super().__init__(config, bus)
-        bus.register('tick')
-        self.sleep_time = config['sleep']
+class SpeedControl:
+    def __init__(self):
+        self.min_pwm = 100
+        self.max_pwm = MAX_PWM
+        self.step_pwm = 10
+        self.tolerance = 5  # do not change PWM output if it is in +/- tolerance
 
-    def run(self):
-        try:
-            while self.is_alive():
-                self.publish('tick', None)
-                self.sleep(self.sleep_time)
+        self.desired_ticks = 0
+        self.last_pwm = 0
 
-        except BusShutdownException:
-            pass
+    def set_desired_ticks(self, ticks):
+        """desired encoder ticks per fixed period"""
+        self.desired_ticks = ticks
+
+    def update(self, encoder_ticks):
+        """Regular status of encoders ticks, return new PWM"""
+        pwm = self.last_pwm
+        if self.desired_ticks == 0:
+            pwm = 0
+        elif self.desired_ticks > 0:
+            if encoder_ticks < -self.tolerance:
+                # change of polarity, transit via 0
+                pwm = 0
+            elif abs(encoder_ticks - self.desired_ticks) > self.tolerance:
+                if encoder_ticks > self.desired_ticks:
+                    pwm = max(self.last_pwm - self.step_pwm, self.min_pwm)
+                else:
+                    pwm = max(self.last_pwm + self.step_pwm, self.min_pwm)
+        else:  # self.desired_ticks < 0
+            if encoder_ticks > self.tolerance:
+                pwm = 0
+            elif abs(encoder_ticks - self.desired_ticks) > self.tolerance:
+                if abs(encoder_ticks) > abs(self.desired_ticks):
+                    pwm = -max(-self.last_pwm - self.step_pwm, self.min_pwm)
+                else:
+                    pwm = -max(-self.last_pwm + self.step_pwm, self.min_pwm)
+        pwm = max(-self.max_pwm, min(self.max_pwm, pwm))  # clip limits
+        self.last_pwm = pwm
+        return pwm
 
 
 class RobotTankMaria(Node):
@@ -54,6 +81,8 @@ class RobotTankMaria(Node):
         # commands
         self.desired_speed = 0.0  # m/s
         self.desired_angular_speed = 0.0
+        self.sc_left = SpeedControl()
+        self.sc_right = SpeedControl()
 
         # status
         self.emergency_stop = None  # uknown state
@@ -102,6 +131,28 @@ class RobotTankMaria(Node):
             #  This is different to compass where you have only current heading.
         return True, (x, y, heading), (dist, angle)
 
+    def update_pwm0(self, encoders_left, encoders_right):
+        STEP = int(min(255, abs(100 * self.desired_speed)))  # test PWM
+        if STEP == 0:
+            STEP = 200  # hardcoded for rotaion in place
+        if self.desired_speed > 0:
+            left, right = STEP, STEP
+        elif self.desired_speed < 0:
+            left, right = -STEP, -STEP
+        elif self.desired_angular_speed > 0:
+            left, right = -STEP, STEP
+        elif self.desired_angular_speed < 0:
+            left, right = STEP, -STEP
+        else:
+            left, right = 0, 0  # STOP
+
+        self.publish('raw', b'%d %d %d\n' % (left, right, self.led))
+
+    def update_pwm(self, encoders_left, encoders_right):
+        left = self.sc_left.update(encoders_left)
+        right = self.sc_right.update(encoders_right)
+        self.publish('raw', b'%d %d %d\n' % (left, right, self.led))
+
     def get_packet(self, buf):
         s = buf.split(b'\n')
         if len(s) < 2:
@@ -110,6 +161,10 @@ class RobotTankMaria(Node):
 
     def slot_desired_speed(self, timestamp, data):
         self.desired_speed, self.desired_angular_speed = data[0]/1000.0, math.radians(data[1]/100.0)
+        left = self.desired_speed - self.desired_angular_speed/WHEEL_DISTANCE/2
+        right = self.desired_speed + self.desired_angular_speed/WHEEL_DISTANCE/2
+        self.sc_left.set_desired_ticks(left/ENC_SCALE/UPDATE_RATE)
+        self.sc_right.set_desired_ticks(right/ENC_SCALE/UPDATE_RATE)
 
     def slot_raw(self, timestamp, data):
         self.buf += data
@@ -127,7 +182,7 @@ class RobotTankMaria(Node):
             assert len(d) == 4, d
             prev = self.encoder
             self.milisec = d[0]
-            self.encoder = d[1], d[2]  # more inputs expected
+            self.encoder = d[1], d[2]
             if prev is not None:
                 left, right = self.encoder[0] - prev[0], self.encoder[1] - prev[1]
                 self.publish('encoders', [left, right])
@@ -135,25 +190,9 @@ class RobotTankMaria(Node):
                 if valid:
                     self.pose2d = pose2d
                 self.send_pose2d()
+                self.update_pwm(left, right)
 
             packet, self.buf = self.get_packet(self.buf)
-
-    def slot_pwm(self, timestamp, data):
-        STEP = int(min(255, abs(100 * self.desired_speed)))  # test PWM
-        if STEP == 0:
-            STEP = 200  # hardcoded for rotaion in place
-        if self.desired_speed > 0:
-            left, right = STEP, STEP
-        elif self.desired_speed < 0:
-            left, right = -STEP, -STEP
-        elif self.desired_angular_speed > 0:
-            left, right = -STEP, STEP
-        elif self.desired_angular_speed < 0:
-            left, right = STEP, -STEP
-        else:
-            left, right = 0, 0  # STOP
-
-        self.publish('raw', b'%d %d %d\n' % (left, right, self.led))
 
     def run(self):
         try:
@@ -162,8 +201,6 @@ class RobotTankMaria(Node):
 
                 if channel == 'raw':
                     self.slot_raw(self.time, data)
-                elif channel == 'pwm':
-                    self.slot_pwm(self.time, data)
                 elif channel == 'desired_speed':
                     self.slot_desired_speed(self.time, data)
                 else:
--- osgar/drivers/test_tankmaria.py
+++ osgar/drivers/test_tankmaria.py
@@ -2,7 +2,7 @@ import unittest
 import datetime
 from unittest.mock import MagicMock
 
-from osgar.drivers.tankmaria import RobotTankMaria
+from osgar.drivers.tankmaria import RobotTankMaria, SpeedControl, MAX_PWM
 
 
 class RobotTankMariaTest(unittest.TestCase):
@@ -16,5 +16,31 @@ class RobotTankMariaTest(unittest.TestCase):
         r = RobotTankMaria({}, MagicMock())
         self.assertEqual((None, b'\xfe'), r.get_packet(b'\xfe'))
         self.assertEqual((b'\xfe51 0 0', b''), r.get_packet(b'\xfe51 0 0\r\n'))
+
+    def test_speed_control(self):
+        sc = SpeedControl()
+        self.assertEqual(sc.update(0), 0)
+
+        sc.set_desired_ticks(50)
+        self.assertEqual(sc.update(0), 100)
+        self.assertEqual(sc.update(0), 110)
+        self.assertEqual(sc.update(50), 110)
+        self.assertEqual(sc.update(70), 100)
+
+        # change polarity
+        sc.set_desired_ticks(-50)
+        self.assertEqual(sc.update(50), 0)  # wait until it is not moving wrong direction
+        self.assertEqual(sc.update(3), -100)
+        self.assertEqual(sc.update(-30), -110)
+        self.assertEqual(sc.update(-51), -110)
+        self.assertEqual(sc.update(-66), -100)
+
+        for __ in range(20):
+            self.assertGreaterEqual(sc.update(-10), -MAX_PWM)
+
+        sc.set_desired_ticks(50)
+        self.assertEqual(sc.update(-70), 0)
+        self.assertEqual(sc.update(-2), 100)
+        self.assertEqual(sc.update(-2), 110)
 
 # vim: expandtab sw=4 ts=4
