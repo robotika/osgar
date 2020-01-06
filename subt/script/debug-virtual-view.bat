d74ea9654e4027d4546e7ee61035bacee05a556a
Martin Dlouhy <md@robotika.cz>
2020-01-06 07:01:37 +0100

    Add virtual-depth-view.bat and allow optional parameters to other BAT files

--- subt/script/debug-virtual-view.bat
+++ subt/script/debug-virtual-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera detector.debug_artf --keyframes detector.artf %1 
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera detector.debug_artf --keyframes detector.artf %1 %2 %3 %4 %5 %6 %7 %8 %9
