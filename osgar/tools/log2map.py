240200179bad9025a2d866b2fbe6916485d5add5
Martin Dlouhy <md@robotika.cz>
2020-01-08 19:51:33 +0100

    log2map.py - provide useful defaults

--- osgar/tools/log2map.py
+++ osgar/tools/log2map.py
@@ -1,6 +1,8 @@
 """
   Create map (gridmap image)
 """
+import os.path
+
 import cv2  # for video output
 import numpy as np  # faster depth data processing
 
@@ -16,14 +18,13 @@ def main():
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('logfile', help='recorded log file')
-    parser.add_argument('--out', help='map output file', default='world.jpg')
-    parser.add_argument('--pose2d', help='stream ID for pose2d messages')
+    parser.add_argument('--out', help='map output file')
+    parser.add_argument('--pose2d', help='stream ID for pose2d messages', default='app.pose2d')
     args = parser.parse_args()
-    if args.pose2d is None:
-        print("Available streams:")
-        for stream in lookup_stream_names(args.logfile):
-            print("  ", stream)
-        return
+
+    out_filename = args.out
+    if out_filename is None:
+        out_filename = os.path.join(os.path.dirname(args.logfile), 'world.jpg')
 
     pts = []
     for dt, channel, data in LogReader(args.logfile, only_stream_id=lookup_stream_id(args.logfile, args.pose2d)):
@@ -41,7 +42,7 @@ def main():
         px = int(SCALE*(x - min_x)/1000.0) + BORDER_PX
         py = int(SCALE*(y - min_y)/1000.0) + BORDER_PX
         world[height_px - py - 1, px] = 255
-    cv2.imwrite(args.out, world)
+    cv2.imwrite(out_filename, world)
 
 
 if __name__ == "__main__":
