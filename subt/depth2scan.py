6d72ba8bc6f1978efec8c098ca7f696cfcdd1472
Martin Dlouhy <md@robotika.cz>
2020-01-13 21:10:50 +0100

    depth2scan.py - FIXUP (use really slope and not horizontal plane)

--- subt/depth2scan.py
+++ subt/depth2scan.py
@@ -21,44 +21,30 @@ class DepthToScan(Node):
         channel = super().update()
         assert channel in ["depth", "scan"], channel
 
-        if channel == 'scan':
-            assert len(self.scan) == 720, len(self.scan)
-        elif channel == 'depth':
+        if channel == 'depth':
             assert self.depth.shape == (360, 640), self.depth.shape
-            i = (360//2)
-            mid_line = self.depth[i]
-            # 60*720/270 = 160.0 ... i.e. 160 elements to be replaced
-            # 640/160 = 4.0 ... i.e. downsample by 4
-            small = mid_line[::4]
-            mask = small == 0xFFFF
-            small[mask] = 0
-            if self.scan is not None:
-                new_scan = self.scan[:360-80] + np.flip(small, axis=0).tolist() + self.scan[360+80:]
-                assert len(new_scan) == 720, len(new_scan)
-                self.publish('scan', new_scan)
         elif channel == 'scan':
             assert len(self.scan) == 720, len(self.scan)
             if self.depth is None:
                 self.publish('scan', self.scan)
                 return channel  # when no depth data are available ...
-            i = 300*640  # slope down lidar
-            mid_line = np.array(self.depth[i:i+640], dtype=np.dtype('H'))
+            i = 300  # slope down lidar ... used to be (360//2)
+            mid_line = self.depth[i]
             # 60*720/270 = 160.0 ... i.e. 160 elements to be replaced
             # 640/160 = 4.0 ... i.e. downsample by 4
             small = np.array(mid_line[::4], dtype=np.int32)  # problem with abs() of uint16
             mask0 = small == 0xFFFF
             diff = abs(small[:-1] - small[1:])
             mask1 = diff > 100
-            if self.scan is not None:
-                new_scan = self.scan[:]
-                for i, val in enumerate(mask1):
-                    if val:
-                        if self.verbose:
-                            print(i, new_scan[360+79-i], int(small[i]))
-                            assert False, small
-                        new_scan[360+79-i] = int(small[i])
-                assert len(new_scan) == 720, len(new_scan)
-                self.publish('scan', new_scan)
+            new_scan = self.scan[:]
+            for i, val in enumerate(mask1):
+                if val:
+                    if self.verbose:
+                        print(i, new_scan[360+79-i], int(small[i]))
+                        assert False, small
+                    new_scan[360+79-i] = int(small[i])
+            assert len(new_scan) == 720, len(new_scan)
+            self.publish('scan', new_scan)
         else:
             assert False, channel  # unsupported channel
 
