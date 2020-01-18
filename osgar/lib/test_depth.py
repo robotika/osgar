5b596f26ac6d004ff79e9bc2dcf3de7a5525966e
Martin Dlouhy <md@robotika.cz>
2020-01-18 11:42:16 +0100

    lib/depth.py - implement depth2danger() function
    - increase threshold to 8cm and slope to 45 degrees
    - add test_depth.py and danger2dist
    - extend distance test

---
+++ osgar/lib/test_depth.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from osgar.lib.depth import danger2dist
+
+
+class DepthTest(unittest.TestCase):
+
+    def test_danger2dist(self):
+        danger = np.zeros((360, 640), dtype=bool)
+        danger[100][100] = True
+        danger[200][630] = True
+        dist = danger2dist(danger)
+        self.assertEqual(len(dist), 640)
+        self.assertEqual(dist[100], 100)
+        self.assertEqual(dist[630], 200)
+
+        values = danger[dist, np.arange(640)]
+        self.assertTrue(values[100])
+        self.assertTrue(values[630])
+        self.assertFalse(values[300])
+
+# vim: expandtab sw=4 ts=4
