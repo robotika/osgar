dffb68558c18ef193c54f82e96c4cca8000f2cc9
Martin Dlouhy <md@robotika.cz>
2020-01-19 18:32:15 +0100

    add --title rosmsg.sim_time_sec to all virtual scripts

--- subt/script/debug-virtual-view.bat
+++ subt/script/debug-virtual-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera detector.debug_artf --keyframes detector.artf %1 %2 %3 %4 %5 %6 %7 %8 %9
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera detector.debug_artf --keyframes detector.artf --title rosmsg.sim_time_sec %1 %2 %3 %4 %5 %6 %7 %8 %9
