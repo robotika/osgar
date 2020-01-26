7d6db933e3b1286e0b0ce917b755f1106b566559
Martin Dlouhy <md@robotika.cz>
2020-01-26 09:50:34 +0100

    rosbag2log.py - use --all option also for SQLite record (common for all robots)

--- subt/rosbag2log.py
+++ subt/rosbag2log.py
@@ -192,7 +192,8 @@ if __name__ == "__main__":
                     print(member.name)
                     with open(os.path.join(os.path.dirname(args.filename), rosout_name), 'wb') as f:
                         f.write(tar.extractfile(member).read())
-                elif member.name in ['score.yml', 'server_console.log'] or member.name.startswith('subt_urban_'):
+                elif (member.name in ['score.yml', 'server_console.log'] or member.name.startswith('subt_urban_') or
+                      (member.name.startswith('state.tlog') and args.all)):
                     print(member.name)
                     with open(os.path.join(os.path.dirname(args.filename), member.name.replace(':', '_')), 'wb') as f:
                         f.write(tar.extractfile(member).read())
