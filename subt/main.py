7d487097b1828af9586e00694a2d44f1285a8a35
Martin Dlouhy <md@robotika.cz>
2020-01-30 15:09:54 +0100

    subt/main.py - set Ver55

--- subt/main.py
+++ subt/main.py
@@ -801,7 +801,7 @@ class SubTChallenge:
         self.stdout("Dump END")
 
     def play_virtual_track(self):
-        self.stdout("SubT Challenge Ver54!")
+        self.stdout("SubT Challenge Ver55!")
         self.stdout("Waiting for robot_name ...")
         while self.robot_name is None:
             self.update()
