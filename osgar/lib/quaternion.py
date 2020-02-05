625a2602250d80c98a1252f3632e5005e68ce542
Martin Dlouhy <md@robotika.cz>
2020-02-05 23:38:14 +0100

    lib/quaternion.py - change (w, x, y, z) to (x, y, z, w)

--- osgar/lib/quaternion.py
+++ osgar/lib/quaternion.py
@@ -1,20 +1,31 @@
+"""
+  Quaternion - defined as (x, y, z, w) to be compatible with ROS and RealSense
 
+  ROS:
+    http://wiki.ros.org/tf2/Tutorials/Quaternions
+
+ROS uses quaternions to track and apply rotations. A quaternion has 4
+components (x,y,z,w). That's right, 'w' is last (but beware: some libraries
+like Eigen put w as the first number!). The commonly-used unit quaternion that
+yields no rotation about the x/y/z axes is (0,0,0,1)
+"""
 import math
 
+
 def multiply(quaternion1, quaternion0):
-    w0, x0, y0, z0 = quaternion0
-    w1, x1, y1, z1 = quaternion1
-    return [-x1*x0 - y1*y0 - z1*z0 + w1*w0,
-             x1*w0 + y1*z0 - z1*y0 + w1*x0,
+    x0, y0, z0, w0 = quaternion0
+    x1, y1, z1, w1 = quaternion1
+    return [ x1*w0 + y1*z0 - z1*y0 + w1*x0,
             -x1*z0 + y1*w0 + z1*x0 + w1*y0,
-             x1*y0 - y1*x0 + z1*w0 + w1*z0]
+             x1*y0 - y1*x0 + z1*w0 + w1*z0,
+            -x1*x0 - y1*y0 - z1*z0 + w1*w0]
 
 def conjugate(quaternion):
-    w, x, y, z = quaternion
-    return [w, -x, -y, -z]
+    x, y, z, w = quaternion
+    return [-x, -y, -z, w]
 
 def identity():
-    return [1, 0, 0, 0]
+    return [0, 0, 0, 1]
 
 def rotate_vector(vector, quaternion):
     qvector = [0] + vector
@@ -23,14 +34,14 @@ def rotate_vector(vector, quaternion):
     return multiply(part1, con)[1:]
 
 def euler_zyx(quaternion):
-    w0, x0, y0, z0 = quaternion
+    x0, y0, z0, w0 = quaternion
     ax =  math.atan2(2*(w0*x0+y0*z0), 1-2*(x0*x0+y0*y0))
     ay =  math.asin(2*(w0*y0-z0*x0))
     az =  math.atan2(2*(w0*z0+x0*y0), 1-2*(y0*y0+z0*z0))
     return [az, ay, ax]
 
 def heading(quaternion):
-    w0, x0, y0, z0 = quaternion
+    x0, y0, z0, w0 = quaternion
     az =  math.atan2(2*(w0*z0+x0*y0), 1-2*(y0*y0+z0*z0))
     return az
 
--- osgar/lib/test_quaternion.py
+++ osgar/lib/test_quaternion.py
@@ -6,13 +6,13 @@ from . import quaternion
 class QuaternionTest(unittest.TestCase):
 
     def test_multiply(self):
-        qa = [1, 0, 0, 0]
-        qb = [0, 1, 0, 0]
+        qa = [0, 0, 0, 1]
+        qb = [1, 0, 0, 0]
         qc = quaternion.multiply(qa, qb)
         self.assertEqual(qc, qb)  # identity
 
         qd = quaternion.multiply(qb, qb)
-        self.assertEqual(qd, [-1, 0, 0, 0])  # twice rotation by 90deg
+        self.assertEqual(qd, [0, 0, 0, -1])  # twice rotation by 90deg
 
         qdd = quaternion.multiply(qd, qd)
         self.assertEqual(qdd, qa)  # identity
