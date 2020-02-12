f2622cdb2aa57e9c4955b0ef1c59cd9aab569e6e
Martin Dlouhy <md@robotika.cz>
2020-02-12 06:49:49 +0100

    kloubak-view.bat - add --joint parameter to draw K2 angle

--- subt/script/kloubak-view.bat
+++ subt/script/kloubak-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --lidar lidar.scan --camera camera.raw --camera2 camera_back.raw --pose2d app.pose2d --keyframes detector.artf %*
+python -m osgar.tools.lidarview --lidar lidar.scan --camera camera.raw --camera2 camera_back.raw --pose2d app.pose2d --keyframes detector.artf --joint kloubak.joint_angle %*
