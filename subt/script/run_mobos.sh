bd0214da7f783148df72253e3c81cbb5a274e487
Pavliik <pavel.jiroutek@gmail.com>
2020-02-19 22:42:22 +0100

    add mobos run script

---
+++ subt/script/run_mobos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+(python3 -m subt.main run config/mobos-ros.json --side left --speed 0.5 --walldist 0.5 --timeout 90;python3 -m osgar.record config/test-lora.json) &
+#python3 -m subt.main run config/mobos-ros.json --side left --speed 0.5 --walldist 0.5 &
+roslaunch robot auto_mob.launch
+
+wait
+
