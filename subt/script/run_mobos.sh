db6dde689a6e8b6c31097e9ed4f5350d9955e090
Pavel Jiroutek <pavel.jiroutek@gmail.com>
2020-02-20 06:52:29 +0100

    change mobos camera mounting; prepare script for first round

--- subt/script/run_mobos.sh
+++ subt/script/run_mobos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(python3 -m subt.main run config/mobos-ros.json --side left --speed 0.5 --walldist 0.5 --timeout 90;python3 -m osgar.record config/test-lora.json) &
+(python3 -m subt.main run config/mobos-ros.json --side left --speed 0.5 --walldist 0.5 --start-stopped;python3 -m osgar.record config/test-lora.json) &
 #python3 -m subt.main run config/mobos-ros.json --side left --speed 0.5 --walldist 0.5 &
 roslaunch robot auto_mob.launch
 
