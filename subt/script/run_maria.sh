5b2eba54e2b6304c81bb5cfebf11e6130740d3c0
shared_user@maria <shared_user@maria>
2020-02-20 00:46:14 -0500

    camera mounting for Maria

--- subt/script/run_maria.sh
+++ subt/script/run_maria.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(python3 -m subt.main run config/maria-ros.json --side left --speed 0.5 --walldist 0.6 --start-paused;python3 -m osgar.record config/test-lora.json) &
+(python3 -m subt.main run config/maria-ros.json --side left --speed 0.5 --walldist 0.7 --start-paused;python3 -m osgar.record config/test-lora.json) &
 roslaunch robot maria.launch
 
 wait
