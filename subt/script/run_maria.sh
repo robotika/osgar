8afa4d85a1e00eed4e49d48a8e76eee8e09a218d
shared_user@maria <shared_user@maria>
2020-02-19 23:15:37 -0500

    Maria: startup script; camera params

---
+++ subt/script/run_maria.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+(python3 -m subt.main run config/maria-ros.json --side left --speed 0.5 --walldist 0.6 --start-paused;python3 -m osgar.record config/test-lora.json) &
+roslaunch robot maria.launch
+
+wait
+
