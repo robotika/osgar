3dbecc966a339a60817136579d8debaf69e01c57
Jakub <jlev@tf.czu.cz>
2020-02-21 10:02:37 +0100

    subt/script/run_kloubak3.sh: add osgar.go -d 4
    - keep go 4m as commented option for Alpha Course only

--- subt/script/run_kloubak3.sh
+++ subt/script/run_kloubak3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python -m subt run config/kloubak3-subt-estop-lora.json --side left --speed 0.7 --timeout 300
+#python -m osgar.go config/kloubak3-subt-estop-lora.json -d 4 --timeout 20
 python -m osgar.record config/test-lora.json
