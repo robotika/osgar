0a1a3342cd12daf2aaeeb8c244e8b6498b5c3a55
Zbynek Winkler <zw@robotika.cz>
2020-02-20 23:51:10 -0800

    scan_mixer: take minimum from two scans
     * handle infinity
     * add tests
     * don't mix scans if too old

--- subt/scan_mixer.py
+++ subt/scan_mixer.py
@@ -1,14 +1,13 @@
 """
   Convert depth image to "lidar" scan
 """
-import math
-
+import logging
+from datetime import timedelta
 import numpy as np
 
 from osgar.node import Node
-from osgar.bus import BusShutdownException
-from osgar.lib.depth import depth2dist, FX, CAMW
-from osgar.lib.mathex import normalizeAnglePIPI
+
+g_logger = logging.getLogger(__name__)
 
 
 class ScanMixer(Node):
@@ -18,6 +17,7 @@ class ScanMixer(Node):
         bus.register("scan")
         self.scan = None  # original lidar scan
         self.rs_scan = None  # RealSense RGBD converted scan from ROS
+        self.rs_scan_time = timedelta(0)
         self.verbose = False
 
     def update(self):
@@ -27,16 +27,68 @@ class ScanMixer(Node):
         if channel == 'rs_scan':
             self.rs_scan_time = self.time
         elif channel == 'scan':
-            assert len(self.scan) in [271, ], len(self.scan)  # Eduro=271
-            if self.rs_scan is None and self.slope_scan is None:
+            #assert len(self.scan) in [271, ], len(self.scan)  # Eduro=271
+            if self.rs_scan is not None:
+                max_delay = timedelta(milliseconds=500)
+                rs_delay = self.time - self.rs_scan_time
+                if rs_delay > max_delay:
+                    g_logger.debug(f"rs_scan older than {max_delay}: {rs_delay}")
+                    self.publish('scan', self.scan)
+                else:
+                    indexes = np.linspace(0, len(self.rs_scan), num=len(self.scan), endpoint=False, dtype=int)
+                    rs_scan = np.asarray(self.rs_scan)
+                    scan = np.asarray(self.scan)
+                    # replace infinity with long distance
+                    infinity = 30000
+                    rs_scan[rs_scan == 0] = infinity
+                    scan[scan == 0] = infinity
+                    # resample
+                    rs_scan = rs_scan[indexes]
+                    scan = np.minimum(rs_scan, scan)
+                    # replace long distance with infinity
+                    scan[scan == infinity] = 0
+                    self.publish('scan', scan.tolist())
+            else:
                 self.publish('scan', self.scan)
-                return channel  # when no other sources are available ...
-
-            # TODO some smart processing here
-            self.publish('scan', self.scan)
         else:
             assert False, channel  # unsupported channel
 
         return channel
+
+
+if __name__ == "__main__":
+    import argparse
+    from unittest.mock import MagicMock
+    from osgar import logger
+    from osgar import bus
+    from osgar.lib.serialize import deserialize
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('logfile', help='filename of stored file')
+    rs_scan = "rosmsg.scan"
+    scan = "lidar.scan"
+    stream_names = [rs_scan, scan]
+
+    bus = bus.Bus(MagicMock())
+    tester = bus.handle('tester')
+    tester.register("scan", "rs_scan")
+    mixer = ScanMixer(config={}, bus=bus.handle('mixer'))
+    bus.connect("tester.scan", "mixer.scan")
+    bus.connect("tester.rs_scan", "mixer.rs_scan")
+    bus.connect("mixer.scan", "tester.mixed")
+    mixer.start()
+
+    args = parser.parse_args()
+    stream_ids = [logger.lookup_stream_id(args.logfile, stream) for stream in stream_names]
+    id2name = { stream_id: name for stream_id, name in zip(stream_ids, ["rs_scan", "scan"]) }
+    with logger.LogReader(args.logfile, only_stream_id=stream_ids) as log:
+        for timestamp, stream_id, bytes_data in log:
+            data = deserialize(bytes_data)
+            tester.publish(id2name[stream_id], data)
+            if id2name[stream_id] == "scan":
+                t, s, d = tester.listen()
+
+    mixer.request_stop()
+    mixer.join()
 
 # vim: expandtab sw=4 ts=4
--- subt/test_scan_mixer.py
+++ subt/test_scan_mixer.py
@@ -1,7 +1,8 @@
 import unittest
-import math
+from unittest.mock import MagicMock
+from datetime import timedelta
 
-import numpy as np
+from osgar.bus import Bus
 
 from subt.scan_mixer import ScanMixer
 
@@ -9,7 +10,91 @@ from subt.scan_mixer import ScanMixer
 class ScanMixerTest(unittest.TestCase):
 
     def test_usage(self):
-        pass
+        bus = Bus(MagicMock())
+        tester = bus.handle('tester')
+        tester.register("scan", "rs_scan")
+        mixer = ScanMixer(config={}, bus=bus.handle('mixer'))
+        bus.connect("tester.scan", "mixer.scan")
+        bus.connect("tester.rs_scan", "mixer.rs_scan")
+        bus.connect("mixer.scan", "tester.mixed")
+        mixer.start()
+        scan = [10, 10, 10, 10]
+        tester.publish("scan", scan)
+        _, channel, data = tester.listen()
+        self.assertEqual(data, scan)
+        mixer.request_stop()
+        mixer.join()
+
+    def test_mixing(self):
+        log = MagicMock()
+        log.write.return_value = timedelta()
+        bus = Bus(log)
+        tester = bus.handle('tester')
+        tester.register("scan", "rs_scan")
+        mixer = ScanMixer(config={}, bus=bus.handle('mixer'))
+        bus.connect("tester.scan", "mixer.scan")
+        bus.connect("tester.rs_scan", "mixer.rs_scan")
+        bus.connect("mixer.scan", "tester.mixed")
+        mixer.start()
+        scan = 4*[10]
+        rs_scan = 3*[8]
+        tester.publish("rs_scan", rs_scan)
+        tester.publish("scan", scan)
+        _, channel, data = tester.listen()
+        self.assertEqual(data, 4*[8])
+        mixer.request_stop()
+        mixer.join()
+
+    def test_infinity(self):
+        log = MagicMock()
+        log.write.return_value = timedelta()
+        bus = Bus(log)
+        tester = bus.handle('tester')
+        tester.register("scan", "rs_scan")
+        mixer = ScanMixer(config={}, bus=bus.handle('mixer'))
+        bus.connect("tester.scan", "mixer.scan")
+        bus.connect("tester.rs_scan", "mixer.rs_scan")
+        bus.connect("mixer.scan", "tester.mixed")
+
+        mixer.start()
+        scan = 3*[10]+[0]
+        rs_scan = 3*[8]
+        tester.publish("rs_scan", rs_scan)
+        tester.publish("scan", scan)
+        _, channel, data = tester.listen()
+        self.assertEqual(data, 4*[8])
+
+        scan = 4*[10]
+        rs_scan = 2*[8]+[0]
+        tester.publish("rs_scan", rs_scan)
+        tester.publish("scan", scan)
+        _, channel, data = tester.listen()
+        self.assertEqual(data, 3*[8]+[10])
+
+        mixer.request_stop()
+        mixer.join()
+
+    def test_old_rs_scan(self):
+        log = MagicMock()
+        log.write.return_value = timedelta()
+        bus = Bus(log)
+        tester = bus.handle('tester')
+        tester.register("scan", "rs_scan")
+        mixer = ScanMixer(config={}, bus=bus.handle('mixer'))
+        bus.connect("tester.scan", "mixer.scan")
+        bus.connect("tester.rs_scan", "mixer.rs_scan")
+        bus.connect("mixer.scan", "tester.mixed")
+
+        mixer.start()
+        scan = 3*[10]+[0]
+        rs_scan = 3*[8]
+        tester.publish("rs_scan", rs_scan)
+        log.write.return_value = timedelta(seconds=1)
+        tester.publish("scan", scan)
+        _, channel, data = tester.listen()
+        self.assertEqual(data, 3*[10]+[0])
+
+        mixer.request_stop()
+        mixer.join()
 
 # vim: expandtab sw=4 ts=4
-
