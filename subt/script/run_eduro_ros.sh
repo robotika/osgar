f2823a67b04dac1bf862bee456e492c9cddd634d
Eduro user <shared@eduro>
2020-02-24 05:54:12 +0000

    change script and osgar config for eduro jetson

--- subt/script/run_eduro_ros.sh
+++ subt/script/run_eduro_ros.sh
@@ -2,7 +2,7 @@
 
 trap "exit;" HUP INT TERM
 trap "kill 0" EXIT
-(python -m subt run config/eduro-subt-estop-lora-wifi-jetson.json --side right --speed 0.7 --walldist 0.6 --timeout 300 --start-paused; python -m osgar.record config/test-lora.json) &
+(python -m subt run config/eduro-subt-estop-lora-wifi-jetson.json --side right --speed 0.7 --walldist 0.6 --timeout 300 --start-paused ; python -m osgar.record config/test-lora.json) &
 ssh -t edurojetson "source /opt/ros/melodic/setup.bash;source ~/ros_ws/devel/setup.bash; roslaunch robot eduro.launch"
 wait
 
