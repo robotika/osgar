77a71b12aa6e08b2787c6e8490b292ce220ac1ee
Martin Dlouhy <md@robotika.cz>
2020-03-10 20:54:00 +0100

    Space Robotics Challenge 2 - add dummy skeleton

---
+++ moon/main.py
@@ -0,0 +1,15 @@
+"""
+  Space Robotics Challenge 2
+"""
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Space Robotics Challenge 2')
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
+
+# vim: expandtab sw=4 ts=4
