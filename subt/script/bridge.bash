89c13422cdc53d61e47d5885d568fbc27703becf
Zbynek Winkler <zw@robotika.cz>
2020-03-11 16:51:17 +0100

    Allow running sim.bash and bridge.bash from any dir.

--- subt/script/bridge.bash
+++ subt/script/bridge.bash
@@ -3,7 +3,9 @@
 ROBOT="${ROBOT:-X0F200L}"
 WORLD="${WORLD:-urban_circuit_practice_01}"
 
-./run.bash osrf/subt-virtual-testbed:cloudsim_bridge_latest \
+cd $(git rev-parse --show-toplevel)
+
+./subt/docker/run.bash osrf/subt-virtual-testbed:cloudsim_bridge_latest \
   circuit:=urban \
   worldName:=$WORLD \
   robotName1:=$ROBOT \
--- subt/script/sim.bash
+++ subt/script/sim.bash
@@ -3,7 +3,9 @@
 ROBOT="${ROBOT:-X0F200L}"
 WORLD="${WORLD:-urban_circuit_practice_01}"
 
-./run.bash osrf/subt-virtual-testbed:cloudsim_sim_latest cloudsim_sim.ign \
+cd $(git rev-parse --show-toplevel)
+
+./subt/docker/run.bash osrf/subt-virtual-testbed:cloudsim_sim_latest cloudsim_sim.ign \
   circuit:=urban \
   worldName:=$WORLD \
   robotName1:=$ROBOT \
