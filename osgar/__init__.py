7774adb4996d0bc91aefacb144d699c44d64c14a
Zbynek Winkler <zw@robotika.cz>
2020-03-24 10:54:16 +0100

    Allow `python -m unittest <package>" to work.

--- osgar/__init__.py
+++ osgar/__init__.py
@@ -1,2 +1,4 @@
 __version__ = '0.0.1'
 
+from osgar.lib import create_load_tests
+load_tests = create_load_tests(__file__)
--- osgar/drivers/__init__.py
+++ osgar/drivers/__init__.py
@@ -31,3 +31,6 @@ all_drivers = dict(
     realsense="osgar.drivers.realsense:RealSense",
 )
 
+
+from osgar.lib import create_load_tests
+load_tests = create_load_tests(__file__)
