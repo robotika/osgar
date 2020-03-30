f027afd76d86a9f340368405faf1c0129afe27e0
Pavel Jiroutek <pavel.jiroutek@gmail.com>
2020-03-30 23:40:40 +0200

    first trial of flying drone setup (not working yet)

---
+++ subt/docker/robotika/ros/robotika/src/drone_height.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import rospy
+from nav_msgs.msg import *
+from std_msgs.msg import *
+from sensor_msgs.msg import *
+from geometry_msgs.msg import *
+
+import pdb
+import robomath
+    
+HEIGHT = 1.0
+lastScan = 0.0
+
+def scan_callback(scan):
+    global lastScan, publisherTwist
+    lastScan = scan
+
+def twist_callback(cmd_vel):
+    global HEIGHT, lastScan
+
+    if lastScan > HEIGHT:
+        cmd_vel.linear.z = -0.1
+    else:
+        cmd_vel.linear.z = 0.1
+    
+    publisherTwist.publish(cmd_vel)
+
+rospy.init_node("drone_height",log_level=rospy.DEBUG)
+lastTime = rospy.Time.now()
+subscriberTwist = rospy.Subscriber("/cmd_vel", Twist, twist_callback, queue_size=15)
+subscriberOdom = rospy.Subscriber("/scan", LaserScan, scan_callback, queue_size=15)
+publisherTwist = rospy.Publisher("/cmd_vel_drone", Twist, queue_size=50)
+
+rospy.spin()
+
