4e005f007c9133fb45260b7e905aeea7eae27aab
Zbynek Winkler <zw@robotika.cz>
2020-04-01 21:39:16 +0200

    Do release build of ros_proxy_node.

--- subt/script/sync.sh
+++ subt/script/sync.sh
@@ -3,5 +3,5 @@
 cp osgar/subt/docker/robotika/ros_proxy_node.cc src/subt_seed/src/subt_seed_node.cc
 cp osgar/subt/docker/robotika/CMakeLists.txt src/subt_seed/CMakeLists.txt
 
-catkin_make install
+catkin_make install -DCMAKE_BUILD_TYPE=Release
 
