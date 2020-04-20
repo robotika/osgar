c28a53daa90f7916619342aee289db38c3718896
Frantisek Brabec <brabec@gmail.com>
2020-04-20 16:57:07 -0400

    add linux view script

---
+++ moon/moon-view.sh
@@ -0,0 +1,1 @@
+python3 -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera rosmsg./scout_1/camera/left/image_raw/compressed --camera2 rosmsg./scout_1/camera/right/image_raw/compressed --lidar-limit 20000 --window-size 1200 660 --title rosmsg.rot $1  
