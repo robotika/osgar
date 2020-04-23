be6d472af3f551902176e860f54cfa143dd5b9df
Martin Dlouhy <md@robotika.cz>
2020-04-23 21:53:19 +0200

    SubT - add Torch on CUDA for docker image

--- subt/docker/robotika/entrypoint.bash
+++ subt/docker/robotika/entrypoint.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source ~/subt_solution/devel/setup.sh
+# We need to look for NVidia libraries, but they are not around yet when
+# building the image, so `ldconfig` in Dockerfile is not a working solution.
+# https://gitlab.com/nvidia/container-images/cuda/-/issues/34
+sudo ldconfig
 
 exec "$@"
