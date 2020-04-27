f13f652fc38659558a198f80d66161684e763655
Zbyněk Winkler <zw@robotika.cz>
2020-04-27 13:26:45 +0200

    subt/script/{bridge,sim}: set terminal title (#452)
    Having the terminal title set makes it easier to later find the right tab.

--- subt/script/bridge.bash
+++ subt/script/bridge.bash
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+termtitle() { printf "\033]0;$*\007"; }
+
+trap "kill %1" EXIT
+
 ROBOT="${ROBOT:-X0F200L}"
 WORLD="${WORLD:-urban_circuit_practice_01}"
+
+( while true; do termtitle "bridge $ROBOT $WORLD"; sleep 5; done ) &
 
 cd $(git rev-parse --show-toplevel)
 
