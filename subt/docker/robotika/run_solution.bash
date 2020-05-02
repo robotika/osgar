1c239c2d5bd292fb44650934272ea463809058be
Zbynek Winkler <zw@robotika.cz>
2020-05-02 21:55:10 +0200

    fixup! run_solution.bash: refactor exiting to use SIGINT

--- subt/docker/robotika/run_solution.bash
+++ subt/docker/robotika/run_solution.bash
@@ -34,7 +34,7 @@ roslaunch proxy sim.launch --wait robot_name:=$ROBOT_NAME &
 
 echo "Starting osgar"
 export OSGAR_LOGS=/osgar-ws/logs
-/osgar-ws/env/bin/python3 -m subt run /osgar-ws/src/osgar/subt/zmq-subt-x2.json --side auto --walldist 0.8 --timeout 100 --speed 1.0 --note "run_solution.bash" &
+/osgar-ws/env/bin/python3 -m subt run /osgar-ws/src/osgar/subt/zmq-subt-x2.json --side auto --walldist 0.8 --timeout 100 --speed 1.0 --note "run_solution.bash"
 
 echo "Sleep and finish"
 sleep 30
