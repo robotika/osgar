943ea5a1645cc5bfa1c8e752a57c61a5f38962c9
Zbynek Winkler <zw@robotika.cz>
2020-05-05 13:27:22 +0200

    Make more readable/understandable.

--- subt/docker/cudatest/cloudsim_run.py
+++ subt/docker/cudatest/cloudsim_run.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python2
 
+"""
+In order to run on cloudsim, the solution must call `/subt/start` and `/subt/finish` service. If the solution
+does not call these, cloudsim sees it as an error and restarts the whole simulation. The function `main` here
+provides such a wrapper.
+"""
+
 from __future__ import print_function
 
 import errno
 import socket
 import subprocess
+import sys
 import time
 
 import rospy
@@ -12,6 +19,8 @@ from rospy.impl import rosout
 from std_srvs.srv import SetBool
 
 def wait_for_master():
+    """Return when /use_sim_time is set in rosparams. If rospy.init_node is called before /use_sim_time
+    is set, it uses real time."""
     print("Waiting for rosmaster")
     start = time.time()
     last_params = []
@@ -20,7 +29,7 @@ def wait_for_master():
             params = rospy.get_param_names()
             if params != last_params:
                 print(time.time()-start, params)
-                if '/robot_names' in params:
+                if '/use_sim_time' in params:
                     return True
                 last_params = params
             time.sleep(0.01)
@@ -30,31 +39,37 @@ def wait_for_master():
             time.sleep(0.5)
 
 def main():
+    "Call /subt/start, call sys.argv[1:], call /subt/finish."
     wait_for_master()
     rospy.init_node('cudatest', log_level=rospy.DEBUG)
     rospy.loginfo("waiting for rosout logger")
+    # if rosout is not logged, we will loose anything written to it
     while rosout._rosout_pub.get_num_connections() == 0:
         time.sleep(0.2)
     rospy.loginfo("got rosout logger")
 
+    # we need these services
+    rospy.loginfo("waiting for /subt/start and /subt/finish")
+    rospy.wait_for_service('/subt/start')
+    rospy.wait_for_service('/subt/finish')
+
+    # if clock is not ticking, service /subt/start will return with error
     rospy.loginfo("waiting for clock")
     rospy.sleep(2)
     rospy.loginfo(repr(rospy.get_rostime()))
-    rospy.wait_for_service('/subt/start')
-    rospy.wait_for_service('/subt/finish')
 
     try:
         subt_start = rospy.ServiceProxy('/subt/start', SetBool)
         ret = subt_start(True)
         rospy.loginfo("/subt/start: {0}".format(ret.success))
+        rospy.loginfo("executing {0}".format(sys.argv[1:]))
 
         with open('/dev/null') as devnul:
-            a = subprocess.check_output(["/osgar-ws/env/bin/python", "src/cudatest3.py"], stderr=devnul)
+            a = subprocess.check_output(sys.argv[1:], stderr=devnul)
             for line in a.splitlines():
                 rospy.loginfo(line)
 
     finally:
-        rospy.sleep(2)
         subt_finish = rospy.ServiceProxy('/subt/finish', SetBool)
         ret = subt_finish(True)
         rospy.loginfo("/subt/finish: {0}".format(ret.success))
