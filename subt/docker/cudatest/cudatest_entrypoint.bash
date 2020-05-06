37687faf858937e84d13c2c4e77e9daef146f64d
Zbyněk Winkler <zw@robotika.cz>
2020-05-06 13:32:18 +0200

    cudatest: add ldconfig (#465)
    If the image is run on debian host, it fails due to not being able
    to find all required libraries.

--- subt/docker/cudatest/cudatest_entrypoint.bash
+++ subt/docker/cudatest/cudatest_entrypoint.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# We need to look for NVidia libraries, but they are not around yet when
+# building the image, so `ldconfig` in Dockerfile is not a working solution.
+# https://gitlab.com/nvidia/container-images/cuda/-/issues/34
+sudo ldconfig
+
 echo "source /opt/ros/melodic/setup.sh" >> ~/.bashrc
 
 source /opt/ros/melodic/setup.bash
