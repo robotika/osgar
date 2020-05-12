99da658c31a16c65e5f62f16ad01c04c99195175
tajgr <levjakub@gmail.com>
2020-05-12 17:33:06 +0200

    driver gps: add "try" to "str2ms" function

--- osgar/drivers/gps.py
+++ osgar/drivers/gps.py
@@ -24,7 +24,10 @@ def str2ms(s):
     if s == b'':  # unknown position
         return None
     dm, frac = (b'0000' + s).split(b'.')
-    return round((int(dm[:-2]) * 60 + float(dm[-2:] + b'.' + frac)) * 60000)
+    try:
+        return round((int(dm[:-2]) * 60 + float(dm[-2:] + b'.' + frac)) * 60000)
+    except:
+        return None
 
 
 def parse_line(line):
