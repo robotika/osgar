e405a204749014a7e394331c7c2c82aa5504a2a2
Zbynek Winkler <zw@robotika.cz>
2020-05-14 13:35:13 +0200

    address comments from review

--- subt/simulation.py
+++ subt/simulation.py
@@ -1,51 +1,25 @@
 import collections
 import datetime
 import inspect
+import logging
 import math
+
+import numpy as np
 
 from threading import Thread, RLock
 
 from osgar.lib import quaternion
 
-try:
-    from colored import stylize, fg
-except ImportError:
-    def stylize(what, style):
-        return what
-    def fg(color_name):
-        return ""
 
-_saved_print = print
-
-def verbose(ok):
-    print(ok)
-    if ok:
-        globals()['print'] = _saved_print
-    else:
-        globals()['print'] = lambda *args: None
+g_logger = logging.getLogger(__name__)
 
 
 def log(*args):
     up = inspect.currentframe().f_back
     caller = up.f_code.co_name
     caller_class = up.f_locals['self'].__class__
-    color_name = getattr(caller_class, caller).color
-    print(stylize(f"{caller_class.__name__}.{caller}", fg(color_name)), *args)
-
-
-def clip(min_value, value, max_value):
-    return max(min(value, max_value), min_value)
-
-
-class Color:
-    def __getattr__(self, color_name):
-        def a(method):
-            method.color = color_name
-            return method
-        return a
-
-
-color = Color()
+    msg = f"{caller_class.__name__}.{caller}, " + ", ".join(str(a) for a in args)
+    g_logger.info(msg)
 
 
 class SimLogger:
@@ -59,9 +33,7 @@ class SimLogger:
         self.names.append(name)
         return len(self.names)-1
 
-    @color.dodger_blue_1
     def write(self, stream_id, data, dt=None):
-        #log(self.names[stream_id], data)
         dt = datetime.datetime.now(datetime.timezone.utc) - self.start_time
         return dt
 
@@ -91,7 +63,6 @@ class Simulation:
     def join(self, timeout=None):
         self.thread.join(timeout)
 
-    @color.green
     def main(self):
         log("started")
         self.on_request_origin(datetime.timedelta(), "request_origin", True)
@@ -108,11 +79,9 @@ class Simulation:
     def handle_incomming(self, dt, channel, data):
         self._handlers[channel](dt, channel, data)
 
-    @color.red
     def on_default(self, dt, channel, data):
         log(dt, channel, data)
 
-    @color.gold_1
     def on_request_origin(self, dt, channel, data):
         log(dt, channel, data)
         # start sending origin data until robot arrives within 0.3m from (0,0,0)
@@ -120,7 +89,6 @@ class Simulation:
         self.bus.publish('origin', [b'X0F100L', *corrected, *self.orientation])
         self.bus.publish('scan', [0] * 100)
 
-    @color.turquoise_4
     def on_pose(self, dt, channel, data):
         if channel == 'pose2d':
             out = str(data)
@@ -130,22 +98,22 @@ class Simulation:
             out = out0 + out1
         log(dt, channel, out)
 
-    @color.sea_green_3
     def on_desired_speed(self, dt, channel, data):
-        forward = data[0]/1000.
-        angular = data[1]/100.
-        diff_forward = clip(-0.1, forward - self.speed_forward, 0.1)
-        diff_angular = clip(-10.0, angular - self.speed_angular, 10.0)
-        #print(diff_forward, diff_angular)
-        self.speed_forward += diff_forward
-        self.speed_angular += diff_angular
-        log("desired", f"forward {forward:.2f} m/s", f"angular {angular:.2f} deg/s")
-        log("current", f"forward {self.speed_forward:.2f} m/s", f"angular {self.speed_angular:.2f} deg/s")
-        dtime = 0.1
+        forward = data[0]/1000.               # from millimeters per second to meters per second
+        angular = math.radians(data[1]/100.)  # from centidegrees per second to radians per second
+        dtime = 0.1                           # simulation step time in seconds
+        max_acc_forward = 1                   # meters per sec**2
+        max_acc_angular = 1                   # radians per sec**2
+        acc_forward = np.clip(-max_acc_forward, (forward - self.speed_forward)/dtime, max_acc_forward)
+        acc_angular = np.clip(-max_acc_angular, (angular - self.speed_angular)/dtime, max_acc_angular)
+        self.speed_forward += acc_forward * dtime
+        self.speed_angular += acc_angular * dtime
+        log("desired", f"forward {forward:.2f} m/s", f"angular {math.degrees(angular):.2f} deg/s")
+        log("current", f"forward {self.speed_forward:.2f} m/s", f"angular {math.degrees(self.speed_angular):.2f} deg/s")
         dist = dtime * self.speed_forward
         dist3d = quaternion.rotate_vector([dist, 0, 0], self.orientation)
         self.xyz = [a + b for a, b in zip(self.xyz, dist3d)]
-        turn = quaternion.from_axis_angle((0,0,1), math.radians(dtime * self.speed_angular))
+        turn = quaternion.from_axis_angle((0,0,1), dtime * self.speed_angular)
         self.orientation = quaternion.multiply(self.orientation, turn)
         self.time += datetime.timedelta(seconds=dtime)
         heading_centidegrees = round(math.degrees(quaternion.heading(self.orientation))*100)
@@ -156,4 +124,3 @@ class Simulation:
         self.bus.publish('sim_time_sec', self.time.total_seconds())
         self.bus.publish('scan', [0] * 100)
 
-        #log("++++", x, y, math.degrees(heading))
