bea1e86bc28a669e1501bc5c9b01cc7d30b3a48f
Zbyněk Winkler <zw@robotika.cz>
2020-05-15 10:53:35 +0200

    subt.test_subt: add test_go_to_entrance (#478)

---
+++ subt/simulation.py
@@ -0,0 +1,127 @@
+import collections
+import datetime
+import inspect
+import logging
+import math
+
+import numpy as np
+
+from threading import Thread, RLock
+
+from osgar.lib import quaternion
+
+
+g_logger = logging.getLogger(__name__)
+
+
+def log(*args):
+    up = inspect.currentframe().f_back
+    caller = up.f_code.co_name
+    caller_class = up.f_locals['self'].__class__
+    msg = f"{caller_class.__name__}.{caller}, " + ", ".join(str(a) for a in args)
+    g_logger.info(msg)
+
+
+class SimLogger:
+
+    def __init__(self):
+        self.lock = RLock()
+        self.start_time = datetime.datetime.now(datetime.timezone.utc)
+        self.names = ["sys"]
+
+    def register(self, name):
+        self.names.append(name)
+        return len(self.names)-1
+
+    def write(self, stream_id, data, dt=None):
+        dt = datetime.datetime.now(datetime.timezone.utc) - self.start_time
+        return dt
+
+
+class Simulation:
+
+    def __init__(self, bus, end_condition):
+        bus.register('scan', 'rot', 'orientation', 'sim_time_sec', 'origin', 'pose2d', 'acc', 'artf')
+        self.bus = bus
+        self.end_condition = end_condition
+        self._handlers = collections.defaultdict(lambda: self.on_default)
+        self.set_handler('request_origin', self.on_request_origin)
+        self.set_handler('desired_speed', self.on_desired_speed)
+        self.set_handler('pose2d', self.on_pose)
+        self.set_handler('pose3d', self.on_pose)
+        self.origin = [7.0, 3.0, 0.0]
+        self.xyz = [0.0, 0.0, 0.0]
+        self.orientation = quaternion.identity()
+        self.speed_forward = 0.0
+        self.speed_angular = 0.0
+        self.time = datetime.timedelta()
+
+    def start(self):
+        self.thread = Thread(target=self.main)
+        self.thread.start()
+
+    def join(self, timeout=None):
+        self.thread.join(timeout)
+
+    def main(self):
+        log("started")
+        self.on_request_origin(datetime.timedelta(), "request_origin", True)
+
+        for _ in range(500):
+            packet = self.bus.listen()
+            self.handle_incomming(*packet)
+            if self.end_condition(self):
+                return
+
+    def set_handler(self, channel, handler):
+        self._handlers[channel] = handler
+
+    def handle_incomming(self, dt, channel, data):
+        self._handlers[channel](dt, channel, data)
+
+    def on_default(self, dt, channel, data):
+        log(dt, channel, data)
+
+    def on_request_origin(self, dt, channel, data):
+        log(dt, channel, data)
+        # ros_proxy_node.cc keeps sending origin data until robot arrives within 0.3m from (0,0,0)
+        # we send it only once
+        corrected = [rr - oo for rr, oo in zip(self.xyz, self.origin)]
+        self.bus.publish('origin', [b'X0F100L', *corrected, *self.orientation])
+        self.bus.publish('scan', [0] * 100)
+
+    def on_pose(self, dt, channel, data):
+        if channel == 'pose2d':
+            out = str(data)
+        elif channel == 'pose3d':
+            out0 = "[" + ', '.join([f"{p:.2f}" for p in data[0]]) + "]"
+            out1 = " [" + ', '.join([f"{p:.2f}" for p in data[1]]) + "]"
+            out = out0 + out1
+        log(dt, channel, out)
+
+    def on_desired_speed(self, dt, channel, data):
+        forward = data[0]/1000.               # from millimeters per second to meters per second
+        angular = math.radians(data[1]/100.)  # from centidegrees per second to radians per second
+        dtime = 0.1                           # simulation step time in seconds
+        max_acc_forward = 1                   # meters per sec**2
+        max_acc_angular = 1                   # radians per sec**2
+        acc_forward = np.clip(-max_acc_forward, (forward - self.speed_forward)/dtime, max_acc_forward)
+        acc_angular = np.clip(-max_acc_angular, (angular - self.speed_angular)/dtime, max_acc_angular)
+        self.speed_forward += acc_forward * dtime
+        self.speed_angular += acc_angular * dtime
+        log("desired", f"forward {forward:.2f} m/s", f"angular {math.degrees(angular):.2f} deg/s")
+        log("current", f"forward {self.speed_forward:.2f} m/s", f"angular {math.degrees(self.speed_angular):.2f} deg/s")
+        dist = dtime * self.speed_forward
+        dist3d = quaternion.rotate_vector([dist, 0, 0], self.orientation)
+        self.xyz = [a + b for a, b in zip(self.xyz, dist3d)]
+        turn = quaternion.from_axis_angle((0,0,1), dtime * self.speed_angular)
+        self.orientation = quaternion.multiply(self.orientation, turn)
+        self.time += datetime.timedelta(seconds=dtime)
+        heading_centidegrees = round(math.degrees(quaternion.heading(self.orientation))*100)
+        x_mm, y_mm, z_mm = [round(c*1000) for c in self.xyz]
+        self.bus.publish('orientation', self.orientation)
+        self.bus.publish('rot', [heading_centidegrees, 0, 0])
+        self.bus.publish('pose2d', [x_mm, y_mm, heading_centidegrees])
+        self.bus.publish('sim_time_sec', self.time.total_seconds())
+        self.bus.publish('scan', [0] * 100)
+
--- subt/test_subt.py
+++ subt/test_subt.py
@@ -1,7 +1,23 @@
 import unittest
+import logging
+
 from unittest.mock import MagicMock
 
+from osgar.bus import Bus
 from subt.main import SubTChallenge
+from subt.trace import distance3D
+
+from subt import simulation
+
+g_logger = logging.getLogger(__name__)
+
+
+def entrance_reached(sim):
+    corrected = [(rr - oo) for rr, oo in zip(sim.xyz, sim.origin)]
+    goal = [2.5, 0, 0]
+    if distance3D(corrected, goal) < 2:
+        return True
+    return False
 
 
 class SubTChallengeTest(unittest.TestCase):
@@ -19,6 +35,30 @@ class SubTChallengeTest(unittest.TestCase):
         # 2nd report should be ignored
         self.assertEqual(game.maybe_remember_artifact(artf_data, artf_xyz), False)
 
+    def test_go_to_entrance(self):
+        config = {'virtual_world': True, 'max_speed': 1.0, 'walldist': 0.8, 'timeout': 600, 'symmetric': False, 'right_wall': 'auto'}
+        bus = Bus(simulation.SimLogger())
+        app = SubTChallenge(config, bus.handle('app'))
+        sim = simulation.Simulation(bus.handle('sim'), end_condition=entrance_reached)
+        g_logger.info("connecting:")
+        for o in bus.handle('sim').out:
+            g_logger.info(f'  sim.{o} → app.{o}')
+            bus.connect(f'sim.{o}', f'app.{o}')
+        for o in bus.handle('app').out:
+            g_logger.info(f'  app.{o} → sim.{o}')
+            bus.connect(f'app.{o}', f'sim.{o}')
+        g_logger.info("done.")
+        app.start()
+        sim.start()
+        sim.join()
+        app.request_stop()
+        app.join()
+        self.assertTrue(entrance_reached(sim))
+
+if __name__ == "__main__":
+    import sys
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    unittest.main()
 
 # vim: expandtab sw=4 ts=4
 
