67ec8c63593beced5c6ed0a2405c6bb87824011a
Martin Dlouhy <md@robotika.cz>
2020-05-16 16:17:25 +0200

    moon/skyline.py - find_peaks() and visual debugging tool

--- moon/skyline.py
+++ moon/skyline.py
@@ -4,6 +4,7 @@
 import math
 
 import numpy as np
+import cv2  # for debug
 
 
 def skyline(img):
@@ -14,6 +15,15 @@ def skyline(img):
     return mask.argmax(axis=0)
 
 
+def find_peaks(arr):
+    x = arr.argmin()
+    y = arr[x]
+    ret = []
+    if x > 0:
+        ret.append((x, y))
+    return ret
+
+
 def draw_skyline(img, skyline):
     # TODO optimize - there is surely many times faster version, but this is just for debug
     img2 = img.copy()
@@ -21,7 +31,38 @@ def draw_skyline(img, skyline):
         img2[y, x, 0] = 0
         img2[y, x, 1] = 255
         img2[y, x, 2] = 0
+
+    for x, y in find_peaks(skyline):
+        cv2.drawMarker(img2, (x, y), color=(255, 0, 0), markerType=cv2.MARKER_TRIANGLE_UP, thickness=3)
     return img2
 
+
+def main():
+    import argparse
+
+    from osgar.logger import LogReader, lookup_stream_id
+    from osgar.lib.serialize import deserialize
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('logfile')
+    parser.add_argument('--stream', help='stream name with JPEG images', required=True)
+    args = parser.parse_args()
+
+    only_stream = lookup_stream_id(args.logfile, args.stream)
+    with LogReader(args.logfile, only_stream_id=only_stream) as log:
+        for timestamp, stream_id, data in log:
+            buf = deserialize(data)
+            img = cv2.imdecode(np.fromstring(buf, dtype=np.uint8), 1)
+            s = skyline(img)
+            img2 = draw_skyline(img, s)
+            cv2.imshow('skyline', img2)
+            KEY_Q = ord('q')
+            key = cv2.waitKey(1) & 0xFF
+            if key == KEY_Q:
+                break
+
+
+if __name__ == "__main__":
+    main()
 
 # vim: expandtab sw=4 ts=4
