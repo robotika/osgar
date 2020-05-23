ebae5e2ce60b858db1f819471298003333de0405
Frantisek Brabec <frantisek.brabec@robotika.cz>
2020-05-23 18:56:17 +0200

    rover/run_solution.bash - add parameter to run different rounds

--- moon/docker/rover/run_solution.bash
+++ moon/docker/rover/run_solution.bash
@@ -1,15 +1,50 @@
 #!/usr/bin/env bash
 
+while getopts hr: arg; do
+    case $arg in
+	r)
+	    case $OPTARG in
+		"1" )
+		    JSONFILES=("moon-round1.json")
+		    ROVERSCRIPTS=("rospy_scout_round1.py --robot_name=scout_1 --push_port=5555 --pull_port=5556 --reqrep_port=5557")
+		    ;;
+		"2" )
+		    JSONFILES=("moon-excavator-round2.json" "moon-hauler-round2.json")
+		    ROVERSCRIPTS=("rospy_excavator_round2.py --robot_name=excavator_1 --push_port=5555 --pull_port=5556 --reqrep_port=5557" "rospy_hauler_round2.py --robot_name=hauler_1 --push_port=6555 --pull_port=6556 --reqrep_port=6557")
+		    ;;
+		"3" )
+		    JSONFILES=("moon-round3.json")
+		    ROVERSCRIPTS=("rospy_scout_round3.py --robot_name=scout_1 --push_port=5555 --pull_port=5556 --reqrep_port=5557")
+		;;
+		
+	    esac
+	    ;;
+	h)
+	    echo "Use -r [1|2|3] to choose the run to execute"
+	    exit
+	    ;;
+	*)
+	    exit
+	    ;;
+    esac
+done
+
 echo "Unpause simulation"
 rosservice call /gazebo/unpause_physics "{}"
 echo "wait a moment"
 sleep 5
 
+ROBOT_PIDS=()
+ROS_PIDS=()
 echo "Start robot solution"
 export OSGAR_LOGS=`pwd`
 cd osgar
-python3 -m osgar.record --duration 2700 moon/scout1.json --note "collect some ROS data" &
-ROBOT_PID=$!
+for s in ${JSONFILES[@]}; do
+    echo "starting recording of $s"
+    python3 -m osgar.record --duration 2700 moon/$s --note "collect some ROS data" &
+    ROBOT_PIDS+=($!)
+done
+
 cd ..
 
 # get directory where this bash script lives
@@ -21,26 +56,40 @@ export ROSCONSOLE_CONFIG_FILE="${samedir}/rosconsole.config"
 # Run your solution and wait for ROS master
 # http://wiki.ros.org/roslaunch/Commandline%20Tools#line-45
 ## roslaunch subt_seed x1.launch --wait &
-python ./osgar/moon/rospy_rover.py &
-ROS_PID=$!
+
+for ((i=0; i < ${#ROVERSCRIPTS[@]}; i++)) do
+    echo "Starting script '${ROVERSCRIPTS[$i]}'"
+    python ./osgar/moon/rospy/${ROVERSCRIPTS[$i]} &
+    ROS_PIDS+=($!)
+done
 
 # Turn everything off in case of CTRL+C and friends.
 function shutdown {
-       kill ${ROBOT_PID}
-       kill ${ROS_PID}
-       wait
-       exit
+    for p in ${ROBOT_PIDS[@]}; do
+	kill $p
+    done
+    for p in ${ROS_PIDS[@]}; do
+	kill $p
+    done
+    
+    wait
+    exit
 }
 trap shutdown SIGHUP SIGINT SIGTERM
 
 
 # Wait for the controllers to finish.
-wait ${ROBOT_PID}
+for r in ${ROBOT_PIDS[@]}; do
+    wait $r
+done
+
 
 echo "Sleep and finish"
 sleep 30
 
 # Take robot simulation down.
-kill ${ROS_PID}
+for r in ${ROS_PIDS[@]}; do
+    wait $r
+done
 wait
 
