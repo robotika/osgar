3e1b8a835e545fe75defd76ff7d48601277d7d06
Martin Dlouhy <md@robotika.cz>
2020-05-25 06:45:55 +0200

    Add dummy test_validator.py

---
+++ subt/tools/test_validator.py
@@ -0,0 +1,11 @@
+import unittest
+
+from subt.tools.validator import evaluate_poses
+
+
+class ValidatorTest(unittest.TestCase):
+
+    def test_evaluate_poses(self):
+        pass
+
+# vim: expandtab sw=4 ts=4
