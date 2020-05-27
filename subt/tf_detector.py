0da82d0cad4a6d821a2f994eb0938af9ea054bd8
tajgr <levjakub@gmail.com>
2020-05-27 19:54:35 +0200

    subt/tf_detector: Adaptation for ssd_inception_v2_coco model

--- subt/tf_detector.py
+++ subt/tf_detector.py
@@ -1,10 +1,11 @@
 import sys
+import os
 import tensorflow as tf
 import numpy as np
 import cv2
 
-
-MODEL_DIR = "tf_models"
+# Directory with files saved_model.pb and saved_model.pbtxt
+MODEL_DIR = "subt/tf_models"
 
 class Tf_detector:
     def __init__(self):
@@ -12,44 +13,50 @@ class Tf_detector:
         self.model = model.signatures['serving_default']
 #        print(self.model.inputs)
         self.model.output_dtypes
-        self.artf_names = np.array(["backpack", "survivor", "phone"])
+        self.artf_names = np.array(["backpack", "survivor", "phone", "rope", "helmet"])
 
 
     def detect(self, img):
         rows = img.shape[0]
         cols = img.shape[1]
-#        image = np.asarray(img)
         input_tensor = tf.convert_to_tensor(img)
         input_tensor = input_tensor[tf.newaxis, ...]
         raw_dict = self.model(input_tensor)
 
-        num_detections = int(raw_dict['num_detections'])
+        scores = raw_dict['detection_scores'][0, :].numpy()
+#        print("scores: ", scores)
+        mask = scores > 0.1
+        num_detections = np.count_nonzero(mask)
         if num_detections > 0:
-            print(num_detections)
-            bboxes = raw_dict['detection_boxes'][0,:num_detections].numpy()
+            scores = scores[mask]
+            bboxes = raw_dict['detection_boxes'][0, :].numpy()[mask]
             bboxes = bboxes*np.array([rows, cols, rows, cols])
             bboxes = bboxes.astype('int32')
-            classes = raw_dict['detection_classes'][0, :num_detections].numpy() - 1
+            classes = raw_dict['detection_classes'][0, :].numpy()[mask] - 1
             classes = classes.astype('int32')
-            scores = raw_dict['detection_scores'][0, :num_detections].numpy()
             classes_names = self.artf_names[classes]
 
             return { 'bboxes': bboxes, 'classes_names': classes_names, 'scores': scores, 'num_detections': num_detections }
 
         return None
 
-    def show_result(self, img, detection):
-        num_detections = detection['num_detections']
-        for ii in range(num_detections):
-            y, x, h, w = detection['bboxes'][ii]
-            score = detection['scores'][ii]
-            name = detection['classes_names'][ii]
-            cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), thickness=2)
-            print(x, y)
-            cv2.putText(img, name+' '+str(score), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
 
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
+    def show_result(self, img, detection):
+        if detection:
+            num_detections = detection['num_detections']
+            for ii in range(num_detections):
+                y, x, h, w = detection['bboxes'][ii]
+                score = detection['scores'][ii]
+                name = detection['classes_names'][ii]
+                cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), thickness=2)
+                cv2.putText(img, name+' '+str(score), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+
+            cv2.imshow('img', img)
+            cv2.waitKey(1000)
+        else:
+            cv2.imshow('img', img)
+            cv2.waitKey(1)
+
 
 if __name__ == "__main__":
     detector = Tf_detector()
@@ -58,14 +65,26 @@ if __name__ == "__main__":
         while True:
             __, img = cam.read()
             bbox = detector.detect(img)
-            print("frame")
-            if bbox is not None:
-                detector.show_result(img, bbox)
+            print(bbox)
+            detector.show_result(img, bbox)
 
-    img = cv2.imread(sys.argv[1])
-    if img is not None:
-        detection = detector.detect(img)
-        if detection is not None:
-            detector.show_result(img, detection)
+    path = sys.argv[1]
+    if os.path.isdir(path):
+        im_file_list = os.listdir(path)
+        for im_file in im_file_list:
+            im_path = os.path.join(path, im_file)
+            img = cv2.imread(im_path)
+            if img is not None:
+                detection = detector.detect(img)
+                if detection:
+                    print(detection)
+                detector.show_result(img, detection)
+
     else:
-        print("no image")
+        img = cv2.imread(path)
+        if img is not None:
+            detection = detector.detect(img)
+            if detection is not None:
+                detector.show_result(img, detection)
+        else:
+            print("no image")
