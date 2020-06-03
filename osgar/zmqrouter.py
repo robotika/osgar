e22ae844549d533283b1a83324c6ebf0931fe8d2
Zbynek Winkler <zw@robotika.cz>
2020-06-03 15:53:22 +0200

    osgar.zmqrouter: refactoring

--- osgar/zmqrouter.py
+++ osgar/zmqrouter.py
@@ -38,8 +38,10 @@ def record(config, log_prefix, log_filename=None, duration_sec=None):
             module_config['name'] = module_name
             modules[module_name] = subprocess.Popen([sys.executable, __file__, str(module_config)])
 
-        router.connect(len(modules), config['robot']['links'])
-        #pprint(router.subscriptions)
+        router.register_nodes(modules.keys())
+        links =  config['robot']['links']
+        for link_from, link_to in links:
+            router.connect(link_from, link_to)
         router.run()
 
         for module in modules.values():
@@ -50,19 +52,19 @@ class Router:
     def __init__(self):
         self.start_time = datetime.datetime.now(datetime.timezone.utc)
         self._context = zmq.Context()
-        self.s = self._context.socket(zmq.ROUTER)
+        self.socket = self._context.socket(zmq.ROUTER)
         self.nodes = dict()
         self.subscriptions = dict()
         self.listening = set()
         self.stopping = datetime.timedelta()
 
     def __enter__(self):
-        self.s.bind("tcp://127.0.0.1:8881")
+        self.socket.bind("tcp://127.0.0.1:8881")
         signal.signal(signal.SIGINT, self.sigint)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.s.close()
+        self.socket.close()
         self._context.term()
 
     def sigint(self, signum, frame):
@@ -70,34 +72,35 @@ class Router:
         # use Event? what is ok to do from a signal handler?
         self.request_stop(b'ctrl-c')
 
-    def connect(self, num, links):
+    def register_nodes(self, nodes):
         # wait for all nodes to "register" their outputs
-        for _ in range(num):
-            packet = self.s.recv_multipart() # TODO handle timeout
-            sender, _, action, *args = packet
-            assert action == b"register", (sender, action, args)
+        nodes = [bytes(node_name, "ascii") for node_name in nodes]
+        for _ in range(len(nodes)):
+            packet = self.socket.recv_multipart()  # TODO handle timeout
+            sender, _, action, *outputs = packet
+            assert action == b"register", (sender, action, outputs)
             assert sender not in self.nodes
+            assert sender in nodes, (sender, nodes)
+            # receiving queue
             self.nodes[sender] = collections.deque()
-        
-        for sender, receiver in links:
-            sender = sender.encode("ascii")
-            receiver = receiver.encode("ascii")
-            receiver, input = receiver.split(b'.')
-            self.subscriptions.setdefault(sender, []).append((receiver, input))
 
-        # answer all connected nodes that it is ok to run now (connections are set up)
-        for node_name in self.nodes:
-            self.s.send_multipart([node_name, b"", b"register"])
+    def connect(self, link_from, link_to):
+        link_from = bytes(link_from, "ascii")
+        assert link_from in self.stream_id, self.stream_id
+        receiver, channel = bytes(link_to, "ascii").split(b".")
+        self.subscriptions.setdefault(link_from, []).append((receiver, channel))
 
     def run(self):
+        # answer all connected nodes that it is ok to run now (connections are set up)
+        for node_name in self.nodes:
+            self.socket.send_multipart([node_name, b"", b"start"])
+
         for packet in self.receive():
             sender, action, *args = packet
             getattr(self, action.decode('ascii'))(sender, *args)
-        #for node_name in self.nodes:
-        #    self.send(node_name, b"quit")
 
     def receive(self, hz=10):
-        socket = self.s
+        socket = self.socket
         poller = zmq.Poller()
         poller.register(socket, zmq.POLLIN)
         while not self.stopping or self._now() - self.stopping < datetime.timedelta(seconds=1):
@@ -115,12 +118,13 @@ class Router:
         else:
             packet = queue.popleft()
             assert packet[0] == sender, (packet[0], sender)
-            self.s.send_multipart(packet)
+            self.socket.send_multipart(packet)
 
     def publish(self, sender, channel, data):
         dt_uint32 = self._pack_timestamp(self._now())
-        self.s.send_multipart([sender, b"", b"publish", dt_uint32])
-        for node_name, input_channel in self.subscriptions.get(sender+b"."+channel, []):
+        self.socket.send_multipart([sender, b"", b"publish", dt_uint32])
+        link_from = sender + b"." + channel
+        for node_name, input_channel in self.subscriptions.get(link_from, []):
             self.send(node_name, b'listen', dt_uint32, input_channel, data)
     
     def request_stop(self, sender):
@@ -133,13 +137,13 @@ class Router:
 
     def is_alive(self, sender):
         dt_uint32 = self._pack_timestamp(self._now())
-        self.s.send_multipart([sender, b"", b"quit" if self.stopping else b"is_alive", dt_uint32])
+        self.socket.send_multipart([sender, b"", b"quit" if self.stopping else b"is_alive", dt_uint32])
 
     def send(self, node_name, *args):
         packet = [node_name, b"", *args]
         if node_name in self.listening:
             self.listening.remove(node_name)
-            self.s.send_multipart(packet)
+            self.socket.send_multipart(packet)
         else:
             self.nodes[node_name].append(packet)
 
@@ -164,7 +168,7 @@ class Bus:
         bytes_outputs = list(o.encode('ascii') for o in outputs)
         self.sock.send_multipart([b"register", *bytes_outputs])
         resp = self.sock.recv()
-        assert resp == b'register'
+        assert resp == b'start'
 
     def listen(self):
         self.sock.send(b"listen")
@@ -200,7 +204,6 @@ class Bus:
         return True
 
     def _quit(self):
-        self.quit = True
         g_logger.info(f"{self.name} received quit")
         return SystemExit()
 
