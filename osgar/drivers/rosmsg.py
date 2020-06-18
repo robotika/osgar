a951eb9f1aa398c52b01f12b3d14f515ea4e3f45
Martin Dlouhy <md@robotika.cz>
2020-06-18 12:43:14 +0200

    rosmsg.py - implement optional ROS topic rename

--- osgar/drivers/rosmsg.py
+++ osgar/drivers/rosmsg.py
@@ -428,8 +428,12 @@ class ROSMsgParser(Thread):
                    "depth:gz", "t265_rot", "orientation", "debug",
                     "joint_name", "joint_position", "joint_velocity", "joint_effort"]
         self.topics = config.get('topics', [])
-        for topic_name, topic_type in self.topics:
-            outputs.append(topic_name)
+        for row in self.topics:
+            topic_name, topic_type = row[:2]
+            if len(row) > 2:  # extra rename
+                outputs.append(row[2])
+            else:
+                outputs.append(topic_name)  # default use topic name
         bus.register(*outputs)
 
         self.bus = bus
@@ -540,10 +544,13 @@ class ROSMsgParser(Thread):
                 self.bus.publish('gas_detected', self.gas_detected)
 
         elif b'\0' in packet[:MAX_TOPIC_NAME_LENGTH]:
-            name = packet[:packet.index(b'\0')].decode('ascii')
-            for n, t in self.topics:
-                if name == n:
-                    result = parse_topic(t, packet[len(name) + 1:])
+            ros_name = packet[:packet.index(b'\0')].decode('ascii')
+            for row in self.topics:
+                n, t = row[:2]
+                new_name = n if len(row) == 2 else row[2]
+
+                if ros_name == n:
+                    result = parse_topic(t, packet[len(ros_name) + 1:])
                     if t == 'sensor_msgs/JointState':
                         name, position, velocity, effort = result
 
@@ -556,7 +563,7 @@ class ROSMsgParser(Thread):
                         self.bus.publish('joint_velocity', list(velocity))
                         self.bus.publish('joint_effort', list(effort))
                     else:
-                        self.bus.publish(name, result)
+                        self.bus.publish(new_name, result)
 
     def slot_desired_speed(self, timestamp, data):
         self.desired_speed, self.desired_angular_speed = data[0]/1000.0, math.radians(data[1]/100.0)
