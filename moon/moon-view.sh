49b2f47ddec873e224461bfe4b5bd63e8b45b1db
Martin Dlouhy <md@robotika.cz>
2020-06-19 18:19:04 +0200

    Update moon/moon-view.sh
    fixup
    
    Co-authored-by: frantisekbrabec <frantisek.brabec@robotika.cz>

--- moon/moon-view.sh
+++ moon/moon-view.sh
@@ -1,1 +1,1 @@
-python3 -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera rosmsg.left_image --camera2 rosmsg.right_image_raw --lidar-limit 20000 --window-size 1200 660 --title rosmsg.rot $1  
+python3 -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera rosmsg.left_image --camera2 rosmsg.right_image --lidar-limit 20000 --window-size 1200 660 --title rosmsg.rot $1  
