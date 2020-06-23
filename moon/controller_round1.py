0fe5dd3a3447ac599ead436f743341a217df2886
frantisekbrabec <frantisek.brabec@robotika.cz>
2020-06-23 18:22:47 -0400

    Bugfix/moon post v1 merge fixes (#554)
    * fix format of set camera angle command
    
    * timestamp parameter no longer used
    
    * decrease effort in response to NASA rover model properties change

--- moon/controller_round1.py
+++ moon/controller_round1.py
@@ -9,11 +9,11 @@ class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
     def __init__(self, config, bus):
         super().__init__(config, bus)
 
-    def on_object_reached(self, timestamp, data):
+    def on_object_reached(self, data):
         object_type = data
         x,y,z = self.xyz
         print(self.time, "app: Object %s reached" % object_type)
-        response = self.send_request('artf %s %f %f 0.0\n' % (object_type, x, y)).decode("ascii") 
+        response = self.send_request('artf %s %f %f 0.0\n' % (object_type, x, y)).decode("ascii")
         print(self.time, "app: Volatile report response: %s" % response)
         if response == 'ok':
             pass
@@ -22,7 +22,7 @@ class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
             pass
 
     def run(self):
-        message = self.send_request('request_origin').decode("ascii") 
+        message = self.send_request('request_origin').decode("ascii")
         print ("controller round 1: origin received: %s" % message)
 
         super().run()
