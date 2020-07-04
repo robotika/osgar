42c1506ce19c2057d50a0e0ee14e3c535d4dd646
tajgr <levjakub@gmail.com>
2020-07-04 17:24:04 +0200

    osgar/tools/lidarview.py: add run_on_image function, use argparse instead of sys.argv

--- subt/tf_detector.py
+++ subt/tf_detector.py
@@ -1,4 +1,5 @@
-import sys
+"""Artefacts detection via tensorflow"""
+
 import os
 import tensorflow as tf
 import numpy as np
@@ -14,7 +15,7 @@ class TfDetector:
 #        print(self.model.inputs)
         self.model.output_dtypes
         self.artf_names = np.array(["backpack", "survivor", "phone", "rope", "helmet"])
-        self.min_scores = np.array([0.2, 0.8, 0.2, 0.8, 0.2])
+        self.min_scores = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
 
 
     def detect(self, img):
@@ -29,7 +30,6 @@ class TfDetector:
         classes = classes.astype('int32')
         mask = self.min_scores[classes] < scores
 
-#        print("scores: ", scores)
         num_detections = np.count_nonzero(mask)
         if num_detections > 0:
             scores = scores[mask]
@@ -44,7 +44,8 @@ class TfDetector:
         return None
 
 
-    def show_result(self, img, detection, ret_img = False):
+    def run_on_image(self, img):
+        detection = self.detect(img)
         if detection:
             num_detections = detection['num_detections']
             for ii in range(num_detections):
@@ -52,71 +53,69 @@ class TfDetector:
                 score = detection['scores'][ii]
                 name = detection['classes_names'][ii]
                 cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), thickness=2)
-                cv2.putText(img, name+' '+str(score), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
-            if not ret_img:
-                cv2.imshow('img', img)
-                cv2.waitKey(1000)
-        elif not ret_img:
-            cv2.imshow('img', img)
-            cv2.waitKey(1)
-
+                cv2.putText(img, "%s %0.3f " %(name, score), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1,
+                            cv2.LINE_AA)
         return img
 
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--path', help='path to log file, image or directory wit images')
+    parser.add_argument('--stream', help='image stream', default='camera.raw')
+    parser.add_argument('--cam', help='test with notebook camera', action='store_true')
+    args = parser.parse_args()
+
     detector = TfDetector()
-    if sys.argv[1] == "cam":
+    if args.cam:
         cam = cv2.VideoCapture(0)
         while True:
             __, img = cam.read()
-            bbox = detector.detect(img)
-            print(bbox)
-            detector.show_result(img, bbox)
+            img = detector.run_on_image(img)
+            cv2.imshow('img', img)
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord("q"):
+                cv2.destroyAllWindows()
+                break
+        cam.release()
 
-    path = sys.argv[1]
-    if os.path.isdir(path):
-        im_file_list = os.listdir(path)
-        for im_file in im_file_list:
-            im_path = os.path.join(path, im_file)
-            img = cv2.imread(im_path)
+    if args.path:
+        path = args.path
+        if os.path.isdir(path):
+            im_file_list = os.listdir(path)
+            for im_file in im_file_list:
+                im_path = os.path.join(path, im_file)
+                img = cv2.imread(im_path)
+                if img is not None:
+                    img = detector.run_on_image(img)
+                    cv2.imshow('img', img)
+                    k = cv2.waitKey(500) & 0xFF
+                    if k == ord("q"):
+                        cv2.destroyAllWindows()
+                        break
+
+        elif path.endswith(".log"):
+            from osgar.logger import LogReader, lookup_stream_id
+            from osgar.lib.serialize import deserialize
+            stream_name = args.stream
+            only_stream = lookup_stream_id(path, stream_name)
+            with LogReader(path, only_stream_id=only_stream) as log:
+                for timestamp, stream_id, data_raw in log:
+                    buf_color = deserialize(data_raw)
+                    img = cv2.imdecode(np.frombuffer(buf_color, dtype=np.uint8), 1)
+                    img = detector.run_on_image(img)
+                    cv2.imshow('img', img)
+                    k = cv2.waitKey(200) & 0xFF
+                    if k == ord("q"):
+                        cv2.destroyAllWindows()
+                        break
+
+        else:
+            img = cv2.imread(path)
             if img is not None:
-                detection = detector.detect(img)
-                if detection:
-                    print(detection)
-                detector.show_result(img, detection)
-
-    elif path.endswith(".log"):
-        from osgar.logger import LogReader, lookup_stream_id
-        from osgar.lib.serialize import deserialize
-        import time
-        proc_time = []
-
-        if len(sys.argv) == 3:
-            stream_name = sys.argv[2]
-        else:
-            stream_name = "camera.raw"
-
-        only_stream = lookup_stream_id(path, stream_name)
-        with LogReader(path, only_stream_id=only_stream) as log:
-            ii = 0
-            for timestamp, stream_id, data_raw in log:
-                buf_color = deserialize(data_raw)
-                img = cv2.imdecode(np.fromstring(buf_color, dtype=np.uint8), 1)
-                t0 = time.time()
-                detection = detector.detect(img)
-                proc_time.append(time.time() - t0)
-                if detection:
-                    print(detection)
-#                    cv2.imwrite("tmp/im%05d.png" %ii, img)
-                detector.show_result(img, detection)
-                ii += 1
-            print("Average processing time: ", np.mean(proc_time))
-
-    else:
-        img = cv2.imread(path)
-        if img is not None:
-            detection = detector.detect(img)
-            if detection is not None:
-                detector.show_result(img, detection)
-        else:
-            print("no image")
+                img = detector.run_on_image(img)
+                cv2.imshow('img', img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+            else:
+                print("no image")
