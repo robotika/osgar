3e0091934d359a8370995e79d1d84769199f844d
Martin Dlouhy <md@robotika.cz>
2020-07-10 21:39:37 +0200

    motorpid.py - change API to remember last current_speed

--- moon/motorpid.py
+++ moon/motorpid.py
@@ -5,12 +5,17 @@
 class MotorPID:
     def __init__(self, p=1, i=0, d=0):
         self.desired_speed = 0.0
+        self.current_speed = 0.0
         self.param_p = p
 
     def set_desired_speed(self, speed):
         self.desired_speed = speed
 
     def update(self, current_speed):
-        return self.param_p * (self.desired_speed - current_speed)
+        self.current_speed = current_speed
+
+    def get_effort(self):
+        err = self.desired_speed - self.current_speed
+        return self.param_p * err
 
 # vim: expandtab sw=4 ts=4
--- moon/test_motorpid.py
+++ moon/test_motorpid.py
@@ -8,7 +8,8 @@ class MotorPIDTest(unittest.TestCase):
     def test_pid(self):
         pid = MotorPID(p=1, i=0, d=0)
         pid.set_desired_speed(0.5)
-        effort = pid.update(current_speed=0)
+        pid.update(current_speed=0)
+        effort = pid.get_effort()
         self.assertEqual(effort, 0.5)
 
 
