fb71e7d28b83871620141f66c65ff0da1d4223c6
Martin Dlouhy <md@robotika.cz>
2020-07-10 21:07:06 +0200

    moon/vehicles/test_rover.py - add test for get_steering_and_effort()

--- moon/vehicles/test_rover.py
+++ moon/vehicles/test_rover.py
@@ -9,6 +9,7 @@ JOINT_NAME = [b'bl_arm_joint', b'bl_steering_arm_joint', b'bl_wheel_joint',
         b'fl_steering_arm_joint', b'fl_wheel_joint', b'fr_arm_joint', b'fr_steering_arm_joint',
         b'fr_wheel_joint', b'sensor_joint']
 
+
 class RoverTest(unittest.TestCase):
 
     def test_rover_pid(self):
@@ -16,6 +17,16 @@ class RoverTest(unittest.TestCase):
 
         rover.joint_name = JOINT_NAME  # in reality received as the first joint status
         rover.on_joint_effort([0]*len(JOINT_NAME))
+
+    def test_get_steering_and_effort(self):
+        rover = Rover(config={}, bus=MagicMock())
+        self.assertEqual(rover.get_steering_and_effort(), ([0.0, 0.0, 0.0, 0.0], [0, 0, 0, 0]))
+
+        rover.drive_speed = 1.0
+        self.assertEqual(rover.get_steering_and_effort(), ([0.0, 0.0, 0.0, 0.0], [40, 40, 40, 40]))
+
+        rover.drive_speed = -1.0
+        self.assertEqual(rover.get_steering_and_effort(), ([0.0, 0.0, 0.0, 0.0], [-40, -40, -40, -40]))
 
 # vim: expandtab sw=4 ts=4
 
