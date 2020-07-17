7f2ef0718828b052833564589a8877410ecd0271
Frantisek Brabec <frantisek.brabec@robotika.cz>
2020-07-17 16:20:45 -0400

    add an explanatory comment

--- moon/test_artifacts.py
+++ moon/test_artifacts.py
@@ -28,6 +28,8 @@ class ArtifactTest(unittest.TestCase):
 
         with open(str(curdir/'test_data/cube_homebase.jpg'), mode='rb') as img_h:
             img = img_h.read()
+
+        # classifier detector returns one of two matches for this image, allow both
         self.is_in_test(detector.detect(img, img), [[('cubesat', 143, 24, 35, 40, 356)], [('homebase', 180, 185, 93, 93, 6264), ('homebase', 176, 140, 117, 114, 7158)], [('homebase', 202, 163, 71, 41, 1594)]])
 
         with open(str(curdir/'test_data/basemarker.jpg'), mode='rb') as img_h:
