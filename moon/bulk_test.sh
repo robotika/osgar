efd2665b5a8beef14ae152b52f9a6f1d97151cdb
Frantisek Brabec <frantisek.brabec@robotika.cz>
2020-07-19 13:36:21 -0400

    add an option to repeat each seed multiple times add a fail string (terminate run as unsuccessful if this string appears)

--- moon/bulk_test.sh
+++ moon/bulk_test.sh
@@ -18,7 +18,9 @@ UNSUCCESS_LOGS_ONLY=0
 
 LOG_DIR_PREFIX="."
 
-while getopts "r:t:s:f:l:hw:ud:" opt; do
+REPEAT_SEED=1
+
+while getopts "r:t:s:f:l:hw:ud:p:" opt; do
     case ${opt} in
         h)
             echo "Usage:"
@@ -30,6 +32,7 @@ while getopts "r:t:s:f:l:hw:ud:" opt; do
             echo " -w <SRCP2 simulator startup wait>"
             echo " -u .. only record unsuccessful run logs"
             echo " -d <log dir prefix>"
+            echo " -p <times to repeat each seed>"
             exit 0
             ;;
         r)
@@ -56,6 +59,9 @@ while getopts "r:t:s:f:l:hw:ud:" opt; do
         d)
             LOG_DIR_PREFIX=$OPTARG
             ;;
+        p)
+            REPEAT_SEED=$OPTARG
+            ;;
     esac
 done
 
@@ -69,38 +75,63 @@ if (docker ps | grep -q scheducation); then
     exit 1
 fi
 
+SUCCESS_STRING="Score: ${SCORE}"
+case ${ROUND} in
+    1)
+        FAIL_STRING="<none>"
+        FAIL_SWITCH=""
+        ;;
+    2)
+        FAIL_STRING="<none>"
+        FAIL_SWITCH=""
+        ;;
+    3)
+        FAIL_STRING="object location incorrect"
+        FAIL_SWITCH="-e \"${FAIL_STRING}\""
+        ;;
+esac
 
 now=$(date +"%m%d%Y_%H%M")
 log_dir="${LOG_DIR_PREFIX}/bulklogs_${now}"
 mkdir ${log_dir}
 
-echo "Round: ${ROUND}, Score: ${SCORE}; Seed range: ${RANGE_START}-${RANGE_END}; Timeout(min): `expr ${TIMEOUT} / 60`; Unsuccessful logs only: ${UNSUCCESS_LOGS_ONLY}; Log dir: ${log_dir}"
+
+
+echo "Round: ${ROUND}, Score: ${SCORE}; Fail String: ${FAIL_STRING}; Seed range: ${RANGE_START}-${RANGE_END}; Repeat seeds: ${REPEAT_SEED}; Timeout(min): `expr ${TIMEOUT} / 60`; Unsuccessful logs only: ${UNSUCCESS_LOGS_ONLY}; Log dir: ${log_dir}"
 
 for i in $(seq $RANGE_START $RANGE_END)
 do
 
-    echo "==========" `date` "====== seed:" $i
-    $HOME/space-challenge/srcp2-competitors/docker/scripts/launch/roslaunch_docker -q -n --run-round $ROUND -s $i >& /dev/null
+    for j in $(seq 1 $REPEAT_SEED)
+    do
 
-    # wait for SRCP2 simulator to start
-    sleep ${SRCP2_WAIT}
+        log_file=${log_dir}/osgar-tty-${now}-seed_${i}_run_${j}.txt
 
-    osgar_image_id=`docker image ls | grep rover | grep latest | awk '{print $3}'`
-    srcp2_image_id=`docker image ls | grep comp | head -1 | awk '{print $3}'`
-    echo "Running OSGAR image ${osgar_image_id}" >> ${log_dir}/osgar-tty-${now}-seed_${i}.txt
-    echo "Running SRCP2 image ${srcp2_image_id}" >> ${log_dir}/osgar-tty-${now}-seed_${i}.txt
+        echo "==========" `date` "====== seed:" $i
+        $HOME/space-challenge/srcp2-competitors/docker/scripts/launch/roslaunch_docker -q -n --run-round $ROUND -s ${i} >& /dev/null
 
-    timeout $TIMEOUT sh -c "( docker run --rm --network=host -t --name nasa-rover rover:latest /osgar/moon/docker/rover/run_solution.bash -r $ROUND &) | tee -a ${log_dir}/osgar-tty-${now}-seed_${i}.txt | grep -m 1 \"Score: $SCORE\""
-    exit_status=$?
-    if [[ $exit_status -eq 124 || $UNSUCCESS_LOGS_ONLY -eq 0 ]]; then
-        for f in $(docker exec -it nasa-rover /bin/bash -c "ls /*.log"); do
-            f=`echo $f | sed 's/\r//g'`
-            readarray -d . -t strarr <<< "$f"
-            docker cp nasa-rover:$f ${log_dir}/${strarr[0]}-seed_${i}.${strarr[1]};
-        done
-    fi
+        # wait for SRCP2 simulator to start
+        sleep ${SRCP2_WAIT}
 
-    docker kill nasa-rover >& /dev/null
-    docker kill srcp2-simulation >& /dev/null
+        osgar_image_id=`docker image ls | grep rover | grep latest | awk '{print $3}'`
+        srcp2_image_id=`docker image ls | grep comp | head -1 | awk '{print $3}'`
+        echo "Running OSGAR image ${osgar_image_id}" >> ${log_file}
+        echo "Running SRCP2 image ${srcp2_image_id}" >> ${log_file}
 
+        timeout $TIMEOUT sh -c "( docker run --rm --network=host -t --name nasa-rover rover:latest /osgar/moon/docker/rover/run_solution.bash -r $ROUND &) | tee -a ${log_file} | grep -m 1 ${FAIL_SWITCH} -e \"${SUCCESS_STRING}\""
+        # timeout exit status 124 means the timeout limit was reached
+        timeout_status=$?
+        grep -q "${SUCCESS_STRING}" < ${log_file}
+        was_found=$?
+        if [[ $timeout_status -eq 124 || $was_found -ne 0 || $UNSUCCESS_LOGS_ONLY -eq 0 ]]; then
+            for f in $(docker exec -it nasa-rover /bin/bash -c "ls /*.log"); do
+                f=`echo $f | sed 's/\r//g'`
+                readarray -d . -t strarr <<< "$f"
+                docker cp nasa-rover:$f ${log_dir}/${strarr[0]}-seed_${i}_run_${j}.${strarr[1]};
+            done
+        fi
+
+        docker kill nasa-rover >& /dev/null
+        docker kill srcp2-simulation >& /dev/null
+    done
 done
