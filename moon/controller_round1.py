eddf8ad8c32ab598e4d335971d2f3b08a7ef3b78
Frantisek Brabec <frantisek.brabec@robotika.cz>
2020-07-28 18:40:11 -0400

    utilize default effort to slow driving down compared to maximum speed; establish some basic search patterns and exception handling for R1

--- moon/controller_round1.py
+++ moon/controller_round1.py
@@ -14,12 +14,12 @@ from osgar.lib.virtual_bumper import VirtualBumper
 from moon.controller import (SpaceRoboticsChallenge, VirtualBumperException, ChangeDriverException,  VSLAMLostException, VSLAMFoundException,
                              LidarCollisionException, LidarCollisionMonitor)
 
-SPEED_ON = 10 # only +/0/- matters
-
 class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
     def __init__(self, config, bus):
         super().__init__(config, bus)
         self.use_gimbal = False
+
+        self.default_effort_level = 800 # out of 1000; indicates how much effort compared to max should be used on driving
 
         self.volatile_queue = [] # each element is an array [index, name x, y]
         self.volatile_last_submit_time = None
@@ -99,20 +99,62 @@ class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
 
         # chord length=2*sqrt(h * (2* radius - h)) where h is the distance from the circle boundary
         # https://mathworld.wolfram.com/CircularSegment.html
+        # TODO: 3d gimbal - point camera in the same direction rich with landmarks
 
         # generate sweep trajectory for searching a square
-        sweep_steps = []
+        stripes = [
+
+            # FULL SWEEP BEGIN
+            #            [-65,-55], [43,-55],
+            #            [-25,-42], [43,-42],
+            #            [-25,-35], [65,-35],
+            #            [-65,-5], [65,-5],
+            #            [-65,20],[65,20],
+            #            [-65,25],[40,25]
+            # FULL SWEEP END
+
+            # DONT GO TOO CLOSE TO EDGES
+#            [-65,-56], [42,-56],
+            [-25,-42], [42,-42],
+            [-25,-35], [42,-35],
+            [-65,-5], [42,-5],
+            [-65,20],[42,20],
+            [-65,25],[40,25]
+
+        ]
+
         current_sweep_step = 0
-        x = [-26, 44]
-        i = 1
-        sweep_steps.append(["goto", [-11,-10, SPEED_ON]])
-        sweep_steps.append(["turn", math.radians(90)])
-        sweep_steps.append(["turn", math.radians(-180)])
-        sweep_steps.append(["goto", [-27, -50, SPEED_ON]])
-        for y in range (-50, 40, 4):
-            sweep_steps.append(["goto", [x[i], y, SPEED_ON]])
-            sweep_steps.append(["goto", [x[1-i], y, -SPEED_ON]])
-            sweep_steps.append(["goside", [4, 0]])
+        sweep_steps = []
+        #sweep_steps.append(["goto", [-11,-10, -SPEED_ON]]) # back into the corner so that it keeps an eye on landmarks
+        #sweep_steps.append(["goto", [-14,-40, -SPEED_ON]]) # back into the corner so that it keeps an eye on landmarks
+        #sweep_steps.append(["turn", math.radians(90)])
+        #sweep_steps.append(["turn", math.radians(-180)])
+        sweep_steps.append(["goto", [stripes[0][0], stripes[0][1], -self.default_effort_level]])
+        #self.processing_plant_found = True
+        p = 0
+        while p < len(stripes) - 2:
+            i = 0
+            while stripes[p][1] + i < stripes[p+2][1]:
+                sweep_steps.append(["goto", [stripes[p+1][0], stripes[p+1][1] + i, self.default_effort_level]])
+                sweep_steps.append(["goside", [3, 0]])
+                i = i + 3
+                sweep_steps.append(["goto", [stripes[p][0], stripes[p][1] + i, -self.default_effort_level]])
+                sweep_steps.append(["goside", [3, 0]])
+                i = i + 3
+            p += 2
+
+#        print ("Stripe plan: " + str(sweep_steps))
+
+#        x = [-25, 44]
+#        i = 1
+#        sweep_steps.append(["goto", [-11,-10, SPEED_ON]])
+#        sweep_steps.append(["turn", math.radians(90)])
+#        sweep_steps.append(["turn", math.radians(-180)])
+#        sweep_steps.append(["goto", [-27, -50, SPEED_ON]])
+#        for y in range (-50, 40, 4):
+#            sweep_steps.append(["goto", [x[i], y, SPEED_ON]])
+#            sweep_steps.append(["goto", [x[1-i], y, -SPEED_ON]])
+#            sweep_steps.append(["goside", [4, 0]])
 
 
 
@@ -137,8 +179,8 @@ class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
 
             while current_sweep_step < len(sweep_steps) and self.sim_time - start_time < timedelta(minutes=60):
                 try:
-                    self.virtual_bumper = VirtualBumper(timedelta(seconds=4), 0.1) # radius of "stuck" area
-                    with LidarCollisionMonitor(self):
+                    self.virtual_bumper = VirtualBumper(timedelta(seconds=4), 0.2) # radius of "stuck" area; a little more as the robot flexes
+                    with LidarCollisionMonitor(self, 2500): # 2.5m
                         move, data = sweep_steps[current_sweep_step]
                         if move == "goto":
                             self.go_to_location(data[0], data[1], data[2])
@@ -147,37 +189,60 @@ class SpaceRoboticsChallengeRound1(SpaceRoboticsChallenge):
                         elif move == "turn":
                             self.turn(data, timeout=timedelta(seconds=10))
                         else:
-                            assert(False, "Incorrect move queued")
+                            assert False, "Incorrect move queued"
                         # if completed or timed out, move on; if exception, try again
                         current_sweep_step += 1
                         continue
                 except VSLAMLostException as e:
                     print("VSLAM lost")
                     self.returning_to_base = True
-                    sweep_steps.insert(current_sweep_step, ["goto", [-11,-10, SPEED_ON]])
+                    self.inException = True
+                    if move == "goto":
+                        self.go_straight(math.copysign(3, -data[2]), timeout=timedelta(seconds=10)) # go 3m in opposite direction
+                    elif move == "goside":
+                        self.move_sideways(math.copysign(1, -data[0]))
+                    elif move == "turn":
+                        self.turn(-data/2, timeout=timedelta(seconds=10))
+                    self.inException = False
+                    #sweep_steps.insert(current_sweep_step, ["goto", [-11,-10, SPEED_ON]])
                     continue
                 except VSLAMFoundException as e:
                     print("VSLAM found")
                     current_sweep_step += 1
                     continue
                 except LidarCollisionException as e:
+                    # TODO: Lidar can be triggered when going backwards if climbing on top of a rock and camera points down
                     print(self.sim_time, "Lidar")
                     self.inException = True
-                    self.go_straight(-2.0, timeout=timedelta(seconds=10))
+                    self.go_straight(-1) # go 1m backward
+                    # only go around if this is a forward movement
+                    if move == "goto" and data[2] > 0:
+                        self.lidar_drive_around()
                     self.inException = False
-                    current_sweep_step += 1
+                    # continue to previous target from a position next to the rock
+                    #current_sweep_step += 1
                 except VirtualBumperException as e:
                     move, data = sweep_steps[current_sweep_step] # what were we trying to do?
+                    self.send_speed_cmd(0.0, 0.0)
                     print(self.sim_time, "Bumper")
                     self.inException = True
                     if move == "goto":
-                        self.go_straight(math.copysign(2.0, -data[2]), timeout=timedelta(seconds=10)) # go 2m in opposite direction
+                        # evasion pattern:
+                        # 1) pull back 0.5m so we can maneuver;
+                        self.go_straight(math.copysign(0.5, -data[2]), timeout=timedelta(seconds=10)) # go 2m in opposite direction
+                        # 2) turn back to original direction (in case the bump changed direction)
+                        self.turn(self.get_angle_diff([data[0],data[1]], data[2]), timeout=timedelta(seconds=15))
+                        # 3) go away on diagonal in opposite direction
+                        # 4) go forward fixed distance in original direction to avoid a boulder (distance = max size of boulder plus cos of diagonal)
+                        # 5) go back in on diagonal in opposite direction
+                        # 6) turn control over to original plan
+                        self.drive_around_rock(math.copysign(6,  data[2])) # assume 6m the most needed
                     elif move == "goside":
                         self.move_sideways(math.copysign(1, -data[0]))
                     elif move == "turn":
-                        self.turn(data/2, timeout=timedelta(seconds=10))
+                        self.turn(-data/2, timeout=timedelta(seconds=10))
                     self.inException = False
-                    current_sweep_step += 1
+                    #current_sweep_step += 1
 
         except BusShutdownException:
             pass
