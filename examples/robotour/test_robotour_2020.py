a21528c060b0682b856c769c1ae38c9b905be82c
tajgr <levjakub@gmail.com>
2020-08-25 17:00:32 +0200

    examples/robotour/test_robotour_2020.py: test_nagate

--- examples/robotour/test_robotour_2020.py
+++ examples/robotour/test_robotour_2020.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest.mock import MagicMock
 import math
-from examples.robotour.robotour_2020 import get_direction
+import numpy as np
+from examples.robotour.robotour_2020 import get_direction, Robotour, downsample_scan
+
 
 class RobotourTest(unittest.TestCase):
     def test_get_direction(self):
@@ -8,6 +11,19 @@ class RobotourTest(unittest.TestCase):
         self.assertEqual(get_direction(-0.5, -1), -2.0344439357957027) # approx. -126.6 deg
         self.assertEqual(get_direction(0, -1), math.radians(-90))
         self.assertEqual(get_direction(-1, 0), math.radians(180))
+
+    def test_nagate(self):
+        scan = np.ones(270)
+        scan[135:] = 3
+        bus = MagicMock()
+        r = Robotour(bus=bus, config={"max_speed": 0.5})
+
+        r.last_scan = scan*1000
+        self.assertEqual(r.navigate(math.pi), math.pi/2)
+        self.assertEqual(r.navigate(0), 0.6981317007977318)
+
+        r.last_scan = scan[::-1] * 1000
+        self.assertEqual(r.navigate(0), -0.6981317007977318)
 
 
 if __name__ == '__main__':
