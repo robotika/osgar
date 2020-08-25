cf976d61e8f5d1d64f79d560b31c067b9d1c5536
tajgr <levjakub@gmail.com>
2020-08-25 17:02:31 +0200

    examples/robotour/robotour_2020.py: navigate around obstacles ver0

--- examples/robotour/robotour_2020.py
+++ examples/robotour/robotour_2020.py
@@ -3,7 +3,9 @@ Robotour competition
 https://robotika.cz/competitions/robotour/2020/cs
 """
 import math
+import numpy as np
 from datetime import timedelta
+import time
 
 from osgar.node import Node
 
@@ -17,6 +19,12 @@ def get_direction(x_diff, y_diff):
     return phi
 
 
+def downsample_scan(scan):
+    scan = np.median(np.reshape(scan[:810], (3, 270)), axis=0)
+    scan[scan==0] = 9999
+    return scan
+
+
 class Robotour(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
@@ -24,7 +32,10 @@ class Robotour(Node):
         self.speed = config['max_speed']
         self.pose = [0, 0, 0]
         self.verbose = False
-        self.destination = [1, 2]
+        self.destination = [1.5, 1.5]
+        self.last_scan = None
+        self.dangerous_dist = 0.35
+        self.min_safe_dist = 2
 
 
     def update(self):
@@ -34,6 +45,9 @@ class Robotour(Node):
         if channel == 'pose2d':
             x, y, heading = self.pose2d
             self.pose = (x / 1000.0, y / 1000.0, math.radians(heading / 100.0))
+        if channel == "scan":
+            assert len(self.scan) == 811, len(self.scan)
+            self.last_scan = downsample_scan(self.scan)
 
 
     def send_speed_cmd(self, speed, angular_speed):
@@ -47,24 +61,75 @@ class Robotour(Node):
         while self.time - start_time < dt:
             self.update()
 
+    def go_safely(self, desired_direction):
+        angular_speed = desired_direction
+        size = len(self.last_scan)
+        dist = np.min(self.last_scan[size//3:2*size//3])/1000
+        if dist > self.min_safe_dist:
+            self.send_speed_cmd(self.speed, angular_speed)
+        else:
+            if dist < self.dangerous_dist:
+                self.send_speed_cmd(0, 0)
+                print(self.time, "Dangerous dist!")
+                return False
+
+            speed = self.speed * (dist - self.dangerous_dist) / (self.min_safe_dist - self.dangerous_dist)
+            speed = max(0.25, speed)
+            self.send_speed_cmd(speed, angular_speed)
+
+        return True
+
+
+    def navigate(self, direction):
+        scan = self.last_scan/1000
+        # use max feasible direction
+        #print(direction)
+        direction = math.copysign(min(abs(direction), math.pi/2), direction)
+        direction_id = round(math.degrees(direction)) + 135
+        #print(direction_id)
+
+        binarized = scan>1.2
+        possible_directions = np.convolve(binarized, np.ones(80) / 80, mode="valid")
+        possible_directions_idx = np.where(possible_directions == 1)[0] + 40
+        #print(possible_directions_idx)
+        if len(possible_directions_idx) == 0:
+            return None
+
+        idx = np.argmin(abs(possible_directions_idx - direction_id))
+        #print(idx)
+        new_direction = possible_directions_idx[idx]
+        #print(new_direction)
+        safe_direction = math.radians(new_direction - 135)
+
+        return safe_direction
+
 
     def run(self):
         self.update()  # define self.time
         print(self.time, "Go!")
         start_time = self.time
         while self.time - start_time < timedelta(seconds=20):
-            x, y, heading = self.pose
-            y_diff = self.destination[1] - y
-            x_diff = self.destination[0] - x
-            destination_dist = math.hypot(x_diff, y_diff )
-            if destination_dist < 1:
-                print("Destination reached")
-                break
+            if self.last_scan is not None:
+                x, y, heading = self.pose
+                y_diff = self.destination[1] - y
+                x_diff = self.destination[0] - x
+                destination_dist = math.hypot(x_diff, y_diff )
+                if destination_dist < 1:
+                    print("Destination reached")
+                    break
 
-            direction = get_direction(x_diff, y_diff)
+                direction = get_direction(x_diff, y_diff) - heading
+                #t0 = time.time()
+                safe_direction = self.navigate(direction)
+                #print(time.time()-t0)
 
-            angular_speed = direction - heading
-            self.send_speed_cmd(self.speed, angular_speed)
+                if safe_direction:
+                    #print(self.time, direction, safe_direction)
+                    self.go_safely(safe_direction)
+
+                else:
+                    print(self.time, "NO SAVE DIRECTION!!!")
+                    self.send_speed_cmd(0, 0)
             self.update()
 
         self.send_speed_cmd(0.0, 0.0)
