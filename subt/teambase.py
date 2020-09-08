92d76501b7df846ea4598339fb214c0e3b90a8ea
Martin Dlouhy <md@robotika.cz>
2020-09-09 00:52:12 +0200

    add subt/teambase.py possibility to get robot name

--- subt/teambase.py
+++ subt/teambase.py
@@ -20,4 +20,27 @@ class Teambase(Node):
         if handler is not None:
             handler(getattr(self, channel))
 
+
+def main():
+    import argparse
+    import os
+    from osgar.lib.config import config_load
+    from osgar.record import record
+
+    parser = argparse.ArgumentParser(description='SubT Teambase')
+    parser.add_argument('config', nargs='+', help='configuration file')
+    parser.add_argument('--note', help='add description')
+    parser.add_argument('--log', nargs='?', help='record log filename')
+    parser.add_argument('--robot-name', required=True, help='T<simulated seconds>F* name')
+    args = parser.parse_args()
+
+    cfg = config_load(*args.config)
+    cfg['robot']['modules']['app']['init']['robot_name'] = args.robot_name
+    prefix = os.path.basename(args.config[0]).split('.')[0] + '-'
+    record(cfg, prefix, args.log)
+
+
+if __name__ == "__main__":
+    main()
+
 # vim: expandtab sw=4 ts=4
