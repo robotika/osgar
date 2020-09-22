ae233fb61789ec9db35c64640d62528698a2e489
Martin Dlouhy <md@robotika.cz>
2020-09-22 22:17:40 +0200

    subt/main.py - fix deadlock self.xyz and self.offset

--- subt/main.py
+++ subt/main.py
@@ -479,6 +479,9 @@ class SubTChallenge:
         self.trace.update_trace(self.xyz)
 
     def on_pose3d(self, timestamp, data):
+        if self.xyz is None:
+            # to avoid deadlock when we are waiting for offset but it is defined after self.xyz is not None
+            self.xyz = data[0]
         if self.offset is None:
             # we cannot align global coordinates if offset is not known
             return
