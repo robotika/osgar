59e370aeec68f5a301dd9b6da3925546f3870cae
Martin Dlouhy <md@robotika.cz>
2020-09-24 22:56:56 +0200

    subt/artifacts.py - report accumulated list of artifacts

--- subt/artifacts.py
+++ subt/artifacts.py
@@ -437,6 +437,16 @@ class ArtifactReporter(Node):
         super().__init__(config, bus)
         bus.register("artf_cmd")
         self.repeat_report_sec = config.get('repeat_report_sec')
+        self.artf_xyz = []
+        self.artf_xyz_accumulated = []
+
+    def publish_artf(self, artf_xyz):
+        print(self.time, "DETECTED:")
+        for artf_type, ix, iy, iz in artf_xyz:
+            print(" ", artf_type, ix/1000.0, iy/1000.0, iz/1000.0)
+            s = '%s %.2f %.2f %.2f\n' % (artf_type, ix/1000.0, iy/1000.0, iz/1000.0)
+            self.publish('artf_cmd', bytes('artf ' + s, encoding='ascii'))
+        print('report completed')
 
     def update(self):  # hack, this method should be called run instead!
         channel = super().update()  # define self.time
@@ -445,14 +455,18 @@ class ArtifactReporter(Node):
         if channel == 'sim_time_sec':
             if self.repeat_report_sec is None or self.sim_time_sec % self.repeat_report_sec != 0:
                 return channel
+            if len(self.artf_xyz) == 0:
+                return channel
+        elif channel == 'artf_xyz':
+            for item in self.artf_xyz:
+                if item not in self.artf_xyz_accumulated:
+                    self.artf_xyz_accumulated.append(item)
 
-        print(self.time, "DETECTED:")
-        for artf_type, ix, iy, iz in self.artf_xyz:
-            print(" ", artf_type, ix/1000.0, iy/1000.0, iz/1000.0)
-            s = '%s %.2f %.2f %.2f\n' % (artf_type, ix/1000.0, iy/1000.0, iz/1000.0)
-            self.publish('artf_cmd', bytes('artf ' + s, encoding='ascii'))
+        if self.repeat_report_sec is None:
+            self.publish_artf(self.artf_xyz)
+        else:
+            self.publish_artf(self.artf_xyz_accumulated)
 
-        print('report completed')
         return channel
 
 
--- subt/test_artifacts.py
+++ subt/test_artifacts.py
@@ -139,6 +139,44 @@ class ArtifactDetectorTest(unittest.TestCase):
         # well, I am not sure how to ensure only 2 calls :(
         bus.publish.assert_has_calls([call('artf_cmd', b'artf BACKPACK 0.10 0.20 -0.00\n'),]*2)
 
+    def test_artf_empty_repeat(self):
+        config = {
+            'repeat_report_sec': 10
+        }
+        bus = MagicMock()
+        reporter = ArtifactReporter(config, bus)
+        sim_time_sec = 0
+        bus.listen = MagicMock(return_value=(1, 'sim_time_sec', sim_time_sec))
+        reporter.update()
+        bus.publish.assert_not_called()
+
+    def test_artf_report_aggregation(self):
+        config = {
+            'repeat_report_sec': 10
+        }
+        bus = MagicMock()
+        reporter = ArtifactReporter(config, bus)
+        bus.listen = MagicMock(return_value=(1, 'artf_xyz', [['TYPE_BACKPACK', 100, 200, -3]]))
+        reporter.update()
+        bus.listen = MagicMock(return_value=(1, 'artf_xyz', [['TYPE_ROPE', 10, 20, 30]]))
+        reporter.update()
+        bus.reset_mock()
+        sim_time_sec = 0
+        bus.listen = MagicMock(return_value=(1, 'sim_time_sec', sim_time_sec))
+        reporter.update()
+        bus.publish.assert_has_calls([call('artf_cmd', b'artf TYPE_BACKPACK 0.10 0.20 -0.00\n'),
+                                      call('artf_cmd', b'artf TYPE_ROPE 0.01 0.02 0.03\n')])
+
+        # keep list unique
+        bus.listen = MagicMock(return_value=(1, 'artf_xyz', [['TYPE_BACKPACK', 100, 200, -3]]))
+        reporter.update()
+        bus.reset_mock()
+        sim_time_sec = 0
+        bus.listen = MagicMock(return_value=(1, 'sim_time_sec', sim_time_sec))
+        reporter.update()
+        # the same issue, that I do not want subset, but 1:1 correspondence - TODO more strict assert
+        bus.publish.assert_has_calls([call('artf_cmd', b'artf TYPE_BACKPACK 0.10 0.20 -0.00\n'),
+                                      call('artf_cmd', b'artf TYPE_ROPE 0.01 0.02 0.03\n')])
 
 # vim: expandtab sw=4 ts=4
 
