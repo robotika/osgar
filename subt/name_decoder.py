5bcd435e30a075b0b4534d725b9dbb405a0407a7
Martin Dlouhy <md@robotika.cz>
2020-09-27 19:20:26 +0200

    subt/name_decoder.py - support more complex names

--- subt/name_decoder.py
+++ subt/name_decoder.py
@@ -2,6 +2,20 @@
   Virtual SubT Challenge - decoder of robot name
 """
 
+def split_multi(s, delimiters):
+    # split string s via multiple character delimiters
+    ret = []
+    word = ''
+    for c in s:
+        word += c
+        if c in delimiters:
+            ret.append(word)
+            word = ''
+    if word != '':
+        ret.append(word)
+    return ret
+
+
 def parse_robot_name(robot_name):
     options = {
         'L': 'left',
@@ -9,13 +23,22 @@ def parse_robot_name(robot_name):
         'C': 'center',
         'W': 'wait'
     }
-    times_sec = [int(x) for x in robot_name[1:-1].split('F')]
-    side = options[robot_name[-1]]
+    parts = robot_name[1:-1].split('F')
+    default_side = options[robot_name[-1]]
     ret = []
-    ret.append(('wait', times_sec[0]))
-    for t in times_sec[1:]:
-        ret.append((side, t))
-        ret.append(('home', t*2))
+    ret.append(('wait', int(parts[0])))
+    for run in parts[1:]:
+        sum_t = 0
+        for part in split_multi(run, options.keys()):
+            try:
+                t = int(part)
+                side = default_side
+            except ValueError:
+                t = int(part[:-1])
+                side = options[part[-1]]
+            ret.append((side, t))
+            sum_t += t
+        ret.append(('home', sum_t * 2))
     return ret
 
 # vim: expandtab sw=4 ts=4
