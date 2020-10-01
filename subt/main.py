0bb33eeaf202257ce3338b436c8066eca212b4a0
Martin Dlouhy <md@robotika.cz>
2020-10-01 10:58:00 +0200

    subt/main.py - set Ver79

--- subt/main.py
+++ subt/main.py
@@ -902,7 +902,7 @@ class SubTChallenge:
         self.stdout('Final xyz (DARPA coord system):', (x + x0, y + y0, z + z0))
 
     def play_virtual_track(self):
-        self.stdout("SubT Challenge Ver78!")
+        self.stdout("SubT Challenge Ver79!")
         self.stdout("Waiting for robot_name ...")
         while self.robot_name is None:
             self.update()
