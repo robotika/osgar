a3a6c10c75fa3403796bcdfdcff510e6c410ace1
Zbynek Winkler <zw@robotika.cz>
2020-10-01 13:27:44 +0200

    fixup! subt.tools.validator: skip teambase, autodetect pose3d stream

--- subt/tools/validator.py
+++ subt/tools/validator.py
@@ -99,14 +99,14 @@ def autodetect_name(logfile):
 
 def autodetect_pose3d(logfile):
     streams = lookup_stream_names(logfile)
-    streams = set(s.split('.')[0] for s in streams if s.endswith('.pose3d'))
-    if 'offseter' in streams:
+    nodes = set(s.split('.')[0] for s in streams if s.endswith('.pose3d'))
+    if 'offseter' in nodes:
         return 'offseter.pose3d'
-    if 'localization' in streams:
+    if 'localization' in nodes:
         return 'localization.pose3d'
-    if len(streams) == 1:
-        return streams[0]+'.pose3d'
-    assert False, f"pose3d stream autodetection failed: {streams}"
+    if len(nodes) == 1:
+        return nodes[0]+'.pose3d'
+    assert False, f"pose3d stream autodetection failed: {nodes}"
 
 
 def main():
@@ -185,8 +185,6 @@ def main():
             ax1.set_xlabel('sim time (s)')
             plt.legend()
             plt.show()
-
-    return
 
 
 if __name__ == "__main__":
