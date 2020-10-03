49f0267dc4463bb4339c2684d7590f38d368316e
Martin Dlouhy <md@robotika.cz>
2020-10-03 16:32:25 +0200

    subt/breadcrumbs.py - deploy based on distance
    as alternative to sim_time_sec interval

--- subt/breadcrumbs.py
+++ subt/breadcrumbs.py
@@ -4,17 +4,29 @@
 from ast import literal_eval
 
 from osgar.node import Node
+from subt.trace import distance3D
 
 
 class Breadcrumbs(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
-        bus.register("deploy")
+        bus.register("deploy", "location")
 
         self.robot_name = config.get('robot_name')
         self.start_time = None  # unknown
         self.next_deploy_time = None
         self.step_time = config.get('step_sec')
+
+        self.radius = config.get('radius')
+        self.locations = [[0, 0, 0]]  # all locations + fake for the base
+
+    def should_deploy(self, xyz):
+        if self.radius is None:
+            return False
+        for loc in self.locations:
+            if distance3D(xyz, loc) < self.radius:
+                return False
+        return True
 
     def on_sim_time_sec(self, data):
         if self.start_time is None:
@@ -24,6 +36,13 @@ class Breadcrumbs(Node):
         if self.next_deploy_time is not None and data > self.next_deploy_time:
             self.next_deploy_time += self.step_time
             self.publish('deploy', [])  # ROS std_msg/Empty expects empty list as input
+
+    def on_pose3d(self, data):
+        xyz, quat = data
+        if self.should_deploy(xyz):
+            self.locations.append(xyz)
+            self.publish('deploy', [])
+            self.publish('location', xyz)
 
     def update(self):
         channel = super().update()
