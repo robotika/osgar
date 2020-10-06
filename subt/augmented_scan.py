f01cf8c892680babe1e63b02ae7af22e32739eac
Martin Dlouhy <md@robotika.cz>
2020-10-06 21:13:37 +0200

    subt/augmented_scan.py - WIP (not working)

--- subt/augmented_scan.py
+++ subt/augmented_scan.py
@@ -1,19 +1,52 @@
 """
   Augmented (Reality) scan with virtual obstacles
 """
+import math
+
+import numpy as np
+
 from osgar.node import Node
+
+
+def compute_scan360(barrier, limit=10.0):
+    """
+    compute raw scan with 360 degrees
+    - ignore yaw, higher resolution, offset and Z coordinate
+    """
+    scan = np.zeros(shape=(360,), dtype=np.uint16)
+    for x, y, z in barrier:
+        dist = math.hypot(x, y)
+        if dist < limit:
+            angle = math.atan2(y, x)
+            i = int(math.degrees(angle))
+            if i < 0:
+                i += 360
+            scan[i] = int(dist * 1000)
+    return scan
 
 
 class AugmentedScan(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
         bus.register("scan")
+        self.barrier = []  # updated remotely via Node
+        self.xyz, self.quat = None, None
 
     def on_scan(self, data):
-        self.publish('scan', data)
+        if len(self.barrier) == 0 or self.xyz is None or self.quat is None:
+            self.publish('scan', data)
+        else:
+            arr = np.array(self.barrier) - np.array(self.xyz)
+            print(arr)
+            tmp = compute_scan360(arr)[:270]
+            mask = tmp != 0
+            scan = np.array(data)
+            print(mask)
+            scan[mask] = tmp
+            self.publish('scan', scan.tolist())
 
     def on_pose3d(self, data):
-        xyz, quat = data
+        self.xyz, self.quat = data
 
     def on_barrier(self, data):
         pass
--- subt/test_augmented_scan.py
+++ subt/test_augmented_scan.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 
-from subt.augmented_scan import AugmentedScan
+from subt.augmented_scan import AugmentedScan, compute_scan360
 from osgar.lib import quaternion
 
 
@@ -13,5 +13,24 @@ class AugmentedScanTest(unittest.TestCase):
         scan = [5000] * 270
         bread.on_scan(scan)
         bus.publish.assert_called()
+
+    def test_barrier(self):
+        barrier = [[10, 20, 0]]  # xyz
+        scan = compute_scan360(barrier, limit=10)
+        self.assertEqual(max(scan), 0)
+
+        scan = compute_scan360(barrier, limit=30)
+        self.assertEqual(max(scan), 22360)
+
+    def test_update(self):
+        bus = MagicMock()
+        bread = AugmentedScan(bus=bus, config={})
+        bread.barrier = [[10, 5, 0]]
+        bread.on_pose3d([[8, 5, 0], quaternion.identity()])
+        scan = [5000] * 270
+        bread.on_scan(scan)
+        scan2 = [5000] * 270
+        scan2[0] = 2000
+        bus.publish.assert_called_with('scan', scan2)
 
 # vim: expandtab sw=4 ts=4
