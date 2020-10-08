9ec6e86483dd088567bba2822120b7ca7cc005d3
Martin Dlouhy <md@robotika.cz>
2020-10-08 15:53:49 +0200

    subt/test_artf_node.py - add debug test

--- subt/test_artf_node.py
+++ subt/test_artf_node.py
@@ -58,4 +58,42 @@ class ArtifactDetectorDNNTest(unittest.TestCase):
         self.assertEqual(checked_result, expected_result)
 
 
+if __name__ == "__main__":
+    # run specific test based on recorded stdout
+    import argparse
+    from datetime import timedelta
+    from osgar.lib.serialize import deserialize
+    from osgar.logger import LogReader, lookup_stream_id
+    from ast import literal_eval
+
+    parser = argparse.ArgumentParser(description='Test 3D reports')
+    parser.add_argument('logfile', help='OSGAR logfile')
+    parser.add_argument('--time-limit-sec', '-t', help='cut time in seconds', type=float)
+    args = parser.parse_args()
+
+    stdout_stream_id = lookup_stream_id(args.logfile, 'detector.stdout')
+#    depth_stream_id = lookup_stream_id(args.logfile, 'detector.debug_depth')
+
+    # dummy depth for loder logfiles
+    row = [5000] * 640
+    depth = np.array([row] * 360, dtype=np.uint16)
+    with LogReader(args.logfile,
+                   only_stream_id=stdout_stream_id) as logreader:
+        for time, stream, msg_data in logreader:
+            if args.time_limit_sec is not None:
+                if time.total_seconds() > args.time_limit_sec:
+                    break
+            assert stream == stdout_stream_id
+            data = deserialize(msg_data)
+#            print(time, data)
+            try:
+                arr = literal_eval(data)
+            except:
+                arr = None
+
+            if arr is not None:
+                ret = result2report(arr, depth)
+                print(ret)
+
+
 # vim: expandtab sw=4 ts=4
