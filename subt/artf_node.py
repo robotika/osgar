548b4ebb7c2164da238c701b16c925198d4f58b2
Martin Dlouhy <md@robotika.cz>
2020-10-10 04:40:34 +0200

    subt/artf_node.py - try str() instead

--- subt/artf_node.py
+++ subt/artf_node.py
@@ -164,7 +164,7 @@ class ArtifactDetectorDNN(Node):
         if result or result_cv:
             # publish the results independent to detection validity
             self.stdout(result, result_cv)  # for debugging in case of crash
-            self.publish('debug_result', result2list(result))
+            self.publish('debug_result', str(result))
             self.publish('debug_cv_result', cv_result2list(result_cv))
             checked_result = check_results(result, result_cv)
             if checked_result:
