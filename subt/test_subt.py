a85293e072523fbb7f5153037b2f91b308d23e16
Martin Dlouhy <md@robotika.cz>
2020-10-14 19:59:35 +0200

    subt/main.py - remove dependency on offset for pose3d (#691)
    * subt/main.py - remove dependency on offset for pose3d
    
    * subt/main.py - remove unused self.xyz_quat
    
    * subt/simulation.py - publish also 'pose3d' in global coordinates
    
    * subt/main.py - set Ver86

--- subt/test_subt.py
+++ subt/test_subt.py
@@ -44,6 +44,8 @@ class SubTChallengeTest(unittest.TestCase):
         sim = simulation.Simulation(bus.handle('sim'), end_condition=entrance_reached)
         g_logger.info("connecting:")
         for o in bus.handle('sim').out:
+            if o == 'pose2d':
+                continue  # connect 'pose3d' only
             g_logger.info(f'  sim.{o} → app.{o}')
             bus.connect(f'sim.{o}', f'app.{o}')
         for o in bus.handle('app').out:
@@ -62,8 +64,7 @@ class SubTChallengeTest(unittest.TestCase):
         bus = MagicMock()
         app = SubTChallenge(config, bus)
         data = ['TYPE_BACKPACK', [5000, -2359, 2222]]
-        app.offset = 100.0, 2.0, 3.0
-        app.xyz = 0, 0, 0
+        app.xyz = 100.0, 2.0, 3.0
         app.orientation = quaternion.identity()
         bus.publish.reset_mock()
         app.on_artf(None, data)
