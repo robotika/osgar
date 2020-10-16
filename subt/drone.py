7469d6f36695236b3cfd34ef3d4651c826266382
Martin Dlouhy <md@robotika.cz>
2020-10-16 08:50:53 +0200

    subt/drone.py - "vsechno vylejt" - P=1, v=0.7m/s

--- subt/drone.py
+++ subt/drone.py
@@ -9,8 +9,8 @@ from osgar.node import Node
 HEIGHT = 2.0
 EMERGENCY_HEIGHT = 0.5
 MAX_ANGULAR = 0.7
-MAX_VERTICAL = 1
-PID_P = 1.5  # 2.0  # 0.5
+MAX_VERTICAL = 0.7
+PID_P = 1.0
 
 
 def altitude_from_pressure(p):
