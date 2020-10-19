9d645ffa988241aea383e36fa20341ded2e89401
Martin Dlouhy <md@robotika.cz>
2020-10-19 15:20:35 +0200

    Revert "subt/main.py - set Ver83"
    This reverts commit d28efc97b2ce1ac929bf5e2ab4a283a1cab20c71.

--- subt/main.py
+++ subt/main.py
@@ -915,7 +915,7 @@ class SubTChallenge:
         self.stdout('Final xyz (DARPA coord system):', (x + x0, y + y0, z + z0))
 
     def play_virtual_track(self):
-        self.stdout("SubT Challenge Ver83!")
+        self.stdout("SubT Challenge Ver82!")
         self.stdout("Waiting for robot_name ...")
         while self.robot_name is None:
             self.update()
