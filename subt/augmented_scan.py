93fcdb2f9a073d71da29b7088bd360c036d75d86
Martin Dlouhy <md@robotika.cz>
2020-12-19 18:45:26 +0100

    fist attempt to integrate augmented scan for X4 drone

--- subt/augmented_scan.py
+++ subt/augmented_scan.py
@@ -8,6 +8,7 @@
 import math
 
 import numpy as np
+from scipy.interpolate import interp1d  # quick and dirty workaround - to be removed
 
 from osgar.node import Node
 from osgar.lib import quaternion
@@ -44,8 +45,8 @@ def compute_scan360(xyz, radius, limit=10.0):
 class AugmentedScan(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
-        bus.register("scan")
-        self.barrier = None  # updated remotely via Node
+        bus.register("scan", "scan360")
+        self.barrier = [[10.0, 0.0, 0.0], 5.0]  # experiment, was None  # updated remotely via Node
         self.xyz, self.quat = None, None
 
     def on_scan(self, data):
@@ -65,6 +66,26 @@ class AugmentedScan(Node):
             scan[mask] = 0
             self.publish('scan', scan.tolist())
 
+    def on_scan360(self, data):
+        if self.barrier is None or self.xyz is None or self.quat is None:
+            self.publish('scan', data)
+        else:
+            assert len(data) == 692, len(data)  # a bit strange resolution for the drone?!
+            assert len(self.barrier) == 2, self.barrier  # expected 1 barrier [[x, y, z], radius]
+            arr = np.array(self.barrier[0]) - np.array(self.xyz)
+            tmp = compute_scan360(arr, radius=self.barrier[1])
+            index = int(math.degrees(quaternion.heading(self.quat)))
+            tmp = np.concatenate((tmp, tmp, tmp))[index+360-180:index+360+180]
+            scan = np.array(data, dtype=np.uint16)
+            mask = (scan == 0)
+            scan[mask] = UNLIMITED
+            xp = np.arange(0, len(tmp), len(tmp)/len(scan))
+            nearest = interp1d(np.arange(len(scan)), scan, kind='nearest')
+            scan = np.minimum(scan, nearest(xp))
+            mask = (scan == UNLIMITED)
+            scan[mask] = 0
+            self.publish('scan360', scan.tolist())
+
     def on_pose3d(self, data):
         self.xyz, self.quat = data
 
@@ -76,5 +97,7 @@ class AugmentedScan(Node):
         handler = getattr(self, "on_" + channel, None)
         if handler is not None:
             handler(getattr(self, channel))
+        else:
+            assert False, channel  # not supported
 
 # vim: expandtab sw=4 ts=4
