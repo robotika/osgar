76f3b52d08b9d49454447b43e9a8f728490c349d
Martin Dlouhy <md@robotika.cz>
2021-02-17 22:29:00 +0100

    Integrate octomap for drone X4 (#765)
    * Integrate octomap for drone X4
    
    * subt/main.py - set Ver98

--- subt/cloudsim2osgar.py
+++ subt/cloudsim2osgar.py
@@ -88,6 +88,8 @@ class main:
             topics.append(('/' + robot_name + '/odom_fused', Odometry, self.odom_fused, ('pose3d',)))
             topics.append(('/' + robot_name + '/air_pressure', FluidPressure, self.air_pressure, ('air_pressure',)))
             topics.append(('/' + robot_name + '/front_scan', LaserScan, self.scan360, ('scan360',)))
+            if robot_name.endswith('XM'):
+                topics.append(('/mapping/octomap_binary', Octomap, self.octomap, ('octomap',)))
             if robot_is_marsupial == 'true':
                 rospy.loginfo("X4 is marsupial")
                 publishers['detach'] = rospy.Publisher('/' + robot_name + '/detach', Empty, queue_size=1)
