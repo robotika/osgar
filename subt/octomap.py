20efa78ef713778b17df2889b2ec46c9556a1e90
Martin Dlouhy <md@robotika.cz>
2021-03-16 16:44:19 +0100

    subt/octomap.py - handle variable zlevel needed for drones

--- subt/octomap.py
+++ subt/octomap.py
@@ -211,6 +211,7 @@ class Octomap(Node):
         self.pose3d = None
         self.video_writer = None
         self.video_outfile = None  # 'octo.mp4'  # optional video output generation
+        self.zlevel = config.get('zlevel', 0.5)
 
     def on_sim_time_sec(self, data):
         if self.time_limit_sec is None:
@@ -237,7 +238,7 @@ class Octomap(Node):
         x = self.pose3d[0][0] - self.start_xyz[0]
         y = self.pose3d[0][1] - self.start_xyz[1]
         start = int(512 + 2*x), int(512 - 2*y)
-        img = data2maplevel(data, level=1)  # 0.5m above the ground?
+        img = data2maplevel(data, level=int(round(self.zlevel/0.5)))  # 0.5m above the ground?
         img2, path = frontiers(img, start)
         cv2.circle(img2, start, radius=0, color=(39, 127, 255), thickness=-1)
         cv2.imwrite('octo_cut.png', img2)  # used for replay debugging
@@ -253,7 +254,7 @@ class Octomap(Node):
             self.video_writer.write(img2)
 
         if path is not None:
-            self.waypoints = [[(x - 512)/2 + self.start_xyz[0], (512 - y)/2 + self.start_xyz[1], 0] for x, y in path]
+            self.waypoints = [[(x - 512)/2 + self.start_xyz[0], (512 - y)/2 + self.start_xyz[1], self.zlevel] for x, y in path]
 
     def update(self):
         channel = super().update()
