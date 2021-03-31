16af7edaf33bbb9a4b3ac89928acdf19809435c7
Martin Dlouhy <md@robotika.cz>
2021-03-31 22:17:26 +0200

    subt/test_depth2scan.py - fix unittest

--- subt/test_depth2scan.py
+++ subt/test_depth2scan.py
@@ -9,16 +9,16 @@ from subt.depth2scan import vertical_step, monotize, find_step
 class Depth2ScanTest(unittest.TestCase):
 
     def test_vertical_step(self):
-        depth = np.zeros((360, 640), dtype=np.uint16)
+        depth = np.zeros((360, 640), dtype=np.float32)
         self.assertIsNone(vertical_step(depth))
 
-        depth[:, 320] = np.arange(0, 4*360, 4)
+        depth[:, 320] = np.arange(0, 4*360, 4)/1000.0
         #self.assertIsNone(vertical_step(depth))
 
-        depth = np.zeros((360, 640), dtype=np.uint16)
-        depth[:,:] = 200
+        depth = np.zeros((360, 640), dtype=np.float32)
+        depth[:,:] = 0.2
         for i in range(180, 250):
-            depth[i, 320] = 400
+            depth[i, 320] = 0.4
         self.assertEqual(vertical_step(depth), 200)  # TODO better example
 
     def test_monotize(self):
