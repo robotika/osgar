248a265e54943ec5a9260c85fb0d2cafa0d2f019
Martin Dlouhy <md@robotika.cz>
2021-04-01 19:49:36 +0200

    virtual-depth-view.bat - tweaks to see new data format

--- subt/script/virtual-depth-view.bat
+++ subt/script/virtual-depth-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar depth2scan.scan --camera rosmsg.image --camera2 rosmsg.depth --keyframes detector.artf --title rosmsg.sim_time_sec %*
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar depth2scan.scan --rgbd fromrospy.rgbd_front --camera2 rosmsg.depth --keyframes detector.localized_artf --title rosmsg.sim_time_sec %*
