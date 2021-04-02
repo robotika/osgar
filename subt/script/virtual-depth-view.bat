28022e8bff4e06e7b99261f98b408ffa88e923f1
Martin Dlouhy <md@robotika.cz>
2021-04-02 13:44:44 +0200

    subt/script/virtual-depth-view.bat - use logimage.image/depth

--- subt/script/virtual-depth-view.bat
+++ subt/script/virtual-depth-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar depth2scan.scan --rgbd fromrospy.rgbd_front --camera2 rosmsg.depth --keyframes detector.localized_artf --title rosmsg.sim_time_sec %*
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar depth2scan.scan --camera logimage.image --camera2 logimage.depth --keyframes detector.localized_artf --title rosmsg.sim_time_sec %*
