0887c1237b1a41b7ad3affb8f170a7ad478e6a50
Martin Dlouhy <md@robotika.cz>
2021-04-16 18:39:21 +0200

    add test_filter_pointcloud.py - dummy

---
+++ subt/ros/robot/src/test_filter_pointcloud.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import MagicMock, patch, call
+
+# https://stackoverflow.com/questions/8658043/how-to-mock-an-import
+import sys
+sys.modules['rospy'] = MagicMock()
+sys.modules['sensor_msgs'] = MagicMock()
+sys.modules['sensor_msgs.msg'] = MagicMock()
+import filter_pointcloud
+
+
+class FilterPointCloudTest(unittest.TestCase):
+
+    def test_usage(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: expandtab sw=4 ts=4
