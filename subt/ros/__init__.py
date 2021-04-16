f68ce5a3b5180488a4ede473d2eea2cec3c93ddc
Martin Dlouhy <md@robotika.cz>
2021-04-16 21:36:32 +0200

    FIXUP attempt to add /subt/ros/robot/src/test_*.py files into suite

--- subt/ros/__init__.py
+++ subt/ros/__init__.py
@@ -1,4 +1,4 @@
 # not so invasive test of Python2 /robot/src/test_*.py files
 from osgar.lib import create_load_tests
 import os.path
-load_tests = create_load_tests(os.path.join(os.dirname(__file__), 'robot', 'src', 'dummy.py'))
+load_tests = create_load_tests(os.path.join(os.path.dirname(__file__), 'robot', 'src', 'dummy.py'))
