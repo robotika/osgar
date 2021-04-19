76aa12c1bc7b89627d17e9ac85be4da096eef8d5
Martin Dlouhy <md@robotika.cz>
2021-04-19 18:52:27 +0200

    subt/push.py - reduce verbosity

--- subt/push.py
+++ subt/push.py
@@ -37,8 +37,6 @@ class Push:
             with contextlib.closing(socket):
                 while True:
                     dt, channel, data = self.bus.listen()
-                    print("*"*50)
-                    print("sending:", data)
                     raw = msgpack.packb(data, use_bin_type=True)
                     socket.send_multipart([bytes(channel, 'ascii'), raw])
         except zmq.ZMQError as e:
