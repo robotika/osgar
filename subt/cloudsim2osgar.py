4ab1ff79790651c681f064ee1018364537d0bf4e
Martin Dlouhy <md@robotika.cz>
2021-04-20 22:24:28 +0200

    subt/cloudsim2osgar.py - comment lidars, use /points

--- subt/cloudsim2osgar.py
+++ subt/cloudsim2osgar.py
@@ -120,11 +120,12 @@ class main:
 
         elif robot_config == "CTU_CRAS_NORLAB_X500_SENSOR_CONFIG_1":
             rospy.loginfo("X500 drone")
-            topics.append(('/' + robot_name + '/top_scan', LaserScan, self.top_scan, ('top_scan',)))
-            topics.append(('/' + robot_name + '/bottom_scan', LaserScan, self.bottom_scan, ('bottom_scan',)))
+#            topics.append(('/' + robot_name + '/top_scan', LaserScan, self.top_scan, ('top_scan',)))
+#            topics.append(('/' + robot_name + '/bottom_scan', LaserScan, self.bottom_scan, ('bottom_scan',)))
+            topics.append(('/' + robot_name + '/points', PointCloud2, self.points, ('points',)))
             topics.append(('/' + robot_name + '/odom_fused', Odometry, self.odom_fused, ('pose3d',)))
             topics.append(('/' + robot_name + '/air_pressure', FluidPressure, self.air_pressure, ('air_pressure',)))
-            topics.append(('/' + robot_name + '/front_scan', LaserScan, self.scan360, ('scan360',)))
+#            topics.append(('/' + robot_name + '/front_scan', LaserScan, self.scan360, ('scan360',)))
             topics.append(('/rtabmap/rgbd/compressed', RGBDImage, self.rgbd_front, ('rgbd_front',)))
             if robot_name.endswith('XM'):
                 topics.append(('/mapping/octomap_binary', Octomap, self.octomap, ('octomap',)))
