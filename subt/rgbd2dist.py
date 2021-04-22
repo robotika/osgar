aae03232bc9ad13a415135b41e933067d7d1c4ed
Martin Dlouhy <md@robotika.cz>
2021-04-22 16:40:11 +0200

    add subt/rgbd2dist.py - RGBD up & down to single lidar reading

---
+++ subt/rgbd2dist.py
@@ -0,0 +1,33 @@
+"""
+  Extract distance from RGBD and report it as single lidar distance
+"""
+import numpy as np
+
+from osgar.node import Node
+from osgar.bus import BusShutdownException
+from osgar.lib.depth import decompress as decompress_depth
+
+
+class RGB2Dist(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register("dist")
+
+    def on_rgbd(self, data):
+        robot_pose, camera_pose, rgb_compressed, depth_compressed = data
+
+        arr = decompress_depth(depth_compressed) * 1000
+        arr = np.clip(arr, 1, 0xFFFF)
+        arr = np.ndarray.astype(arr, dtype=np.dtype('H'))
+        self.publish('dist', [arr[0][0]])  # TODO center or median
+
+    def update(self):
+        channel = super().update()
+        handler = getattr(self, "on_" + channel, None)
+        if handler is not None:
+            handler(getattr(self, channel))
+        else:
+            assert False, channel  # unsupported channel
+        return channel
+
+# vim: expandtab sw=4 ts=4
