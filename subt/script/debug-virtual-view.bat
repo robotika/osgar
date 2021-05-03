a2e581492a46ed0ac178599895e11e2ad710c2c0
Martin Dlouhy <md@robotika.cz>
2021-05-03 21:14:51 +0200

    subt/script/debug-virtual-view.bat - stream name tweaks for detector debugging

--- subt/script/debug-virtual-view.bat
+++ subt/script/debug-virtual-view.bat
@@ -1,1 +1,1 @@
-python -m osgar.tools.lidarview --pose2d app.pose2d --lidar rosmsg.scan --camera detector.debug_artf --camera2 detector.debug_depth --keyframes detector.artf --title rosmsg.sim_time_sec %*
+python -m osgar.tools.lidarview --pose2d app.pose2d --lidar fromrospy.scan360 --rgbd detector.debug_rgbd --keyframes detector.localized_artf --title rosmsg.sim_time_sec %*
