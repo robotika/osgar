4e3db47b97a82e411a1ef093df12f58624b8d2cf
Martin Dlouhy <md@robotika.cz>
2021-06-17 14:18:51 +0200

    subt/artf_node.py - FIXUP merge conflict from master (undo local changes in X500 branch)

--- subt/artf_node.py
+++ subt/artf_node.py
@@ -184,7 +184,7 @@ class ArtifactDetectorDNN(Node):
 
     def run(self):
         try:
-            self.stdout(cv2.dnn.getAvailableTargets(cv2.dnn.DNN_BACKEND_OPENCV))  # test for OpenCL
+            self.stdout(cv2.dnn.getAvailableTargets(cv2.dnn.DNN_BACKEND_CUDA))
             dropped = 0
             while True:
                 now = self.publish("dropped", dropped)
