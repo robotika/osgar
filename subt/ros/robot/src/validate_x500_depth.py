11a7982381c80a9337363e1a20d6eea67931ccec
Martin Dlouhy <md@robotika.cz>
2021-07-05 21:29:22 +0200

    attempt to decompress depth data

--- subt/ros/robot/src/validate_x500_depth.py
+++ subt/ros/robot/src/validate_x500_depth.py
@@ -4,13 +4,28 @@
 """
 
 import rospy
+import numpy as np
+import cv2
 
 from rtabmap_ros.msg import RGBDImage
+
+
+def decompress(data):
+    # RTABMap compresses float32 depth data into RGBA channes of a PNG image.
+    # It does not, however, store information about endiannes. All we can do is
+    # hope that the current machine and the data origin machine have the same
+    # one.
+    return cv2.imdecode(np.frombuffer(data, np.uint8),
+                        cv2.IMREAD_UNCHANGED).view(dtype=np.float32)[:,:,0]
 
 
 def validation_callback(msg):
     global valid_rgbd_pub
     rospy.loginfo("X500 validator - received data")
+    arr = decompress(msg.depth_compressed.data)
+    height, width = arr.shape
+    dist = arr[height // 2:, :].min()
+    rospy.loginfo("X500 validator - dist = " + str(dist))
     valid_rgbd_pub.publish(msg)
 
 
