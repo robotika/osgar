5a4ae6a7ab6fba33281772e34d03d39dad1a609f
Martin Dlouhy <md@robotika.cz>
2021-07-05 14:18:35 +0200

    subt/ros/robot/src/validate_x500_depth.py - add debug msg

--- subt/ros/robot/src/validate_x500_depth.py
+++ subt/ros/robot/src/validate_x500_depth.py
@@ -10,14 +10,17 @@ from rtabmap_ros.msg import RGBDImage
 
 def validation_callback(msg):
     global valid_rgbd_pub
+    rospy.loginfo("X500 validator - received data")
     valid_rgbd_pub.publish(msg)
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('validate_x500_depth', anonymous=True)
+        rospy.loginfo("X500 validator started!")
         valid_rgbd_pub = rospy.Publisher('/rgbd_image_in', RGBDImage, queue_size=10)
         rospy.Subscriber('/rgbd_image_out',RGBDImage, validation_callback)
+        rospy.loginfo("X500 validator ready to spin!")
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
