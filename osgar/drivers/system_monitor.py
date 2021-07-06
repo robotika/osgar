0a7cc5ec14c9851a8aa0e6f1f841cb1f274e50a7
tajgr <levjakub@gmail.com>
2021-07-06 10:53:34 +0200

    osgar/drivers/system_monitor.py: temperature only on linux OS

--- osgar/drivers/system_monitor.py
+++ osgar/drivers/system_monitor.py
@@ -2,6 +2,7 @@
     Records system information like CPU and RAM utilization or device temperatures.
 """
 
+import sys
 import psutil
 # https://psutil.readthedocs.io/en/latest/#system-related-functions
 from threading import Thread
@@ -12,6 +13,7 @@ class SystemMonitor:
         self.bus = bus
         bus.register('cpu', 'ram', 'temp')
         self.sleep = config.get('sleep', 1)
+        self.platform = sys.platform
 
     def start(self):
         self.input_thread.start()
@@ -23,15 +25,13 @@ class SystemMonitor:
         while self.bus.is_alive():
             # current system-wide CPU utilization as a percentage
             cpu = psutil.cpu_percent(percpu=True)
-            if cpu:
-                self.bus.publish('cpu', cpu)
+            self.bus.publish('cpu', cpu)
             # system memory usage in percentage
             used_memory = psutil.virtual_memory().percent
-            if used_memory:
-                self.bus.publish('ram', used_memory)
-            # Probably it is not working on MS Windows. TODO test it.
-            temp_raw = psutil.sensors_temperatures()
-            if temp_raw:
+            self.bus.publish('ram', used_memory)
+
+            if self.platform == "linux":
+                temp_raw = psutil.sensors_temperatures()
                 assert "acpitz" in temp_raw, temp_raw
                 temp = temp_raw["acpitz"][0].current  # Expects only one CPU.
                 self.bus.publish('temp', temp)
