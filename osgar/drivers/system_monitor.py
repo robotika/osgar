a30554f0394fb078be04e676eb315edda31b691d
tajgr <levjakub@gmail.com>
2021-07-08 19:38:50 +0200

    osgar/drivers/system_monitor.py: add dmesg logging

--- osgar/drivers/system_monitor.py
+++ osgar/drivers/system_monitor.py
@@ -3,9 +3,14 @@
 """
 
 import sys
+import subprocess
 import psutil
 # https://psutil.readthedocs.io/en/latest/#system-related-functions
 from threading import Thread
+
+
+def get_timestamp(msg):
+    return float(msg.split()[0][1:-1])
 
 class SystemMonitor:
     def __init__(self, config, bus):
@@ -13,8 +18,31 @@ class SystemMonitor:
         self.bus = bus
         bus.register('cpu', 'ram', 'temp')
         self.sleep = config.get('sleep', 1)
+        self.log_dmesg = config.get('dmesg', False)
+        self.last_dmesg_time = None
+        if self.log_dmesg:
+            bus.register('dmesg')
         self.platform = sys.platform
         self.first_meas = True
+
+    def process_dmesg(self, dmesg_all):
+        ret = ""
+        if self.last_dmesg_time is None:
+            self.last_dmesg_time = get_timestamp(dmesg_all[-2])  # the last line is empty
+            return None
+        new_last_time = get_timestamp(dmesg_all[-2])
+        if self.last_dmesg_time == new_last_time:
+            return None
+
+        ii = -2
+        while True:
+            msg = dmesg_all[ii].decode("utf-8")
+            ret = msg + "\n" + ret
+            ii -= 1
+            timestamp = get_timestamp(dmesg_all[ii])
+            if self.last_dmesg_time == timestamp:
+                self.last_dmesg_time = new_last_time
+                return ret
 
     def start(self):
         self.input_thread.start()
@@ -39,6 +67,11 @@ class SystemMonitor:
                 assert "acpitz" in temp_raw, temp_raw
                 temp = temp_raw["acpitz"][0].current  # Expects only one CPU.
                 self.bus.publish('temp', temp)
+                proc_dmesg = subprocess.Popen(["dmesg"], stdout=subprocess.PIPE)
+                dmesg_all = proc_dmesg.stdout.read().split(b"\n")
+                dmesg = self.process_dmesg(dmesg_all)
+                if dmesg is not None:
+                    self.bus.publish('dmesg', dmesg)
 
             self.bus.sleep(self.sleep)
 
