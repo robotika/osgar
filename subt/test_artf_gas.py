d135f8188a60331f421f1b6e60cca3ed8bb4e4a1
Martin Dlouhy <md@robotika.cz>
2021-08-15 21:58:10 +0200

    Fix Gas detection for drones - move Z coordinate to the ground (#857)
    * Fix Gas detection for drones - move Z coordinate to the ground
    
    * config/coro_pam.json - fix missing link to bottom_scan

--- subt/test_artf_gas.py
+++ subt/test_artf_gas.py
@@ -19,4 +19,19 @@ class ArtifactGasDetectorTest(unittest.TestCase):
         bus.publish.assert_called()
         bus.publish.assert_has_calls([call('localized_artf', [GAS, [1.0, 0, 1.0]])])
 
+    def test_drone_z(self):
+        # the location point is on the ground level, which is OK for ground vehicles but not for drones
+        bus = bus=MagicMock()
+        detector = ArtifactGasDetector(bus=bus, config={})
+        detector.on_pose3d([[1, 2, 3], [0, 0, 0, 1]])
+        detector.on_gas_detected(False)
+        detector.on_pose3d([[1, 0, 1], [0, 0, 0, 1]])
+        detector.on_bottom_scan([1.0])  # top/bottom scans are in meters
+
+        bus.publish.reset_mock()
+        detector.on_gas_detected(True)
+        bus.publish.assert_called()
+        bus.publish.assert_has_calls([call('localized_artf', [GAS, [1.0, 0, 1.0 - 1.0]])])
+
+
 # vim: expandtab sw=4 ts=4
