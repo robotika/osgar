436a8574d1b0b67971b5c4b443d90bcfd40acfc7
tajgr <levjakub@gmail.com>
2021-09-06 16:20:26 +0200

    subt/jetson_artf_node.py: implements result2report

--- subt/jetson_artf_node.py
+++ subt/jetson_artf_node.py
@@ -9,9 +9,7 @@ import numpy as np
 from subt.tf_detector import CvDetector
 
 from osgar.node import Node
-
-def result2report(result):
-    return 1
+from subt.artf_node import result2report
 
 
 class ArtifactDetectorJetson(Node):
@@ -20,6 +18,9 @@ class ArtifactDetectorJetson(Node):
         bus.register("localized_artf", "dropped", "debug_image", "debug_result")
         self.time = None
         self.width = None
+        self.height = None
+        self.camera_pose = config.get("camera_pose", ([0, 0, 0], [0, 0, 0, 1]))
+        self.fx = config.get("fx", 149.01)
         self.detector = CvDetector().subt_detector
 
     def wait_for_data(self):
@@ -43,15 +44,20 @@ class ArtifactDetectorJetson(Node):
 
     def detect(self, img):
         if self.width is None:
-            self.width = img.shape[1]
+            self.height, self.width = img.shape[:2]
         assert self.width == img.shape[1], (self.width, img.shape[1])
+        assert self.height == img.shape[0], (self.height, img.shape[0])
 
         result = self.detector(img)
         if result:
             print(result)
-            report = result2report(result)
-            if report is not None:
-                self.publish('localized_artf', report)
-                self.publish('debug_image', img)
+            for res in result:
+                dist = 2  # There is no source of the artf dist in this moment so just put some number
+                report = result2report(res, (dist, self.width, self.height), self.fx, ([0, 0, 0], [0, 0, 0, 1]),
+                                            self.camera_pose, 10)  # TODO real camera_pose and robot_pose
+                if report is not None:
+                    print(report)
+                    self.publish('localized_artf', report)
+                    self.publish('debug_image', img)
 
         return result
