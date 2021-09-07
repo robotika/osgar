d51ab4f37d0bb8d1d6e5f18c5877488933bce2b7
tajgr <levjakub@gmail.com>
2021-09-07 20:54:53 +0200

    subt/script/run_kloubak2_jetson.sh: initial commit

---
+++ subt/script/run_kloubak2_jetson.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+trap "exit;" HUP INT TERM
+trap "kill 0" EXIT
+(python -m subt run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600 --used_old_record; python -m osgar.record config/test-lora.json) &
+ssh -t k2jetson "python3 -m osgar.record ~/ros_ws/src/osgar/config/jetson-node-k2-front.json"
+wait
