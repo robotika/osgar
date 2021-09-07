282ed499ee6963fb966fb10299ca680f3b4ed305
tajgr <levjakub@gmail.com>
2021-09-07 21:10:48 +0200

    subt/script/run_kloubak2_jetson.sh: fix jetson name

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -2,6 +2,6 @@
 
 trap "exit;" HUP INT TERM
 trap "kill 0" EXIT
-(python -m subt run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600 --used_old_record; python -m osgar.record config/test-lora.json) &
-ssh -t k2jetson "python3 -m osgar.record ~/ros_ws/src/osgar/config/jetson-node-k2-front.json"
+(python -m subt run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600 --used-old-record; python -m osgar.record config/test-lora.json) &
+ssh -t k2jetson_front "python3 -m osgar.record ~/ros_ws/src/osgar/config/jetson-node-k2-front.json"
 wait
