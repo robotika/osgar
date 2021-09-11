c3c0607b971ecf3b2092bd529e85375d8f62e492
Martin Dlouhy <md@robotika.cz>
2021-09-11 21:03:11 +0200

    Revert "disable bundler for a moment"
    This reverts commit f78231a83bc51e154efc7f263654940a3da629f9.

--- subt/bundle_rgbd.py
+++ subt/bundle_rgbd.py
@@ -29,7 +29,7 @@ class Bundler(Node):
         self.img = None
         self.depth = None
 
-        bus.register('rgbd:gzip')
+        bus.register('rgbd')
 
         # Pixel coordinates relative to the center of the image, with positive
         # directions to the right and up.
