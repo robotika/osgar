4df51452c374d0c09449e6cc8e115111d4f53fd3
pskotak <eltec@eltecweb.cz>
2021-09-13 00:09:40 +0200

    Set depth cameras to 10Hz

--- subt/script/run_skiddy.bash
+++ subt/script/run_skiddy.bash
@@ -2,4 +2,6 @@
 
 source ros/subt/setup.bash
 export PYTHONPATH=${PWD}:${PYTHONPATH}
-python3 -m subt --use-old-record run config/skiddy-subt.json --side left --speed 0.4 --timeout 600 --wall-dist 1.0 --gap-size 1.0
+export OMP_NUM_THREADS=1  # Make OpenCV eat fewer cpus.
+python3 -m subt.main --use-old-record run config/skiddy-subt.json --side left --speed 0.4 --timeout 180 --wall-dist 1.0 --gap-size 1.0
+#python3 -m osgar.go run config/skiddy-subt.json -s 0.3 -d -4 --timeout 30
