afd87b158c2a9139cb49c4c795b2c4657ca4fa5c
tajgr <levjakub@gmail.com>
2021-09-14 17:46:47 +0200

    subt/script/run_kloubak2_jetson.sh: update path for jetson front

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -3,5 +3,5 @@
 trap "exit;" HUP INT TERM
 trap "kill 0" EXIT
 (python -m subt run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600 --use-old-record; python -m osgar.record config/test-lora.json) &
-ssh -t k2jetson_front "cd ~/ros_ws/src/osgar/; python3 -m osgar.record ~/ros_ws/src/osgar/config/jetson-node-k2-front.json"
+ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json"
 wait
