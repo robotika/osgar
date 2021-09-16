fef03bf02a19e1bc4a4f02789a214f0d6bc12518
Jakub Lev <levjakub@gmail.com>
2021-09-16 16:57:58 +0200

    jetson node - artf detector (#910)
    * config/test-jetson_artf_node.json: initial commit
    
    * subt/jetson_artf_node.py: initial commit, detection from camera without depth
    
    * config/jetson-node-k2-front.json: initial commit, ip camera and detector
    
    * subt/tf_detector.py: parameter num_threads
    
    * config/test-pull_zmq.json: for test jetson node
    
    * config/jetson-node-k2-front.json: add zmq transmitter
    
    * subt/script/run_kloubak2_jetson.sh: initial commit
    
    * config/kloubak2-subt-estop-lora-jetson.json: add from_jetson
    
    * config/kloubak2-subt-estop-lora-jetson.json: remove rosmsg and scanmixer
    
    * config/kloubak2-subt-estop-lora-jetson.json: remove old detector links
    
    * config/jetson-node-k2-front.json: add realsence cameras
    
    * config/kloubak2-subt-estop-lora-jetson.json: add pose3d from_jetson_front
    
    * subt/push.py: implemnts bind option
    
    * subt/jetson_artf_node.py: convert cv_detector result to mdnet format, it is for result2report

---
+++ subt/script/run_kloubak2_jetson.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+trap "exit;" HUP INT TERM
+trap "kill 0" EXIT
+(python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600; python -m osgar.record config/test-lora.json) &
+ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json"
+wait
