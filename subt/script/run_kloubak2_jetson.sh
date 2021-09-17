ced0c66d6829d03321acc1b996ade5bb5f18300b
tajgr <levjakub@gmail.com>
2021-09-17 16:36:55 -0400

    subt/script/run_kloubak2_jetson.sh: add rear jetson

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -3,5 +3,6 @@
 trap "exit;" HUP INT TERM
 trap "kill 0" EXIT
 (python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600; python -m osgar.record config/test-lora.json) &
-ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json"
+ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json" &
+ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json"
 wait
