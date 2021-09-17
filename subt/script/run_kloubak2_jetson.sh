dcd3e1254c316b50218942141e959aa71cd152b6
tajgr <levjakub@gmail.com>
2021-09-17 18:41:36 -0400

    subt/script/run_kloubak2_jetson.sh: how to kill multiple processes

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-trap "exit;" HUP INT TERM
-trap "kill 0" EXIT
+#trap "exit;" HUP INT TERM
+#trap "kill 0" EXIT
+
+# when signal received just exit
+trap "echo 'got signal... exiting'; exit;" HUP INT TERM
+
+# when exiting, send SIGINT to all processes in current process group
+# and wait for them to finish
+trap "echo 'exiting... killing children...';  kill -s SIGINT 0; wait" EXIT
+
 (python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600; python -m osgar.record config/test-lora.json) &
 ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json" &
 ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json"
+
 wait
