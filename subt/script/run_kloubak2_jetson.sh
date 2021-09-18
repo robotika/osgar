592834d73645b82a5140ca735b6c283f3185d17c
tajgr <levjakub@gmail.com>
2021-09-18 08:50:45 -0400

    subt/script/run_kloubak2_jetson.sh: remove trap_ctrlc_front

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -6,15 +6,9 @@ function trap_ctrlc_front ()
 	    exit 2 
     }
 
-function trap_ctrlc_rear ()
-{
-	  ssh -t k2jetson_rear "pkill -15 -f 'osgar.record'"
-	    exit 2
-    }
-
 (python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600; python -m osgar.record config/test-lora.json) &
 
 trap "trap_ctrlc_front" 2
 ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json" &
-trap "trap_ctrlc_rear" 2
-ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json" &
+ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json"
+wait
