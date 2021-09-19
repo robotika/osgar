b0f7a293b2de18eb8292b1e0a5cac3c128fe922b
tajgr <levjakub@gmail.com>
2021-09-19 16:10:40 -0400

    subt/script/run_kloubak2_jetson.sh: use ros nojetson front, subt parameters

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -2,13 +2,13 @@
 
 function trap_ctrlc_front ()
 {
-	  ssh -t k2jetson_front "pkill -15 -f 'osgar.record'"
+	  ssh -t k2jetson_front "pkill -15 -f 'run_jetson_front.bash'"
 	    exit 2 
     }
 
-(python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side left --speed 0.5 --timeout 600; python -m osgar.record config/test-lora.json) &
+(python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side right --speed 0.5 --timeout 600 --gap-size 0.6 --wall-dist 0.6 --start-paused; python -m osgar.record config/test-lora.json) &
 
 trap "trap_ctrlc_front" 2
-ssh -t k2jetson_front "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-front.json" &
+ssh -t k2jetson_front "./subt/script/run_jetson_front.bash" &
 ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json"
 wait
