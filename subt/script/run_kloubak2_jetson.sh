ea96f695d5d77739247bf8be7ad27a85062d9b7f
tajgr <levjakub@gmail.com>
2021-09-19 21:19:33 -0400

    subt/script/run_kloubak2_jetson.sh: run jetson_rear backround

--- subt/script/run_kloubak2_jetson.sh
+++ subt/script/run_kloubak2_jetson.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-function trap_ctrlc_front ()
+function trap_ctrlc_rear ()
 {
-	  ssh -t k2jetson_front "pkill -15 -f 'run_jetson_front.bash'"
+	  ssh -t k2jetson_rear "pkill -15 -f 'osgar.record'"
 	    exit 2 
     }
 
 (python -m subt --use-old-record run config/kloubak2-subt-estop-lora-jetson.json --side right --speed 0.5 --timeout 600 --gap-size 0.6 --wall-dist 0.6 --start-paused; python -m osgar.record config/test-lora.json) &
 
-trap "trap_ctrlc_front" 2
-ssh -t k2jetson_front "./subt/script/run_jetson_front.bash" &
-ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json"
+trap "trap_ctrlc_rear" 2
+ssh -t k2jetson_rear "python3 -m osgar.record ~/git/osgar/config/jetson-node-k2-rear.json" &
+ssh -t k2jetson_front "cd git/osgar && ./subt/script/run_jetson_front.bash"
 wait
