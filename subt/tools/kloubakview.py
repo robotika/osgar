91c6c337def2af860462778176c23374431afb75
tajgr <levjakub@gmail.com>
2021-09-22 06:10:31 -0400

    subt/tools/kloubakview.py: update, scan360, pose3d

--- subt/tools/kloubakview.py
+++ subt/tools/kloubakview.py
@@ -3,12 +3,12 @@ from osgar.tools import lidarview
 
 if __name__ == "__main__":
     args = [
-        "--pose2d", "app.pose2d",
-        "--lidar", "lidar.scan",
-        "--lidar2", "lidar_back.scan",
+        "--pose3d", "from_jetson_front.pose3d",
+        "--lidar", "fromros.scan360",
+        "--deg","360",
         "--camera", "camera.raw",
         "--camera2", "camera_back.raw",
-        "--keyframes", "detector.artf",
+        "--keyframes", "from_jetson_rear.localized_artf",
         "--joint", "kloubak.joint_angle",
     ] + sys.argv[1:]
     lidarview.main(args, "kloubak")
