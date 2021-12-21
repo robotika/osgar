31a1d8a6a7f1f36e4ce5fc52fb272e2d8db75722
Martin Dlouhy <md@robotika.cz>
2021-12-21 16:14:04 +0100

    Add optional draw

--- osgar/drivers/pozyx.py
+++ osgar/drivers/pozyx.py
@@ -16,6 +16,7 @@ class Pozyx(Node):
         serial_port = config['port']
         self.devices = [int(x, 16) for x in config.get('devices', [])]  # unfortunately JSON does not support hex
         self.pozyx = pypozyx.PozyxSerial(serial_port)
+        self.verbose = False
 
     def run(self):
         try:
@@ -23,15 +24,35 @@ class Pozyx(Node):
             while self.bus.is_alive():
                 for from_id, to_id in itertools.combinations(self.devices, 2):
                     status = self.pozyx.doRanging(from_id, device_range, to_id)
+                    if self.verbose:
+                        print(device_range)
                     self.publish('range', [status, from_id, to_id, [device_range.timestamp, device_range.distance, device_range.RSS]])
         except BusShutdownException:
             pass
 
-    def draw(self):
-        """
-        Debug Draw
-        """
-        pass
 
+if __name__ == '__main__':
+    import argparse
+    import matplotlib.pyplot as plt
+    from osgar.logger import lookup_stream_id, LogReader
+    from osgar.lib.serialize import deserialize
+
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('logfile', help='logfile path')
+    args = parser.parse_args()
+
+    stream_id = lookup_stream_id(args.logfile, 'pozyx.range')
+    arr = []
+    t = []
+    for dt, channel, raw in LogReader(args.logfile, only_stream_id=stream_id):
+        data = deserialize(raw)
+        t.append(dt.total_seconds())
+        arr.append(data[3][1])  # range
+
+    print(len(arr))
+
+    plt.plot(t, arr, 'o-')
+    plt.show()
 
 # vim: expandtab sw=4 ts=4
