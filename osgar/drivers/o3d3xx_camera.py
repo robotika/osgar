5dd818dd9478807c4aeb2ced9f7a925ff2f382f5
tajgr <levjakub@gmail.com>
2022-01-10 10:40:48 +0100

    osgar/drivers/o3d3xx_camera.py: working ver0, publish depth and amplitude streams

--- osgar/drivers/o3d3xx_camera.py
+++ osgar/drivers/o3d3xx_camera.py
@@ -4,14 +4,22 @@
 """
 
 from threading import Thread
-import o3d3xx
+import logging
+import numpy as np
+
+g_logger = logging.getLogger(__name__)
+try:
+    import o3d3xx
+except:
+    g_logger.error("o3d3xx is not installed!")
 
 class O3DCamera:
     def __init__(self, config, bus):
         self.input_thread = Thread(target=self.run_input, daemon=True)
         self.bus = bus
         self.address = config.get("address", "192.168.0.69")
-        bus.register('raw')
+        self.bus.register('depth', 'amplitude_image')
+        self.cam = o3d3xx.FormatClient(self.address, 50010, o3d3xx.PCICFormat.blobs("amplitude_image", "distance_image"))
 
 
     def start(self):
@@ -23,9 +31,16 @@ class O3DCamera:
     def run_input(self):
         while self.bus.is_alive():
             # Capture frame-by-frame
-
-            #self.bus.publish('raw', data.tobytes())
-
+            data = self.cam.readNextFrame()
+            assert "distance_image" in data
+            assert "amplitude_image" in data
+            depth = np.frombuffer(data['distance_image'],dtype='uint16').reshape(132,176)
+            amplitude_image = np.frombuffer(data['amplitude_image'],dtype='uint16').reshape(132,176)
+            self.bus.publish("depth", depth)
+            self.bus.publish("amplitude_image", amplitude_image)
+            # for name in data:
+            #     print(name)
+            # print("---------------")
 
     def request_stop(self):
         self.bus.shutdown()
