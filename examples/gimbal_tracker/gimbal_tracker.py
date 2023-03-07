ef565a18e6394c646ce3c7b98485f5fcb8b5ccd2
Martin Dlouhy <md@robotika.cz>
2023-03-07 20:05:29 +0100

    add example gimbal_tracker

--- examples/gimbal_tracker/gimbal_tracker.py
+++ examples/gimbal_tracker/gimbal_tracker.py
@@ -7,13 +7,27 @@ zdroj videa (OSGAR "opencv" Node)
 
 from osgar.node import Node
 
+
 class Detector(Node):
-    pass
-
-
-class Servo(Node):
-    pass
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('bbox')
 
 
 class Controller(Node):
-    pass
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('desired_position')
+
+    def on_position(self, data):
+        pass
+
+
+class Servo(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('position')
+        self.position = 0
+    def on_tick(self, data):
+        # TODO move commands
+        self.publish('position', self.position)
