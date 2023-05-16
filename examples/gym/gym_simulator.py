5188204a096f4fd86cd9f1bb20a10837aa055d58
tajgr <levjakub@gmail.com>
2023-05-16 21:12:12 +0200

    gym_simulator.py add steer angle

--- examples/gym/gym_simulator.py
+++ examples/gym/gym_simulator.py
@@ -3,6 +3,7 @@
 """
 
 import gym
+from f110_gym.envs.base_classes import Integrator
 import yaml
 from argparse import Namespace
 import math
@@ -19,13 +20,16 @@ class GymSimulator(Node):
             conf_dict = yaml.load(file, Loader=yaml.FullLoader)
         self.conf = Namespace(**conf_dict)
         self.speed = 0
-        self.angular_speed = 0
+        self.steer = 0
 
     def on_desired_speed(self, data):
         speed, angular_speed = data
-        # print(angular_speed)
         self.speed = speed / 1000
-        self.angular_speed = math.radians(angular_speed / 100)
+        angular_speed = math.radians(angular_speed / 100)
+        if self.speed != 0:
+            self.steer = math.atan(0.58*angular_speed/self.speed)  # 0.58 is robot length?
+        else:
+            self.steer = 0
 
     def on_tick(self, data):
         # step simulation
@@ -37,22 +41,19 @@ class GymSimulator(Node):
         x = obs["poses_x"][0]
         y = obs["poses_y"][0]
         heading = obs["poses_theta"][0]
-        print(heading, x, y)
         self.publish('pose2d', [round(x * 1000), round(y * 1000), round(math.degrees(heading) * 100)])
 
     def run(self):
         try:
-            racecar_env = gym.make('f110_gym:f110-v0', map=self.conf.map_path, num_agents=1, timestep = 0.02)
-            obs, step_reward, done, info = racecar_env.reset(np.array([[0., 0., math.pi/2]]))  # pose of agent
-            # simulation loop
+            racecar_env = gym.make('f110_gym:f110-v0', map=self.conf.map_path, num_agents=1, timestep = 0.02,
+                                   integrator=Integrator.RK4)
+            obs, step_reward, done, info = racecar_env.reset(np.array([[self.conf.sx, self.conf.sy, self.conf.stheta]]))
             sim_time = 0
             racecar_env.render()
             # loops when env not done
             while not done:
                 self.update()
-                # stepping through the environment
-                # print("tu", self.angular_speed)
-                obs, step_reward, done, info = racecar_env.step(np.array([[0.001, self.speed]]))
+                obs, step_reward, done, info = racecar_env.step(np.array([[self.steer, self.speed]]))
                 racecar_env.render(mode='human')
                 self.send_data(obs)
                 sim_time += step_reward
