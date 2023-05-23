6b5932fd5168b7d3e15b4d1e33e45a58c63cd6eb
Martin Dlouhy <md@robotika.cz>
2023-05-23 21:04:01 +0200

    reduce filtered output to 1Hz

--- osgar/drivers/rtk_filter.py
+++ osgar/drivers/rtk_filter.py
@@ -1,6 +1,7 @@
 """
   RTK filter (reduce GPS NMEA to 1Hz for query to server)
 """
+from datetime import timedelta
 from osgar.node import Node
 
 
@@ -9,9 +10,21 @@ class RTKFilter(Node):
         super().__init__(config, bus)
         bus.register('filtered')
         self.verbose = False
+        self.trigger_time = None  # not defined
+        self.buf = b''
 
     def on_nmea(self, data):
-        self.publish('filtered', data)
-
+        if self.trigger_time is None:
+            self.trigger_time = self.time + timedelta(seconds=1)
+            self.buf = b''
+            return  # skip first buffer
+        if self.time < self.trigger_time:
+            return  # not there yet
+        self.buf += data
+        if b'$GNGGA' in self.buf:
+            if b'\r\n' in self.buf[self.buf.index(b'$GNGGA'):]:
+                self.publish('filtered', self.buf)
+                self.trigger_time = self.time + timedelta(seconds=1)
+                self.buf = b''
 
 # vim: expandtab sw=4 ts=4
