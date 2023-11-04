d3a8a23d2f12c9c263652add50ca71cbe9811f11
Martin Dlouhy <md@robotika.cz>
2023-11-04 19:40:07 +0100

    VanJeeLidar - fix port and trigger

--- osgar/drivers/vanjee.py
+++ osgar/drivers/vanjee.py
@@ -5,6 +5,7 @@ import struct
 import math
 
 from osgar.node import Node
+from osgar.bus import BusShutdownException
 
 
 class VanJeeLidar(Node):
@@ -14,5 +15,15 @@ class VanJeeLidar(Node):
 
     def on_raw(self, data):
         pass
+
+    def run(self):
+        magic_packet = b'\xFF\xAA\x00\x1E\x00\x00\x00\x00\x00\x00\x01\x01\x19\x0C\x00\x00\x00\x00\x00\x00\x00\x00\x05\x03\x00\x00\x00\x00\x00\x00\x00\x0D\xEE\xEE'
+        self.publish('raw', magic_packet)
+        try:
+            while True:
+                self.update()
+        except BusShutdownException:
+            pass
+
 
 # vim: expandtab sw=4 ts=4
