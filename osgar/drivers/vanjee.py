c87c5d51e246dc23464b14365d7bb0be149e70be
Martin Dlouhy <md@robotika.cz>
2023-11-06 10:46:55 +0100

    osgar/drivers/vanjee.py - completed scan publishing (plane, 0 deg)

--- osgar/drivers/vanjee.py
+++ osgar/drivers/vanjee.py
@@ -12,6 +12,8 @@ class VanJeeLidar(Node):
     def __init__(self, config, bus):
         bus.register('raw', 'xyz', 'scan')
         super().__init__(config, bus)
+        self.last_frame = None  # not defined
+        self.points = []
 
     def on_raw(self, data):
         assert len(data) in [34, 1384], len(data)
@@ -21,6 +23,9 @@ class VanJeeLidar(Node):
         length = struct.unpack_from('>H', data, 2)[0]
         assert length == 1380, length
         frame = struct.unpack_from('>H', data, 4)[0]
+        if self.last_frame is not None:
+            assert self.last_frame + 1 == frame, (self.last_frame, frame)
+        self.last_frame = frame
         assert data[6:8] == bytes.fromhex('0000'), data[6:10].hex()  # reserved
         # reserved [8:10]  - variable
         assert data[11:14] == bytes.fromhex('02 190C'), data[11:14].hex()
@@ -29,10 +34,17 @@ class VanJeeLidar(Node):
         bank, motor_speed = struct.unpack_from('>BH', data, 18)
         assert 1 <= bank <= 16, bank
         assert 38000 <= motor_speed <= 39300, motor_speed
+        if bank == 1:
+            self.points = []  # reset last scan
 
         assert data[-2:] == b'\xEE\xEE', data[-2:].hex()
-        points = struct.unpack_from('>' + 'BH' * 450, data, 21)
-#        assert 0, points[:20]
+        self.points.extend(struct.unpack_from('>' + 'BH' * 450, data, 21))
+        if bank == 16:
+            if len(self.points) == 2*7200:
+                scan = self.points[5::8]  # -10, -5, 0, 0.3
+                self.publish('scan', scan)
+            else:
+                print(self.time, f'Incomplete scan - only {len(self.points)//2} points out of 7200')  # intensity, dist in mm
 
     def run(self):
         magic_packet = b'\xFF\xAA\x00\x1E\x00\x00\x00\x00\x00\x00\x01\x01\x19\x0C\x00\x00\x00\x00\x00\x00\x00\x00\x05\x03\x00\x00\x00\x00\x00\x00\x00\x0D\xEE\xEE'
