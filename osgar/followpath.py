0fe4ccf956e9147def6c200007f3e88d18ed9739
Martin Dlouhy <md@robotika.cz>
2024-02-13 21:04:40 +0100

    use old Route, first attempt ignoring position offset

--- osgar/followpath.py
+++ osgar/followpath.py
@@ -3,8 +3,17 @@
 """
 import math
 
+from osgar.lib.route import Route as GPSRoute, DummyConvertor
 from osgar.node import Node
 from osgar.followme import EmergencyStopException
+
+
+class Route(GPSRoute):
+    """
+    Override GPS defaults to normal planar case
+    """
+    def __init__(self, pts=[]):
+        super().__init__(pts, conv=DummyConvertor(), isLoop=False)
 
 
 class FollowPath(Node):
@@ -12,11 +21,50 @@ class FollowPath(Node):
         super().__init__(config, bus)
         bus.register('desired_speed')
         self.last_position = [0, 0, 0]  # proper should be None, but we really start from zero
+        self.route = Route(pts=config.get('path', []))
+        self.max_speed = config.get('max_speed', 0.2)
         self.raise_exception_on_stop = False
         self.verbose = False
 
+    def nearest(self, pose, path):
+        if len(path) == 0:
+            return None, None
+        if len(path) == 1:
+            return path[0], None
+        # dense points? crossing lines?
+        min_dist = None
+        best = None, None
+        x, y, heading = pose
+        for i, p in enumerate(path):
+            dist = math.hypot(p[0] - x, p[1] - y)
+            if min_dist is None or dist < min_dist:
+                best = i
+                min_dist = dist
+        if best == 0:
+            return path[0], path[1]
+        if best == len(path) - 1:
+            return path[-1], None
+        # choose before or after
+        dist_before = math.hypot(path[best - 1][0] - x, path[best - 1][1] - y)
+        dist_after = math.hypot(path[best + 1][0] - x, path[best + 1][1] - y)
+        if dist_before < dist_after:
+            return path[best - 1], path[best]
+        else:
+            return path[best], path[best + 1]
+
+    def control(self, pose):
+        first, second = self.route.routeSplit(pose[:2])
+        if len(second) <= 1:
+            return 0, 0
+        pt = Route(second).pointAtDist(dist=0.2)  # maybe speed dependent
+        angle = Route(second).turnAngleAt(pt)
+        return self.max_speed, pose[2] - angle
+
     def on_pose2d(self, data):
-        pass  # ignore for now
+        x, y, heading = data
+        self.last_position = [x / 1000.0, y / 1000.0, math.radians(heading / 100.0)]
+        speed, angular_speed = self.control(self.last_position)
+        self.send_speed_cmd(speed, angular_speed)
 
     def on_emergency_stop(self, data):
         if self.raise_exception_on_stop and data:
--- osgar/test_followpath.py
+++ osgar/test_followpath.py
@@ -20,4 +20,41 @@ class FollowPathTest(unittest.TestCase):
         with self.assertRaises(EmergencyStopException):
             app.run()
 
+    def test_nearest(self):
+        app = FollowPath(config={}, bus=MagicMock())
+        a, b = app.nearest([0, 0, 0], [])
+        self.assertIsNone(a)
+        self.assertIsNone(b)
+
+        a, b = app.nearest([0, 0, 0], [[1, 2]])
+        self.assertEqual(a, [1, 2])  # take into account heading?
+        self.assertIsNone(b)
+
+        a, b = app.nearest([0, 0, 0], [[0, 0], [2, 0]])
+        self.assertEqual(a, [0, 0])
+        self.assertEqual(b, [2, 0])
+
+        a, b = app.nearest([2, 0.9, 0], [[0, 0], [2, 0], [2, 2]])
+        self.assertEqual(a, [2, 0])
+        self.assertEqual(b, [2, 2])
+
+    def test_control(self):
+        max_speed = 0.5
+        bus = Bus(MagicMock())
+        config = {
+            'max_speed': max_speed,
+            'path': [[0, 0], [3, 0], [3, 3]]
+        }
+        app = FollowPath(config=config, bus=bus.handle('app'))
+        self.assertEqual(app.control([0, 0, 0]), (max_speed, 0))
+
+    def test_follow_path(self):
+        bus = Bus(MagicMock())
+        config = {
+            'path': [[0, 0], [3, 0]]
+        }
+        app = FollowPath(config=config, bus=bus.handle('app'))
+
+
+
 # vim: expandtab sw=4 ts=4
