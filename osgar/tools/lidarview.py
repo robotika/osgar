c32665fb44cd6dc2d706cfcbb5401d0938541cd5
Martin Dlouhy <md@robotika.cz>
2024-03-18 11:05:43 +0100

    osgar/tools/lidarview.py - hacking yolo bboxes

--- osgar/tools/lidarview.py
+++ osgar/tools/lidarview.py
@@ -147,12 +147,30 @@ def draw(foreground, pose, scan, poses=[], image=None, bbox=None, callback=None,
 
         foreground.blit(cameraView, (0, 0))
 
+        def frameNorm(w, h, bbox):
+            normVals = np.full(len(bbox), w)
+            normVals[::2] = h
+            return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+        if len(bbox) > 0:
+            if bbox == [[]]:
+                bbox = []
+            elif len(bbox[0][0]) == 3:
+                bbox = bbox[0]  # TODO review API
+                old_bbox = bbox
+                bbox = []
+                w, h = image.get_size()
+                for detection in old_bbox:
+                    # new OAK-D format [name, confidence, [p1x, p1y, p2x, p2y]] (points in 0..1 scale)
+                    assert w > h, (w, h)  # cropped to (h x h)
+                    a, b, c, d = frameNorm(h, h, detection[2]).tolist()
+                    bbox.append(['cone', ] + [a + (w - h)/2, b, c - a, d - b] + [0, ])
         for b in bbox:
             assert len(b) > 5, b
             name, x, y, width, height = b[:5]
             color = (0, 255, 0)
             rect = pygame.Rect(x, y, width, height)
-            pygame.draw.rect(image, color, rect, 2)
+            pygame.draw.rect(image, color, rect, 3)
 
     if callback is not None:
         debug_poly = []
