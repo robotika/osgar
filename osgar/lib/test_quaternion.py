6a73bd9b3164b4714352de24eea1bb5ff0b13b09
Milan <milan.petrik@protonmail.com>
2025-04-03 11:39:43 +0200

    Extend unit tests for slerp()
    ... add non-trivial axes of rotation

--- osgar/lib/test_quaternion.py
+++ osgar/lib/test_quaternion.py
@@ -96,12 +96,41 @@ class QuaternionTest(TestCase):
             self.assertAlmostEqual(B[i], D[i], 3)
 
     def test_slerp__various_angles(self):
+        sqrt_3 = math.sqrt(3)
+        sqrt_1_7 = math.sqrt(1/7)
+        sqrt_2_7 = math.sqrt(2/7)
+        sqrt_4_7 = math.sqrt(4/7)
         for a in range(40):
             angle_A = 10*math.pi*(a - 20)/40
+            sin_A_half = math.sin(angle_A/2)
+            cos_A_half = math.cos(angle_A/2)
             for b in range(10):
                 angle_B = angle_A + math.pi*(b - 5)/10
-                A = [0.0, 0.0, math.sin(angle_A/2), math.cos(angle_A/2)]
-                B = [0.0, 0.0, math.sin(angle_B/2), math.cos(angle_B/2)]
+                sin_B_half = math.sin(angle_B/2)
+                cos_B_half = math.cos(angle_B/2)
+                # around x
+                A = [sin_A_half, 0.0, 0.0, cos_A_half]
+                B = [sin_B_half, 0.0, 0.0, cos_B_half]
+                for c in range(10):
+                    t = c / 9
+                    result = quaternion.slerp(A, B, t)
+                    expected_angle = (1 - t)*angle_A + t*angle_B
+                    expected = [math.sin(expected_angle/2), 0.0, 0.0, math.cos(expected_angle/2)]
+                    for i in range(4):
+                        self.assertAlmostEqual(result[i], expected[i], 3)
+                # around y
+                A = [0.0, sin_A_half, 0.0, cos_A_half]
+                B = [0.0, sin_B_half, 0.0, cos_B_half]
+                for c in range(10):
+                    t = c / 9
+                    result = quaternion.slerp(A, B, t)
+                    expected_angle = (1 - t)*angle_A + t*angle_B
+                    expected = [0.0, math.sin(expected_angle/2), 0.0, math.cos(expected_angle/2)]
+                    for i in range(4):
+                        self.assertAlmostEqual(result[i], expected[i], 3)
+                # around z
+                A = [0.0, 0.0, sin_A_half, cos_A_half]
+                B = [0.0, 0.0, sin_B_half, cos_B_half]
                 for c in range(10):
                     t = c / 9
                     result = quaternion.slerp(A, B, t)
@@ -109,8 +138,31 @@ class QuaternionTest(TestCase):
                     expected = [0.0, 0.0, math.sin(expected_angle/2), math.cos(expected_angle/2)]
                     for i in range(4):
                         self.assertAlmostEqual(result[i], expected[i], 3)
+                # around (1/sqrt(3), 1/sqrt(3), 1/sqrt(3))
+                A = [sin_A_half/sqrt_3, sin_A_half/sqrt_3, sin_A_half/sqrt_3, cos_A_half]
+                B = [sin_B_half/sqrt_3, sin_B_half/sqrt_3, sin_B_half/sqrt_3, cos_B_half]
+                for c in range(10):
+                    t = c / 9
+                    result = quaternion.slerp(A, B, t)
+                    expected_angle = (1 - t)*angle_A + t*angle_B
+                    exp_crd = math.sin(expected_angle/2) / sqrt_3
+                    expected = [exp_crd, exp_crd, exp_crd, math.cos(expected_angle/2)]
+                    for i in range(4):
+                        self.assertAlmostEqual(result[i], expected[i], 3)
+                # around (sqrt(1/7), sqrt(2/7), sqrt(4/7))
+                A = [sin_A_half*sqrt_1_7, sin_A_half*sqrt_2_7, sin_A_half*sqrt_4_7, cos_A_half]
+                B = [sin_B_half*sqrt_1_7, sin_B_half*sqrt_2_7, sin_B_half*sqrt_4_7, cos_B_half]
+                for c in range(10):
+                    t = c / 9
+                    result = quaternion.slerp(A, B, t)
+                    expected_angle = (1 - t)*angle_A + t*angle_B
+                    exp_sin = math.sin(expected_angle/2)
+                    exp_cos = math.cos(expected_angle/2)
+                    expected = [exp_sin*sqrt_1_7, exp_sin*sqrt_2_7, exp_sin*sqrt_4_7, exp_cos]
+                    for i in range(4):
+                        self.assertAlmostEqual(result[i], expected[i], 3)
 
-    def test_slerp__totally_random(self):
+    def test_slerp__around_z__totally_random(self):
         for i in range(20):
             t = random.random()
             angle_A = 10*math.pi*(random.random() - 0.5)
