e00265a6c219da2d8d908bbd41e9e69b2e5e7a82
jlev <jlev@tf.czu.cz>
2025-04-30 17:22:21 +0200

    test_depth.py: add basic unittests, in progress

--- osgar/lib/test_depth.py
+++ osgar/lib/test_depth.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from osgar.lib.depth import depth2dist
+from osgar.lib.depth import depth2dist, depth_to_rgb_align
 
 
 class DepthTest(unittest.TestCase):
@@ -20,5 +20,22 @@ class DepthTest(unittest.TestCase):
         self.assertTrue(values[100])
         self.assertTrue(values[630])
         self.assertFalse(values[300])
+
+    def test_depth2rgb(self):
+        T = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        intr_M = [[100, 0, 1], [0, 100, 1], [0, 0, 1]]
+        depth = np.ones((3,3), dtype=int) * 2000
+        depth2 = depth_to_rgb_align(depth, intr_M, intr_M, T, [3,3, 3])
+        print(depth2)
+        self.assertTrue(np.array_equal(depth, depth2))
+
+        intr_rgb = [[200, 0, 2.5], [0, 200, 2.5], [0, 0, 1]]
+        depth2 = depth_to_rgb_align(depth, intr_M, intr_rgb, T, [6, 6, 3])
+        print(depth2)
 
 # vim: expandtab sw=4 ts=4
