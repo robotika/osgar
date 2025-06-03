32b43d24ad6b9bff249d3250d70517d18bb11c1a
Jakub Lev <levjakub@gmail.com>
2025-06-03 08:08:25 +0200

    depth to rgb align (#1015)
    * lib/depth.py: add function for depth to rgb align
    
    * lib/depth.py: add description
    
    * test_depth.py: finish test_depth2rgb
    
    ---------
    
    Co-authored-by: Martin Dlouhy <md@robotika.cz>

--- osgar/lib/test_depth.py
+++ osgar/lib/test_depth.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from osgar.lib.depth import depth2dist
+from osgar.lib.depth import depth2dist, depth_to_rgb_align
 
 
 class DepthTest(unittest.TestCase):
@@ -20,5 +20,29 @@ class DepthTest(unittest.TestCase):
         self.assertTrue(values[100])
         self.assertTrue(values[630])
         self.assertFalse(values[300])
+
+    def test_depth2rgb(self):
+        T = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        intr_M = [[100, 0, 1], [0, 100, 1], [0, 0, 1]]
+        depth = np.ones((3,3), dtype=int) * 2000
+        depth2 = depth_to_rgb_align(depth, intr_M, intr_M, T, [3,3, 3])
+        self.assertTrue(np.array_equal(depth, depth2))
+
+        f_rgb = 5/3 * 100
+        intr_rgb = [[f_rgb, 0, 2], [0, f_rgb, 2], [0, 0, 1]]
+        depth2 = depth_to_rgb_align(depth, intr_M, intr_rgb, T, [5, 5, 3])
+        expected_res = np.array([
+            [1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1],
+        ], dtype=int) * 2000
+        self.assertTrue(np.array_equal(expected_res, depth2))
 
 # vim: expandtab sw=4 ts=4
