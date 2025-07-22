4eb6d699e6a88f26418f830d4fe2a11c3f9c7cf0
Martin Dlouhy <md@robotika.cz>
2025-07-22 21:03:51 +0200

    add change_required param and update config

--- osgar/terminator.py
+++ osgar/terminator.py
@@ -8,13 +8,27 @@ from osgar.followme import EmergencyStopException
 class Terminator(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
+        self.change_required = config.get('change_required')
+        self.prev_state = None  # unknown
         self.raise_exception_on_stop = True
 
     def on_terminate_if_true(self, data):
+        if self.change_required and self.prev_state is None:
+            self.prev_state = data
+            return  # initial value
+        if self.change_required and self.prev_state == data:
+            return   # no change
+        self.change_required = False  # now it finally changed
         if self.raise_exception_on_stop and data:
             raise EmergencyStopException()
 
     def on_terminate_if_false(self, data):
+        if self.change_required and self.prev_state is None:
+            self.prev_state = data
+            return  # initial value
+        if self.change_required and self.prev_state == data:
+            return   # no change
+        self.change_required = False  # now it finally changed
         if self.raise_exception_on_stop and not data:
             raise EmergencyStopException()
 
--- osgar/test_terminator.py
+++ osgar/test_terminator.py
@@ -11,3 +11,29 @@ class TerminatorTest(unittest.TestCase):
         app = Terminator(bus=bus, config={})
         with self.assertRaises(EmergencyStopException):
             app.on_terminate_if_false(False)
+
+    def test_change_required(self):
+        bus = MagicMock()
+        app = Terminator(bus=bus, config={
+            "change_required": True
+        })
+        # Emergency STOP is pressed, but we require change -> ignore
+        app.on_terminate_if_false(False)
+        # now it was released so change is fulfilled
+        app.on_terminate_if_false(True)
+        # and now it is finally rising exception on STOP release
+        with self.assertRaises(EmergencyStopException):
+            app.on_terminate_if_false(False)
+
+    def test_change_required_inv(self):
+        bus = MagicMock()
+        app = Terminator(bus=bus, config={
+            "change_required": True
+        })
+        # Emergency STOP is pressed, but we require change -> ignore
+        app.on_terminate_if_true(True)
+        # now it was released so change is fulfilled
+        app.on_terminate_if_true(False)
+        # and now it is finally rising exception on STOP release
+        with self.assertRaises(EmergencyStopException):
+            app.on_terminate_if_true(True)
