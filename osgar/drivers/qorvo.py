343c10fcef14b21d351cfdf37052a36decabc1e6
Martin Dlouhy <md@robotika.cz>
2025-11-29 14:08:30 +0100

    add unittest and switch to binary protocol

--- osgar/drivers/qorvo.py
+++ osgar/drivers/qorvo.py
@@ -4,22 +4,35 @@
 
 from osgar.node import Node
 
+# Note, that for ASCII shell communication add
+# self.publish("raw", b'\r\r')  # switch to generic shell commands
+
 
 class Qorvo(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
         bus.register('range', 'raw')
+        self.buf = b''
         self.verbose = False
-        self.initialized = False
+
+    def split_data(self, buf):
+        if len(buf) < 2 or len(buf) < buf[1] + 2:
+            return None, buf
+        packet_len = buf[1] + 2
+        return buf[:packet_len], buf[packet_len:]
 
     def on_raw(self, data):
-        if not self.initialized:
-            self.publish("raw", b'la\n')
-        self.initialized = True
+        self.buf += data
+        while True:
+            packet, self.buf = self.split_data(self.buf)
+            if packet is None:
+                break
+            if packet[0] == 0x40:
+                # response
+                assert len(packet) == 3, packet
 
     def on_timer(self, data):
-        if not self.initialized:
-            self.publish("raw", b'\n')
+        self.publish('raw', bytes([0x0C, 0x00]))  # request range reading
 
 
 # vim: expandtab sw=4 ts=4
---
+++ osgar/drivers/test_qorvo.py
@@ -0,0 +1,23 @@
+import unittest
+from osgar.drivers.qorvo import Qorvo
+
+from unittest.mock import MagicMock
+
+
+class QorvoTest(unittest.TestCase):
+
+    def test_split_data(self):
+        config = {}
+        handler = MagicMock()
+        uwb = Qorvo(config, bus=handler)
+        self.assertEqual(uwb.split_data(b''), (None, b''))
+        data = bytes.fromhex('400100410d00000000000000000000000000490100')
+        self.assertEqual(uwb.split_data(data),
+                         (b'@\x01\x00', b'A\r\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00I\x01\x00'))
+        data = b'A\r\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00I\x01\x00'
+        self.assertEqual(uwb.split_data(data),
+                         (bytes.fromhex('410d00000000000000000000000000'), b'I\x01\x00'))
+        self.assertEqual(uwb.split_data(b'I\x01\x00'),
+                         (b'I\x01\x00', b''))
+
+# vim: expandtab sw=4 ts=4
