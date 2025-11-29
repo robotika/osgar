178e647626e692e0f26c7b58b7baf53df35094c8
Martin Dlouhy <md@robotika.cz>
2025-11-29 14:38:39 +0100

    publish range at 20Hz (i.e. with duplicates)

--- osgar/drivers/qorvo.py
+++ osgar/drivers/qorvo.py
@@ -1,6 +1,7 @@
 """
   Simple(minded) driver for Qorvo UWB modules
 """
+import struct
 
 from osgar.node import Node
 
@@ -27,9 +28,25 @@ class Qorvo(Node):
             packet, self.buf = self.split_data(self.buf)
             if packet is None:
                 break
-            if packet[0] == 0x40:
-                # response
+            cmd = packet[0]
+            if cmd == 0x40:
+                # response start and end
                 assert len(packet) == 3, packet
+            elif cmd == 0x41:
+                # position
+                assert len(packet) == 2 + 13, packet
+                # X, Y, Z + quality
+                assert packet[-1] == 0, packet[-1]  # i.e. invalid position
+            elif cmd == 0x49:
+                # range distance
+                num_distances = packet[2]
+                dist_arr = []
+                if num_distances > 0:
+                    for i in range(num_distances):
+                        uwb_addr, dist, quality, x, y, z, pos_quality = struct.unpack_from(
+                            '<HIBiiiB',packet, 3 + i*20)
+                        dist_arr.append([uwb_addr, dist])
+                self.publish('range', dist_arr)
 
     def on_timer(self, data):
         self.publish('raw', bytes([0x0C, 0x00]))  # request range reading
--- osgar/drivers/test_qorvo.py
+++ osgar/drivers/test_qorvo.py
@@ -1,7 +1,7 @@
 import unittest
 from osgar.drivers.qorvo import Qorvo
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 
 class QorvoTest(unittest.TestCase):
@@ -19,5 +19,18 @@ class QorvoTest(unittest.TestCase):
                          (bytes.fromhex('410d00000000000000000000000000'), b'I\x01\x00'))
         self.assertEqual(uwb.split_data(b'I\x01\x00'),
                          (b'I\x01\x00', b''))
+
+    def test_range_decoding(self):
+        config = {}
+        handler = MagicMock()
+        uwb = Qorvo(config, bus=handler)
+        handler.reset_mock()
+        data = bytes.fromhex('400100410d00000000000000000000000000490100')  # no anchor
+        uwb.on_raw(data)
+        self.assertEqual(handler.publish.call_args, call('range', []))
+        handler.reset_mock()
+        data = b'I\x15\x017M\xdc\x02\x00\x00d\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00d'  # single anchor
+        uwb.on_raw(data)
+        self.assertEqual(handler.publish.call_args, call('range', [[19767, 732]]))
 
 # vim: expandtab sw=4 ts=4
