b665bf726dc5b09745dedb3989e943d386309bb3
tajgr <levjakub@gmail.com>
2026-02-27 21:10:25 +0100

    Add stereo_images option, ver0

--- osgar/drivers/oak_camera_v3.py
+++ osgar/drivers/oak_camera_v3.py
@@ -96,6 +96,7 @@ class OakCamera:
             def __init__(self, *args, **kwargs):
                 dai.node.HostNode.__init__(self, *args, **kwargs)
                 self.bus = None
+                self.stream_type = None
 
             def build(self, *args):
                 self.link_args(*args)
@@ -105,8 +106,8 @@ class OakCamera:
                 seq_num = frame.getSequenceNum()  # for sync of various outputs
                 dt = frame.getTimestamp()  # datetime.timedelta
                 timestamp_us = ((dt.days * 24 * 3600 + dt.seconds) * 1000000 + dt.microseconds)
-                self.bus.publish("color_seq", [seq_num, timestamp_us])
-                self.bus.publish("color", frame.getData().tobytes())
+                self.bus.publish(f"{self.stream_type}_seq", [seq_num, timestamp_us])
+                self.bus.publish(self.stream_type, frame.getData().tobytes())
 
 
         with dai.Pipeline() as pipeline:
@@ -121,16 +122,30 @@ class OakCamera:
                                                                        profile=self.video_encoder)
                 saver = pipeline.create(VideoPublisher).build(encoded.out)
                 saver.bus = self.bus
+                saver.stream_type = "color"
 
-            if self.is_depth:
+
+            if self.is_depth or self.is_stereo_images:
                 mono_left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
                 mono_right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
-                stereo = pipeline.create(dai.node.StereoDepth)
-                mono_left_out = mono_left.requestOutput(self.mono_resolution, fps=self.fps)
-                mono_right_out = mono_right.requestOutput(self.mono_resolution, fps=self.fps)
-                mono_left_out.link(stereo.left)
-                mono_right_out.link(stereo.right)
-                depth_queue = stereo.depth.createOutputQueue(blocking=False)
+
+                # type=dai.ImgFrame.Type.NV12 ??
+                mono_left_out = mono_left.requestOutput(self.mono_resolution, type=dai.ImgFrame.Type.NV12, fps=self.fps)
+                mono_right_out = mono_right.requestOutput(self.mono_resolution, type=dai.ImgFrame.Type.NV12, fps=self.fps)
+                if self.is_depth:
+                    stereo = pipeline.create(dai.node.StereoDepth)
+                    mono_left_out.link(stereo.left)
+                    mono_right_out.link(stereo.right)
+                    depth_queue = stereo.depth.createOutputQueue(blocking=False)
+
+                if self.is_stereo_images:
+                    for mono_out, stream_type in zip([mono_left_out, mono_right_out], ['left_im', 'right_im']):
+                        mono_encoded = pipeline.create(dai.node.VideoEncoder).build(mono_out,
+                                                                                    frameRate=self.fps,
+                                                                                    profile=self.video_encoder)
+                        mono_saver = pipeline.create(VideoPublisher).build(mono_encoded.out)
+                        mono_saver.bus = self.bus
+                        mono_saver.stream_type = stream_type
 
             # copy from basalt_vio.py
             imu = pipeline.create(dai.node.IMU)
