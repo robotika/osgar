0743fb126b7316028a3b4b8887d68fbd3f0b419f
Martin Dlouhy <md@robotika.cz>
2026-07-17 15:07:48 +0200

    Add package-level __version__ attribute to osgar (#1073)
    Co-authored-by: Elena Ai <elena.ai@robotika.cz>

--- osgar/__init__.py
+++ osgar/__init__.py
@@ -1,3 +1,16 @@
 
+try:
+    from importlib.metadata import version
+except ImportError:
+    try:
+        from importlib_metadata import version
+    except ImportError:
+        version = lambda name: "0.0.0"
+
+try:
+    __version__ = version("osgar")
+except Exception:
+    __version__ = "0.0.0"
+
 from osgar.lib import create_load_tests
 load_tests = create_load_tests(__file__)
